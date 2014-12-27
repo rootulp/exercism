1f4910ef6bb2a13e52ad6993f4f54fec41cef5e7
Rootul Patel <rootulp@gmail.com>
2014-12-26 19:49:50 -0500

    solve hamming

---
+++ ruby/hamming/hamming.rb
@@ -0,0 +1,17 @@
+class Hamming
+
+  def self.compute(strand1, strand2)
+    count = 0
+
+    strand1 = strand1.split(//)
+    strand2 = strand2.split(//)
+
+    strand1.each_with_index do |char,index|
+      count += 1 if strand2[index] != char
+    end
+
+    return count
+    
+  end
+
+end
---
+++ ruby/hamming/hamming_test.rb
@@ -0,0 +1,69 @@
+require 'minitest/autorun'
+begin
+  require_relative 'hamming'
+rescue LoadError => e
+  puts "\n\n#{e.backtrace.first} #{e.message}"
+  puts DATA.read
+  exit 1
+end
+
+class HammingTest < MiniTest::Unit::TestCase
+  def test_no_difference_between_identical_strands
+    assert_equal 0, Hamming.compute('A', 'A')
+  end
+
+  def test_complete_hamming_distance_of_for_single_nucleotide_strand
+    assert_equal 1, Hamming.compute('A', 'G')
+  end
+
+  def test_complete_hamming_distance_of_for_small_strand
+    assert_equal 2, Hamming.compute('AG', 'CT')
+  end
+
+  def test_small_hamming_distance
+    assert_equal 1, Hamming.compute('AT', 'CT')
+  end
+
+  def test_small_hamming_distance_in_longer_strand
+    assert_equal 1, Hamming.compute('GGACG', 'GGTCG')
+  end
+
+  def test_nonunique_characters_within_first_strand
+    assert_equal 1, Hamming.compute('AGA', 'AGG')
+  end
+
+  def test_nonunique_characters_within_second_strand
+    assert_equal 1, Hamming.compute('AGG', 'AGA')
+  end
+
+  def test_large_hamming_distance
+    assert_equal 4, Hamming.compute('GATACA', 'GCATAA')
+  end
+
+  def test_hamming_distance_in_very_long_strand
+    assert_equal 9, Hamming.compute('GGACGGATTCTG', 'AGGACGGATTCT')
+  end
+end
+
+__END__
+
+*****************************************************
+You got an error, which is exactly as it should be.
+This is the first step in the Test-Driven Development
+(TDD) process.
+
+The most important part of the error is
+
+      cannot load such file
+
+It's looking for a file named hamming.rb that doesn't
+exist yet.
+
+To fix the error, create an empty file named hamming.rb
+in the same directory as the hamming_test.rb file.
+
+Then run the test again.
+
+For more guidance as you work on this exercise, see
+GETTING_STARTED.md.
+*****************************************************
