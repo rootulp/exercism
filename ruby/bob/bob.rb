b68043769d0f09d71a91b501311b9da894a8232c
Rootul Patel <rootulp@gmail.com>
2014-12-27 19:26:58 -0500

    solve bob

---
+++ ruby/bob/bob.rb
@@ -0,0 +1,15 @@
+class Bob
+
+  def hey(phrase)
+    if phrase.strip.empty?
+      return "Fine. Be that way!"
+    elsif phrase == phrase.upcase and phrase != phrase.downcase
+      return "Whoa, chill out!"
+    elsif phrase.end_with?("?")
+      return "Sure." 
+    else
+      return 'Whatever.'
+    end
+  end
+
+end
