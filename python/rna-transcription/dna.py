b3e6b2827470833d56c582712931cf53ff55b7fa
Rootul Patel <rootulp@gmail.com>
2014-12-29 04:29:40 -0500

    solve rna transcription

---
+++ python/rna-transcription/dna.py
@@ -0,0 +1,15 @@
+def to_rna(strand):
+  result = ""
+
+  for i in strand:
+    result += pairing_for(i)
+
+  return result
+
+def pairing_for(nucleobase):
+  pairings = { "G" : "C",
+                     "C" : "G",
+                     "A" : "U",
+                     "T" : "A"}
+                     
+  return pairings[nucleobase]
