a1da062e02bf3334a0196d8bd90fecce490b6018
Rootul Patel <rootulp@gmail.com>
2015-01-11 21:11:29 -0500

    solve accumulate

---
+++ ruby/accumulate/accumulate_test.rb
@@ -0,0 +1,34 @@
+require 'minitest/autorun'
+require_relative 'array'
+
+class ArrayTest < MiniTest::Unit::TestCase
+  def test_empty_accumulation
+    assert_equal [], [].accumulate { |e| e * e }
+  end
+
+  def test_accumulate_squares
+    result = [1, 2, 3].accumulate do |number|
+      number * number
+    end
+    assert_equal [1, 4, 9], result
+  end
+
+  def test_accumulate_upcases
+    result = %w(hello world).accumulate(&:upcase)
+    assert_equal %w(HELLO WORLD), result
+  end
+
+  def test_accumulate_reversed_strings
+    result = %w(the quick brown fox etc).accumulate(&:reverse)
+    assert_equal %w(eht kciuq nworb xof cte), result
+  end
+
+  def test_accumulate_recursively
+    result = %w(a b c).accumulate do |char|
+      %w(1 2 3).accumulate do |digit|
+        "#{char}#{digit}"
+      end
+    end
+    assert_equal [%w(a1 a2 a3), %w(b1 b2 b3), %w(c1 c2 c3)], result
+  end
+end
---
+++ ruby/accumulate/array.rb
@@ -0,0 +1,14 @@
+# Solution from https://github.com/Azdaroth/exercism.io/blob/master/ruby/accumulate/array.rb
+class Array
+  
+  def accumulate
+    accumulated = []
+
+    each do |element|
+      accumulated << (yield element)
+    end
+
+    accumulated 
+  end
+
+end
