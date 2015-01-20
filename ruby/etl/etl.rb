b257e15b6f8480daec6f49a1944b8e11bb6ee046
Rootul Patel <rootulp@gmail.com>
2015-01-20 12:52:57 -0500

    solve etl

---
+++ ruby/etl/etl.rb
@@ -0,0 +1,13 @@
+class ETL
+
+  def self.transform(old)
+    results = {}
+    old.each do |point_val, letters|
+      letters.each do |letter|
+        results[letter.downcase] = point_val
+      end
+    end
+    results
+  end
+
+end
