a415ddbea71ad6300287fc2c58b5f15be64c2a28
Rootul Patel <rootulp@gmail.com>
2015-01-30 14:13:19 -0500

    solve bob

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -6,9 +6,15 @@
 var Bob = function() {};
 
 Bob.prototype.hey = function(input) {
-//
-// YOUR CODE GOES HERE
-//
+    if (!input.trim()) {
+        return "Fine. Be that way!";
+    } else if (input === input.toUpperCase() && input !== input.toLowerCase()){
+        return 'Whoa, chill out!';
+    } else if (input.slice(-1) === '?'){
+        return 'Sure.';
+    } else {
+        return 'Whatever.';
+    }
 };
 
 module.exports = Bob;
