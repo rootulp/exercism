af8b8da1549091437a9bd21899c6c856fac46bcc
Rootul Patel <rootulp@gmail.com>
2015-03-10 14:24:59 -0400

    initial solution to palindrome products

--- ruby/palindrome-products/Palindromes.rb
+++ ruby/palindrome-products/Palindromes.rb
@@ -1,18 +1,70 @@
+require 'set'
+
 class Palindromes
-  attr_accessor :max, :min, :factors
-  
-  DEFAULTS = {min_factor: 1}
+  attr_accessor :min, :max, :factors
+
+  DEFAULT_MIN = 1
   def initialize(args)
+    @min = args[:min_factor] || DEFAULT_MIN
     @max = args[:max_factor]
-    @min = args[:min_factor]
+    @factors = Set.new
   end
 
   def generate
     (min..max).each do |x|
       (min..max).each do |y|
-         factors << [x, y] if factors?(x, y)
+        if factors?(x, y)
+          if x <= y
+            factors << [x, y]
+          else
+            factors << [y, x]
+          end
+        end
       end
     end
   end
 
+  def factors?(x, y)
+    result = (x * y).to_s
+    result == result.reverse
+  end
+
+  def largest
+    build_subset(find_largest)
+  end
+
+  def smallest
+    build_subset(find_smallest)
+  end
+
+  def find_largest
+    factor = factors.max_by {|x| x.reduce(:*)}
+    factor.reduce(:*)
+  end
+
+  def find_smallest
+    factor = factors.min_by {|x| x.reduce(:*)}
+    factor.reduce(:*)
+  end
+
+  def build_subset(val)
+    Subset.new(val, find_subset_factors(val))
+  end
+
+  def find_subset_factors(val)
+    subset_factors = []
+    factors.each do |factor|
+      subset_factors << factor if factor[0] * factor[1] == val
+    end
+    subset_factors
+  end
+
+end
+
+class Subset
+  attr_reader :value, :factors
+  def initialize(value, factors)
+    @value = value
+    @factors = factors
+  end
 end
--- ruby/palindrome-products/palindrome_products_test.rb
+++ ruby/palindrome-products/palindrome_products_test.rb
@@ -11,7 +11,6 @@ class PalindromesTest < MiniTest::Unit::TestCase
   end
 
   def test_largest_palindrome_from_double_digit_factors
-    skip
     palindromes = Palindromes.new(max_factor: 99, min_factor: 10)
     palindromes.generate
     largest = palindromes.largest
@@ -20,7 +19,6 @@ class PalindromesTest < MiniTest::Unit::TestCase
   end
 
   def test_smallest_palindrome_from_double_digit_factors
-    skip
     palindromes = Palindromes.new(max_factor: 99, min_factor: 10)
     palindromes.generate
     smallest = palindromes.smallest
@@ -29,7 +27,6 @@ class PalindromesTest < MiniTest::Unit::TestCase
   end
 
   def test_largest_palindrome_from_triple_digit_factors
-    skip
     palindromes = Palindromes.new(max_factor: 999, min_factor: 100)
     palindromes.generate
     largest = palindromes.largest
@@ -38,7 +35,6 @@ class PalindromesTest < MiniTest::Unit::TestCase
   end
 
   def test_smallest_palindrome_from_triple_digit_factors
-    skip
     palindromes = Palindromes.new(max_factor: 999, min_factor: 100)
     palindromes.generate
     smallest = palindromes.smallest
