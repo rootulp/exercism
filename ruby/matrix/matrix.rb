89d4b0543ffefdaafb53cf40bc8db5745afd33e4
Rootul Patel <rootulp@gmail.com>
2015-03-10 14:41:27 -0400

    initial solution to matrix

---
+++ ruby/matrix/matrix.rb
@@ -0,0 +1,16 @@
+class Matrix
+
+  attr_reader :rows, :columns
+  def initialize(string)
+    @rows = build_rows(string)
+    @columns = rows.transpose
+  end
+
+  private
+
+  def build_rows(string)
+    string.split("\n").map do |row|
+      row.split(" ").map(&:to_i)
+    end
+  end
+end
