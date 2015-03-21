7f065172c0aff0c8c264f9037627301ab8bd7d73
Rootul Patel <rootulp@gmail.com>
2015-03-20 22:06:47 -0400

    pig latin boilerplate

---
+++ ruby/pig-latin/pig_latin.rb
@@ -0,0 +1,14 @@
+class PigLatin
+  require 'set'
+
+  ALPHA = ('a'..'z').to_set
+  VOWELS = ['a', 'e', 'i', 'o', 'u'].to_set
+  CONSONANTS = ALPHA - VOWELS
+
+  def self.translate
+
+  end
+
+end
+
+
