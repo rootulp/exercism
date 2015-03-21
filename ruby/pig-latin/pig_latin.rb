f3c0c7f859166ff87b9cd86ef5058e9607ccacfc
Rootul Patel <rootulp@gmail.com>
2015-03-21 19:27:03 -0400

    solve pig latin
    used a lot of if else checks... probably a smoother way to do this with
    regex

--- ruby/pig-latin/pig_latin.rb
+++ ruby/pig-latin/pig_latin.rb
@@ -5,10 +5,29 @@ class PigLatin
   VOWELS = ['a', 'e', 'i', 'o', 'u'].to_set
   CONSONANTS = ALPHA - VOWELS
 
-  def self.translate
-
+  def self.translate(phrase)
+    phrase.split.map {|word| self.translate_word(word)}.join(' ')
   end
 
+  def self.translate_word(word)
+    if VOWELS.include?(word[0]) ||
+         word.start_with?('yt') ||
+         word.start_with?('xr')
+      word + 'ay'
+
+    elsif word.start_with?('squ') ||
+          word.start_with?('sch') ||
+          word.start_with?('thr')
+      word[3..-1] + word[0..2] + 'ay'
+
+    elsif (CONSONANTS.include?(word[0])  &&
+           CONSONANTS.include?(word[1])) ||
+           word.start_with?('qu')
+      word[2..-1] + word[0..1] + 'ay'
+
+    elsif CONSONANTS.include?(word[0])
+      word[1..-1] + word[0..0] + 'ay'
+
+    end
+  end
 end
-
-
