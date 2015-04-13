925ff5a59efe970a38ac658769a2f61beba258fa
Rootul Patel <rootulp@gmail.com>
2015-04-13 16:02:49 -0400

    Solve grade-school
    Not sure if get and set for roster is the cleanest way to do this.

---
+++ javascript/grade-school/grade-school.js
@@ -0,0 +1,34 @@
+var School = function() {
+  'use strict';
+
+  var roster = {};
+
+  function getRoster() {
+    return roster;
+  }
+
+  function setRoster(name, grade) {
+    if (roster[grade]) {
+      roster[grade].push(name);
+      roster[grade].sort();
+    } else {
+      roster[grade] = [name];
+    }
+  }
+
+  function grade(grade) {
+    if (roster[grade]) {
+      return roster[grade];
+    } else {
+      return [];
+    }
+  }
+
+  return {
+    roster: getRoster,
+    add: setRoster,
+    grade: grade
+  }
+}
+
+module.exports = School
