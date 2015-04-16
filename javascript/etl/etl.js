c673c06b83734e7490bba0b99489b0b2cdf11b22
Rootul Patel <rootulp@gmail.com>
2015-04-16 12:49:19 -0400

    Solve etl
    Iterate through legacy_data properties. Pass key, val to add().
    add() iterates through char_arr and appends char, val to new_data.

---
+++ javascript/etl/etl.js
@@ -0,0 +1,19 @@
+function transform(legacy_data) {
+  var new_data = {}
+
+  for (var key in legacy_data) {
+    if (legacy_data.hasOwnProperty(key)) {
+      add(Number(key), legacy_data[key])
+    }
+  }
+
+  function add(val, char_arr) {
+    for (var i = 0; i < char_arr.length; i++) {
+      new_data[char_arr[i].toLowerCase()] = val
+    }
+  }
+
+  return new_data;
+}
+
+module.exports = transform
