d0aba5d9cc2c2be856d33868913f3ca2c72af1e5
Rootul Patel <rootulp@gmail.com>
2015-04-18 17:14:52 -0400

    Solve triangle
    Translated my previous ruby solution.

---
+++ javascript/triangle/triangle.js
@@ -0,0 +1,35 @@
+function Triangle(sideA, sideB, sideC) {
+  'use strict';
+
+  this.kind = function() {
+    if (this.error()) {
+      throw Error;
+    } else if (this.equilateral()) {
+      return 'equilateral';
+    } else if (this.isosceles()) {
+      return 'isosceles';
+    } else {
+      return 'scalene';
+    }
+  }
+
+  this.equilateral = function() {
+    return sideA === sideB && sideB === sideC
+  }
+
+  this.isosceles = function() {
+    return sideA === sideB || sideB === sideC || sideA === sideC
+  }
+
+  this.error = function() {
+    return this.inequality(sideA, sideB, sideC) ||
+           this.inequality(sideB, sideC, sideA) ||
+           this.inequality(sideA, sideC, sideB)
+  }
+
+  this.inequality = function(x, y, z) {
+    return x + y <= z
+  }
+}
+
+module.exports = Triangle
