89d2b3f18f9e3da2fa4f6e9f5f151e64c7b93931
Rootul Patel <rootulp@gmail.com>
2015-04-23 12:42:47 -0400

    Solve roman-numerals

---
+++ javascript/roman-numerals/roman-numerals.js
@@ -0,0 +1,33 @@
+function toRoman(decimal) {
+  'use strict';
+
+  var result = "";
+  var numerals = [
+    {decimal:    1, roman: 'I' },
+    {decimal:    4, roman: 'IV'},
+    {decimal:    5, roman: 'V' },
+    {decimal:    9, roman: 'IX'},
+    {decimal:   10, roman: 'X' },
+    {decimal:   40, roman: 'XL'},
+    {decimal:   50, roman: 'L' },
+    {decimal:   90, roman: 'XC'},
+    {decimal:  100, roman: 'C' },
+    {decimal:  400, roman: 'CD'},
+    {decimal:  500, roman: 'D' },
+    {decimal:  900, roman: 'CM'},
+    {decimal: 1000, roman: 'M' }
+  ];
+
+  for (var i = numerals.length - 1; i >= 0; i--) {
+    var pairing = numerals[i];
+    while (decimal >= pairing.decimal) {
+      decimal -= pairing.decimal;
+      result += pairing.roman;
+    }
+  }
+
+  return result;
+
+}
+
+module.exports = toRoman
---
+++ javascript/roman-numerals/roman-numerals_test.spec.js
@@ -0,0 +1,75 @@
+var toRoman = require('./roman-numerals');
+
+describe("toRoman()", function() {
+  it("converts 1", function() {
+    expect(toRoman(1)).toEqual('I');
+  });
+
+  it("converts 2", function() {
+    expect(toRoman(2)).toEqual('II');
+  });
+
+  it("converts 3", function() {
+    expect(toRoman(3)).toEqual('III');
+  });
+
+  it("converts 4", function() {
+    expect(toRoman(4)).toEqual('IV');
+  });
+
+  it("converts 5", function() {
+    expect(toRoman(5)).toEqual('V');
+  });
+
+  it("converts 6", function() {
+    expect(toRoman(6)).toEqual('VI');
+  });
+
+  it("converts 9", function() {
+    expect(toRoman(9)).toEqual('IX');
+  });
+
+  it("converts 27", function() {
+    expect(toRoman(27)).toEqual('XXVII');
+  });
+
+  it("converts 48", function() {
+    expect(toRoman(48)).toEqual('XLVIII');
+  });
+
+  it("converts 59", function() {
+    expect(toRoman(59)).toEqual('LIX');
+  });
+
+  it("converts 93", function() {
+    expect(toRoman(93)).toEqual('XCIII');
+  });
+
+  it("converts 141", function() {
+    expect(toRoman(141)).toEqual('CXLI');
+  });
+
+  it("converts 163", function() {
+    expect(toRoman(163)).toEqual('CLXIII');
+  });
+
+  it("converts 402", function() {
+    expect(toRoman(402)).toEqual('CDII');
+  });
+
+  it("converts 575", function() {
+    expect(toRoman(575)).toEqual('DLXXV');
+  });
+
+  it("converts 911", function() {
+    expect(toRoman(911)).toEqual('CMXI');
+  });
+
+  it("converts 1024", function() {
+    expect(toRoman(1024)).toEqual('MXXIV');
+  });
+
+  it("converts 3000", function() {
+    expect(toRoman(3000)).toEqual('MMM');
+  });
+});
