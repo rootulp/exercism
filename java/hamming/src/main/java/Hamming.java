fb2a59b2bf03a369ccff87ee59c297bdd09e5543
Rootul Patel <rootulp@gmail.com>
2015-05-31 23:18:37 -0400

    solve hamming

---
+++ java/hamming/src/main/java/Hamming.java
@@ -0,0 +1,16 @@
+public class Hamming {
+
+  public static Integer compute(String a, String b) {
+    if (a.length() != b.length()) {
+      throw new IllegalArgumentException("strands are unequal in length");
+    }
+
+    int total = 0;
+    for (int i = 0, n = a.length(); i < n; i++) {
+      if (a.charAt(i) != b.charAt(i)) { total += 1; }
+    }
+    return total;
+
+  }
+
+}
---
+++ java/hamming/src/test/java/HammingTest.java
@@ -0,0 +1,53 @@
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class HammingTest {
+
+    @Test
+    public void testNoDifferenceBetweenIdenticalStrands() {
+        assertThat(Hamming.compute("A", "A"), is(0));
+    }
+
+    @Test
+    public void testCompleteHammingDistanceOfForSingleNucleotideStrand() {
+        assertThat(Hamming.compute("A", "G"), is(1));
+    }
+
+    @Test
+    public void testCompleteHammingDistanceForSmallStrand() {
+        assertThat(Hamming.compute("AG", "CT"), is(2));
+    }
+
+    @Test
+    public void testSmallHammingDistance() {
+        assertThat(Hamming.compute("AT", "CT"), is(1));
+    }
+
+    @Test
+    public void testSmallHammingDistanceInLongerStrand() {
+        assertThat(Hamming.compute("GGACG", "GGTCG"), is(1));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testValidatesFirstStrandNotLonger() {
+        Hamming.compute("AAAG", "AAA");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testValidatesOtherStrandNotLonger() {
+        Hamming.compute("AAA", "AAAG");
+    }
+
+    @Test
+    public void testLargeHammingDistance() {
+        assertThat(Hamming.compute("GATACA", "GCATAA"), is(4));
+    }
+
+    @Test
+    public void testHammingDistanceInVeryLongStrand() {
+        assertThat(Hamming.compute("GGACGGATTCTG", "AGGACGGATTCT"), is(9));
+    }
+
+}
