56b24b33bc4fbc0d280fc5fa5ad27acfc1bfd975
Rootul Patel <rootulp@gmail.com>
2015-06-02 17:28:46 -0400

    Solve grade-school
    Don't like how solution returns db to client. Seems unsafe.

---
+++ java/grade-school/src/main/java/School.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class School {
+
+  private Map<Integer, List<String>> db = new HashMap<Integer, List<String>>();
+
+  public Map<Integer, List<String>> db() {
+    return db;
+  }
+
+  public void add(String name, Integer grade) {
+    List<String> temp = grade(grade);
+    temp.add(name);
+    db.put(grade, temp);
+  }
+
+  public List<String> grade(Integer grade) {
+    if (db.containsKey(grade)) {
+      return db.get(grade);
+    }
+    return new ArrayList<String>();
+  }
+
+  public Map<Integer, List<String>> sort() {
+    for (Integer grade : db.keySet()) {
+      List<String> curr = db.get(grade);
+      Collections.sort(curr);
+    }
+    return db;
+  }
+
+}
