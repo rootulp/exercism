b2c441c4e09f069f176cb58731357a14f5b0ddc9
Rootul Patel <rootulp@gmail.com>
2015-06-02 16:19:04 -0400

    solve phone number

---
+++ java/phone-number/src/main/java/PhoneNumber.java
@@ -0,0 +1,37 @@
+public class PhoneNumber {
+
+  private static final String invalid = "0000000000";
+  private final String number;
+
+  public PhoneNumber(String dirtyNum) {
+    this.number = clean(dirtyNum);
+  }
+
+  private String clean(String dirtyNum) {
+    String num = dirtyNum.replaceAll("[^0-9]","");
+    if (num.length() == 10) { return num; }
+    if (num.length() == 11 && num.charAt(0) == '1') { return num.substring(1); }
+    return invalid;
+  }
+
+  public String pretty() {
+    return "(" + getAreaCode() + ") " + getPrefixNum() + "-" + getLineNum();
+  }
+
+  public String getNumber() {
+    return this.number;
+  }
+
+  public String getAreaCode() {
+    return this.number.substring(0, 3);
+  }
+
+  private String getPrefixNum() {
+    return this.number.substring(3, 6);
+  }
+
+  private String getLineNum() {
+    return this.number.substring(6);
+  }
+
+}
