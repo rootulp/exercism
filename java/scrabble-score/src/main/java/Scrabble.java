34b208b2579e89803bfa3a63b5ec27989ae63df0
Rootul Patel <rootulp@gmail.com>
2015-06-03 15:06:38 -0400

    Solve scrabble score
    Used guava ImmutableMap over mutable hashmap for letter value constants

---
+++ java/scrabble-score/src/main/java/Scrabble.java
@@ -0,0 +1,31 @@
+import java.util.Map;
+import com.google.common.collect.*;
+
+public class Scrabble {
+
+  private final String word;
+
+  private static final ImmutableMap<Character, Integer> LETTER_VALS =
+    new ImmutableMap.Builder<Character, Integer>()
+      .put('A', 1).put('B', 3).put('C', 3).put('D', 2).put('E', 1)
+      .put('F', 4).put('G', 2).put('H', 4).put('I', 1).put('J', 8)
+      .put('K', 5).put('L', 1).put('M', 3).put('N', 1).put('O', 1)
+      .put('P', 3).put('Q',10).put('R', 1).put('S', 1).put('T', 1)
+      .put('U', 1).put('V', 4).put('W', 4).put('X', 8).put('Y', 4)
+      .put('Z',10).build();
+
+  public Scrabble(String word) {
+    this.word = word;
+  }
+
+  public int getScore() {
+    if (word == null || word.trim().isEmpty()) { return 0; }
+
+    int score = 0;
+    for (char c : word.toUpperCase().toCharArray()) {
+      score += LETTER_VALS.get(c);
+    }
+    return score;
+  }
+
+}
---
+++ java/scrabble-score/src/test/java/ScrabbleScoreTest.java
@@ -0,0 +1,42 @@
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(Parameterized.class)
+public class ScrabbleScoreTest {
+
+    private String input;
+    private int expectedOutput;
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+                {"", 0},
+                {" \t\n", 0},
+                {null, 0},
+                {"a", 1},
+                {"f", 4},
+                {"street", 6},
+                {"quirky", 22},
+                {"MULTIBILLIONAIRE", 20},
+                {"alacrity", 13},
+        });
+    }
+
+    public ScrabbleScoreTest(String input, int expectedOutput) {
+        this.input = input;
+        this.expectedOutput = expectedOutput;
+    }
+
+    @Test
+    public void test() {
+        Scrabble scrabble = new Scrabble(input);
+
+        assertEquals(expectedOutput, scrabble.getScore());
+    }
+}
