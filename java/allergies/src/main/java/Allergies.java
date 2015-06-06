9959cbf60ff85fb10d048c7a00754fa414187c07
Rootul Patel <rootulp@gmail.com>
2015-06-06 15:32:03 -0400

    Solve allergies
    Allergen Enum was easy to work with

---
+++ java/allergies/src/main/java/Allergies.java
@@ -0,0 +1,27 @@
+import java.util.List;
+import java.util.Arrays;
+import java.util.ArrayList;
+
+public class Allergies {
+
+  private final int score;
+
+  public Allergies(int score) {
+    this.score = score;
+  }
+
+  public boolean isAllergicTo(Allergen a) {
+    return getList().contains(a);
+  }
+
+  public List<Allergen> getList() {
+    List<Allergen> results = new ArrayList<Allergen>();
+
+    for (Allergen a : Allergen.values()) {
+      if ((score & a.getScore()) != 0) { results.add(a); }
+    }
+
+    return results;
+  }
+
+}
