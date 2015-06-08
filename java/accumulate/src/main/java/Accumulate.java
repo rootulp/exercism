aa37e5d8fb7eb60d2c7da1914ee10f3bff270b1c
Rootul Patel <rootulp@gmail.com>
2015-06-08 12:40:40 -0400

    Solve accumulate
    Used java.util.function.Function

---
+++ java/accumulate/src/main/java/Accumulate.java
@@ -0,0 +1,15 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.function.Function;
+
+public class Accumulate {
+  public static <T> List<T> accumulate(List<T> list, Function<T, T> f) {
+    List<T> results = new ArrayList<>();
+
+    for (T item : list) {
+      results.add(f.apply(item));
+    }
+
+    return results;
+  }
+}
