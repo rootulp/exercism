001bd02b3026365e76d79fa521c1ffe588b159cb
Rootul Patel <rootulp@gmail.com>
2015-06-14 12:57:42 -0400

    Solve trinary
    Used reversed loop

---
+++ java/trinary/src/main/java/Trinary.java
@@ -0,0 +1,22 @@
+public class Trinary {
+
+  private static int RADIX = 3;
+
+  public static int toDecimal(String input) {
+    if (!input.matches("[012]+")) { return 0; }
+    return computeTrinary(input);
+
+  }
+
+  private static int computeTrinary(String input) {
+    int result = 0;
+    int pwr = 1;
+
+    for (int i = input.length() - 1; i >= 0; i--, pwr *= RADIX) {
+      int currDigit = Character.digit(input.charAt(i), RADIX);
+      result += currDigit * pwr;
+    }
+
+    return result;
+  }
+}
