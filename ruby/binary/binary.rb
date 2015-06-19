d2f60542fe60ad7a9dff2493e58c9fdd7ad203dd
Rootul Patel <rootulp@gmail.com>
2015-06-18 23:53:49 -0400

    refactor binary solution

--- ruby/binary/binary.rb
+++ ruby/binary/binary.rb
@@ -1,23 +1,23 @@
 class Binary
 
+  attr_reader :binary
   def initialize(binary)
     @binary = binary
   end
 
   def to_decimal
-    return 0 unless valid?
-
-    total = 0
-    @binary.split(//).reverse.each_with_index do |one_or_zero, index|
-      total += 2 ** index if one_or_zero == "1"
-    end
-    total
+    return 0 if invalid?
+    convert_to_decimal
   end
 
-  def valid?
-    regex = /[^01]/
-    return false if @binary.match(regex)
-    true
+  private
+
+  def invalid?
+    binary.match(/[^01]/)
+  end
+
+  def convert_to_decimal
+    binary.chars.reverse.each_with_index.map { |x, i| x == "0" ? 0 : 2 ** i }.reduce(:+)
   end
 
 end
