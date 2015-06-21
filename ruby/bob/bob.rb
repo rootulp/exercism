2a0cf845a9d6e330d918034c55b5eeff2f6339c6
Rootul Patel <rootulp@gmail.com>
2015-06-20 21:15:13 -0400

    Refactor bob
    Remove unnecessary explicit return statements

--- ruby/bob/bob.rb
+++ ruby/bob/bob.rb
@@ -2,13 +2,13 @@ class Bob
 
   def hey(phrase)
     if phrase.strip.empty?
-      return "Fine. Be that way!"
+      "Fine. Be that way!"
     elsif phrase == phrase.upcase and phrase != phrase.downcase
-      return "Whoa, chill out!"
+      "Whoa, chill out!"
     elsif phrase.end_with?("?")
-      return "Sure."
+      "Sure."
     else
-      return 'Whatever.'
+      'Whatever.'
     end
   end
 
