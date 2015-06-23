8fc0e8ab253f538fb7b9fc9f88912ab70d973fb9
Rootul Patel <rootulp@gmail.com>
2015-06-22 23:57:04 -0400

    progress on circular buffer

---
+++ javascript/circular-buffer/circular-buffer.js
@@ -0,0 +1,60 @@
+function CircularBuffer(maxSize) {
+
+  var readIndex = 0;
+  var writeIndex = 0;
+  var buffer = new Array(maxSize);
+
+  function read() {
+    if (!buffer[readIndex]) { throw new BufferEmptyException(); }
+    var data = buffer[readIndex];
+    buffer[readIndex] = null;
+    readIndex = increment(readIndex);
+    return data;
+  }
+
+  function write(data) {
+    buffer[writeIndex] = data;
+    writeIndex = increment(writeIndex);
+  }
+
+  function clear() {
+    readIndex = 0;
+    writeIndex = 0;
+    buffer = new Array(maxSize);
+  }
+
+  function increment(x) {
+    x = (x + 1) % maxSize;
+  }
+
+  return {
+    read: read,
+    write: write,
+    clear: clear
+  }
+
+};
+
+function BufferEmptyException() {
+  this.name = "BufferEmptyException";
+  this.message = "Buffer is empty.";
+};
+
+function BufferFullException() {
+  this.name = "BufferFullException";
+  this.message = "Buffer is full.";
+};
+
+module.exports = {
+  circularBuffer: function(capacity) {
+    return new CircularBuffer(capacity);
+  },
+
+  bufferEmptyException: function() {
+    return new BufferEmptyException();
+  },
+
+  bufferFullException: function() {
+    return new BufferFullException();
+  }
+};
