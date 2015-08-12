e33c69339a4de8419a5d64dba1a1868aa4c583c3
Rootul Patel <rootulp@gmail.com>
2015-08-12 00:21:29 -0400

    Solve hello-world

---
+++ javascript/hello-world/hello-world.js
@@ -0,0 +1,17 @@
+//
+// This is only a SKELETON file for the 'Hello World' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+// Make sure to look at test.script.js--that should give you some hints about what is
+// expected here.
+
+var HelloWorld = function() {};
+
+HelloWorld.prototype.hello = function(input) {
+  if (input !== '') {
+    return 'Hello, ' + input + '!';
+  } else {
+    return 'Hello, world!';
+  }
+};
+
+module.exports = HelloWorld;
