1dfbd7a2daa2b4845d1afe849d600882251f834f
Rootul Patel <rootulp@gmail.com>
2015-09-01 17:31:20 -0700

    exercism fetched new javascript tests

---
+++ javascript/hello-world/hello_world.js
@@ -0,0 +1,15 @@
+//
+// This is only a SKELETON file for the 'Hello World' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+// Make sure to look at test.script.js--that should give you some hints about what is
+// expected here.
+
+var HelloWorld = function() {};
+
+HelloWorld.prototype.hello = function(input) {
+//
+// YOUR CODE GOES HERE
+//
+};
+
+module.exports = HelloWorld;
