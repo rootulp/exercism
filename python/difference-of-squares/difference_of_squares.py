40e4fc775448102de6b4ff093bf4ca55ac829f76
Rootul Patel <rootulp@gmail.com>
2015-11-28 18:04:03 -0500

    dos style fixes

--- python/difference-of-squares/difference_of_squares.py
+++ python/difference-of-squares/difference_of_squares.py
@@ -1,8 +1,10 @@
 def square_of_sum(x):
     return sum(range(x + 1)) ** 2
 
+
 def sum_of_squares(x):
     return sum(y ** 2 for y in range(x + 1))
+
 
 def difference(x):
     return square_of_sum(x) - sum_of_squares(x)
