9d2fe97d403d933844d04784894574e188f6976a
Rootul Patel <rootulp@gmail.com>
2015-11-28 18:03:30 -0500

    bob style fixes

--- python/bob/bob.py
+++ python/bob/bob.py
@@ -1,11 +1,11 @@
 def hey(phrase):
-  phrase = phrase.strip()
+    phrase = phrase.strip()
 
-  if not phrase:
-    return "Fine. Be that way!"
-  elif phrase.isupper():
-    return "Whoa, chill out!"
-  elif phrase.endswith("?"):
-    return "Sure."
-  else:
-    return 'Whatever.'
+    if not phrase:
+        return "Fine. Be that way!"
+    elif phrase.isupper():
+        return "Whoa, chill out!"
+    elif phrase.endswith("?"):
+        return "Sure."
+    else:
+        return 'Whatever.'
