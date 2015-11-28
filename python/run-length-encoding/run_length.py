5cd310656fcd867c83abf50addb1c2887a2ed6d4
Rootul Patel <rootulp@gmail.com>
2015-11-28 15:30:13 -0500

    solve run length

---
+++ python/run-length-encoding/run_length.py
@@ -0,0 +1,16 @@
+from itertools import groupby
+import re
+
+def encode(s):
+    return ''.join(map(lambda g: helper(g), [list(g) for k, g in groupby(s)]))
+
+def helper(g):
+    return g[0] if len(g) == 1 else str(len(g)) + g[0]
+
+def decode(s):
+    groups = re.findall('(\d*\D{1})', s)
+    pairs = map(lambda g: [re.match('\d*', g).group(), g[-1]], groups)
+
+    # Fix hardcoded 0 and 1 indices
+    # Also change name of x variable
+    return ''.join(map(lambda x: int(x[0]) * x[1] if x[0].isdigit() else x[1], pairs))
