f49c55f3444d1f315ab5ec0c3369d3f613337678
Rootul Patel <rootulp@gmail.com>
2015-11-28 18:14:58 -0500

    wordcount style fixes

--- python/word-count/wordcount.py
+++ python/word-count/wordcount.py
@@ -1,19 +1,14 @@
-import string
-
 def word_count(phrase):
 
-  # Remove whitespace and \n and toss into array
-  words = phrase.strip().replace("\n", " ").split(" ")
+    # Remove whitespace and \n and toss into array
+    words = phrase.strip().replace("\n", " ").split(" ")
 
-  # Remove empty strings
-  words = filter(None, words)
+    # Remove empty strings
+    words = filter(None, words)
 
-  # Initialize new dict for count of occurences
-  count = {}
-  for word in words:
-    if word not in count:
-      count[word] = 1
-    else:
-      count[word] += 1
+    # Initialize new dict for count of occurences
+    count = {}
+    for word in words:
+        count[word] = 1 if word not in count else count[word] + 1
 
-  return count
+    return count
