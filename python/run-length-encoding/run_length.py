fa674fcfbdcbf4b36673178cc8c966ba3c990ae8
Rootul Patel <rootulp@gmail.com>
2015-11-28 18:13:08 -0500

    run length style fixes

--- python/run-length-encoding/run_length.py
+++ python/run-length-encoding/run_length.py
@@ -1,11 +1,14 @@
 from itertools import groupby
 import re
 
+
 def encode(s):
     return ''.join(map(lambda g: helper(g), [list(g) for k, g in groupby(s)]))
 
+
 def helper(g):
     return g[0] if len(g) == 1 else str(len(g)) + g[0]
+
 
 def decode(s):
     groups = re.findall('(\d*\D{1})', s)
@@ -13,4 +16,5 @@ def decode(s):
 
     # Fix hardcoded 0 and 1 indices
     # Also change name of x variable
-    return ''.join(map(lambda x: int(x[0]) * x[1] if x[0].isdigit() else x[1], pairs))
+    return ''.join(map(lambda x: int(x[0]) * x[1] if x[0].isdigit() else x[1],
+                       pairs))
