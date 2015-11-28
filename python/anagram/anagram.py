d9ce67cb3aa75ef52e353fdfd568605ce307a936
Rootul Patel <rootulp@gmail.com>
2015-11-28 18:02:34 -0500

    anagram style fixes

--- python/anagram/anagram.py
+++ python/anagram/anagram.py
@@ -1,8 +1,10 @@
 def detect_anagrams(w, poss):
-    return filter(lambda p: anagram(w, p), poss);
+    return filter(lambda p: anagram(w, p), poss)
+
 
 def anagram(w, p):
     return w.lower() != p.lower() and sort(w.lower()) == sort(p.lower())
 
+
 def sort(word):
-    return sorted(list(word.lower()));
+    return sorted(list(word.lower()))
