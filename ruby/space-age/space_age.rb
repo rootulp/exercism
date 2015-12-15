6beb0f5a1df8461788deeef5c9c4075f5a117cd6
Rootul Patel <rootulp@gmail.com>
2015-12-15 14:45:32 -0500

    refactor space age

--- ruby/space-age/space_age.rb
+++ ruby/space-age/space_age.rb
@@ -1,54 +1,34 @@
 class SpaceAge
 
+  SECONDS_IN_YEAR = 31557600.0
+  ORBITAL_PERIODS = {
+    'mercury' => 0.2408467,
+    'venus' => 0.61519726,
+    'earth' => 1,
+    'mars' => 1.8808158,
+    'jupiter' => 11.862615,
+    'saturn' => 29.447498,
+    'uranus' => 84.016846,
+    'neptune' => 164.79132
+  }
+
   attr_reader :seconds
-
-  EARTH_ORBITAL = 31557600.0
-  EARTH_TO_MERCURY = 0.2408467
-  EARTH_TO_VENUS = 0.61519726
-  EARTH_TO_MARS = 1.8808158
-  EARTH_TO_JUPITER = 11.862615
-  EARTH_TO_SATURN = 29.447498
-  EARTH_TO_URANUS = 84.016846
-  EARTH_TO_NEPTUNE = 164.79132
-
   def initialize(seconds)
     @seconds = seconds
   end
 
   def to_earth
-    seconds / EARTH_ORBITAL
+    seconds / SECONDS_IN_YEAR
   end
 
-  def on_earth
-    to_earth.round(2)
+  def on_planet(planet)
+    (to_earth / ORBITAL_PERIODS[planet]).round(2)
   end
 
-  def on_mercury
-    (to_earth / EARTH_TO_MERCURY).round(2)
-  end
-
-  def on_venus
-    (to_earth / EARTH_TO_VENUS).round(2)
-  end
-
-  def on_mars
-    (to_earth / EARTH_TO_MARS).round(2)
-  end
-
-  def on_jupiter
-    (to_earth / EARTH_TO_JUPITER).round(2)
-  end
-
-  def on_saturn
-    (to_earth / EARTH_TO_SATURN).round(2)
-  end
-
-  def on_uranus
-    (to_earth / EARTH_TO_URANUS).round(2)
-  end
-
-  def on_neptune
-    (to_earth / EARTH_TO_NEPTUNE).round(2)
+  ORBITAL_PERIODS.keys.each do |planet|
+    define_method :"on_#{planet}" do
+      on_planet(planet)
+    end
   end
 
 end
