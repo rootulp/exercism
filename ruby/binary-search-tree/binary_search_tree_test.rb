14937d42b109e15177732a4ad16d6d772378f06e
Rootul Patel <rootulp@gmail.com>
2016-01-28 17:16:28 -0500

    refactor bst and style fixes

--- ruby/binary-search-tree/binary_search_tree_test.rb
+++ ruby/binary-search-tree/binary_search_tree_test.rb
@@ -52,26 +52,22 @@ class BstTest < MiniTest::Unit::TestCase
   end
 
   def test_iteratbing_one_element
-    skip
     assert_equal [4], record_all_data(Bst.new 4)
   end
 
   def test_iterating_over_smaller_element
-    skip
     four = Bst.new 4
     four.insert 2
     assert_equal [2, 4], record_all_data(four)
   end
 
   def test_iterating_over_larger_element
-    skip
     four = Bst.new 4
     four.insert 5
     assert_equal [4, 5], record_all_data(four)
   end
 
   def test_iterating_over_complex_tree
-    skip
     four = Bst.new 4
     four.insert 2
     four.insert 1
--- ruby/binary-search-tree/bst.rb
+++ ruby/binary-search-tree/bst.rb
@@ -1,45 +1,26 @@
-class Node
-
-  attr_reader :data
-  attr_accessor :left, :right
-  def initialize(val)
-    @data = val
-  end
-
-end
-
+# Binary Search Tree
 class Bst
+  attr_reader :data, :left, :right
 
-  attr_reader :root
-  def initialize(val)
-    @root = Node.new(val)
-  end
-
-  def data
-    root.data
-  end
-
-  def left
-    root.left
-  end
-
-  def right
-    root.right
+  def initialize(data)
+    @data = data
   end
 
   def insert(val)
-    insert_value(root, val)
+    val <= data ? insert_left(val) : insert_right(val)
   end
 
-  def insert_value(node, val)
-    if node.nil?
-      node = Node.new(val)
-    elsif val > node.data
-      node.right = insert_value(node.right, val)
-    else
-      node.left = insert_value(node.left, val)
-    end
-    node
+  def insert_left(data)
+    left ? left.insert(data) : @left = Bst.new(data)
   end
 
+  def insert_right(data)
+    right ? right.insert(data) : @right = Bst.new(data)
+  end
+
+  def each(&block)
+    left.each(&block) if left
+    yield data
+    right.each(&block) if right
+  end
 end
