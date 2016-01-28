b22a0d20484dfd34d7a83b4efa9e159b9f5db9ab
Rootul Patel <rootulp@gmail.com>
2016-01-28 13:43:24 -0500

    clarify method names and style fixes

--- ruby/atbash-cipher/atbash.rb
+++ ruby/atbash-cipher/atbash.rb
@@ -1,24 +1,31 @@
+# Atbash Cipher
 class Atbash
-  ENCODING = Hash[('a'..'z').zip('a'..'z').to_a.reverse]
+  ENCODING = Hash[('a'..'z').zip(('a'..'z').to_a.reverse)]
 
   def self.encode(str)
-    split_5(encode!(format(str)))
+    split_every_five(encode_word(strip_non_alphanumeric(str.downcase)))
   end
 
-  def self.encode!(str)
-    str.chars.map { |char| is_numeric?(char) ? char : ENCODING[char] }.join('')
+  def self.encode_word(word)
+    word.chars.map { |char| encode_char(char) }.join
   end
 
-  def self.format(str)
-    str.downcase.gsub(/[^a-z0-9]/i, '')
+  def self.encode_char(char)
+    alpha?(char) ? ENCODING[char] : char
   end
 
-  def self.split_5(str)
+  def self.strip_non_alphanumeric(str)
+    str.gsub(/[^a-z0-9]/i, '')
+  end
+
+  def self.split_every_five(str)
     str.scan(/.{5}|.+/).join(' ')
   end
 
-  # Taken from Rosetta code
-  def self.is_numeric?(s)
-    !!Float(s) rescue false
+  def self.alpha?(char)
+    char.match(/[[:alpha:]]/)
   end
+
+  private_class_method :encode_word, :encode_char, :strip_non_alphanumeric,
+                       :split_every_five, :alpha?
 end
