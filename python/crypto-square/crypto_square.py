13cc86486f60db55ca2bf102c4c25d4429a5ecec
Rootul Patel <rootulp@gmail.com>
2016-02-02 18:50:08 -0500

    solve crypto square

---
+++ python/crypto-square/crypto_square.py
@@ -0,0 +1,34 @@
+import string
+import math
+
+
+class CryptoSquare:
+
+    @classmethod
+    def encode(cls, msg):
+        if len(cls.normalize(msg)) == 0:
+            return ''
+        return ' '.join(cls.transpose_square(cls.squarify(cls.normalize(msg))))
+
+    @classmethod
+    def squarify(cls, msg):
+        return [msg[i:i + cls.square_size(len(msg))] for i in range(0,
+                len(msg), cls.square_size(len(msg)))]
+
+    @staticmethod
+    def transpose_square(square):
+        return map(lambda *row: ''.join([ch if ch else '' for ch in row]),
+                   *square)
+
+    @staticmethod
+    def normalize(msg):
+        return ''.join(ch.lower() for ch in msg if ch not in
+                       set(string.punctuation + ' '))
+
+    @staticmethod
+    def square_size(msg_length):
+        return int(math.ceil(msg_length**.5))
+
+
+def encode(msg):
+    return CryptoSquare.encode(msg)
