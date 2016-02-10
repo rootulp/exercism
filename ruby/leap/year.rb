74e827afa02d3b620c727f0ac27fd07ab2de9cad
Rootul Patel <rootulp@gmail.com>
2016-02-10 16:55:24 -0500

    year: style fixes

--- ruby/leap/year.rb
+++ ruby/leap/year.rb
@@ -1,15 +1,14 @@
+# Year
 class Year
-
   def self.leap?(year)
     if year % 400 == 0
-      return true
+      true
     elsif year % 100 == 0
-      return false
+      false
     elsif year % 4 == 0
-      return true
+      true
     else
-      return false
+      false
     end
   end
-
 end
