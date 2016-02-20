9dd6a008f89f75a3fb2dff70112562625315f1bb
Rootul Patel <rootulp@gmail.com>
2016-02-19 23:37:57 -0500

    Pig-latin: style fixes

--- ruby/pig-latin/pig_latin.rb
+++ ruby/pig-latin/pig_latin.rb
@@ -1,33 +1,25 @@
-class PigLatin
-  require 'set'
+require 'set'
 
-  ALPHA = ('a'..'z').to_set
-  VOWELS = ['a', 'e', 'i', 'o', 'u'].to_set
-  CONSONANTS = ALPHA - VOWELS
+# Pig Latin
+class PigLatin
+  ALPHA = ('a'..'z').to_set.freeze
+  VOWELS = %w(a e i o u).to_set.freeze
+  CONSONANTS = (ALPHA - VOWELS).freeze
 
   def self.translate(phrase)
-    phrase.split.map { |word| self.translate_word(word) }.join(' ')
+    phrase.split.map { |word| translate_word(word) }.join(' ')
   end
 
   def self.translate_word(word)
-    if VOWELS.include?(word[0]) ||
-         word.start_with?('yt') ||
-         word.start_with?('xr')
+    if VOWELS.include?(word[0]) || word.start_with?('yt', 'xr')
       word + 'ay'
-
-    elsif word.start_with?('squ') ||
-          word.start_with?('sch') ||
-          word.start_with?('thr')
+    elsif word.start_with?('squ', 'sch', 'thr')
       word[3..-1] + word[0..2] + 'ay'
-
-    elsif (CONSONANTS.include?(word[0])  &&
-           CONSONANTS.include?(word[1])) ||
-           word.start_with?('qu')
+    elsif (CONSONANTS.include?(word[0]) && CONSONANTS.include?(word[1])) ||
+          word.start_with?('qu')
       word[2..-1] + word[0..1] + 'ay'
-
     elsif CONSONANTS.include?(word[0])
       word[1..-1] + word[0..0] + 'ay'
-
     end
   end
 end
