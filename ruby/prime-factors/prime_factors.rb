33c7a0da5e2592bdf39527745f70f6a4f0367b8b
Rootul Patel <rootulp@gmail.com>
2016-02-21 18:51:50 -0500

    Prime-factors: style fix

--- ruby/prime-factors/prime_factors.rb
+++ ruby/prime-factors/prime_factors.rb
@@ -1,11 +1,10 @@
 require 'prime'
 
+# Prime Factors
 class PrimeFactors
-
   def self.for(num)
     Prime.prime_division(num).map do |factors|
       Array.new(factors.last, factors.first)
     end.flatten
   end
-
 end
