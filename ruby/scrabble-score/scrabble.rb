65fef714802c35b3638a98b9b1a1a5790f7b8ce5
Rootul Patel <rootulp@gmail.com>
2016-02-25 13:31:04 -0500

    Scrabble-score: style fix

--- ruby/scrabble-score/scrabble.rb
+++ ruby/scrabble-score/scrabble.rb
@@ -1,26 +1,26 @@
+# Scrabble
 class Scrabble
-
   LETTER_VALUES = {
     A:  1, B:  3, C:  3, D:  2, E:  1, F:  4, G:  2, H:  4, I:  1, J:  8,
     K:  5, L:  1, M:  3, N:  1, O:  1, P:  3, Q: 10, R:  1, S:  1, T:  1,
     U:  1, V:  4, W:  4, X:  8, Y:  4, Z: 10
-  }
+  }.freeze
 
+  attr_reader :word
   def initialize(word)
     @word = word
   end
 
   def score
-    return 0 unless valid_word
-    @word.each_char.map { |char| LETTER_VALUES[char.upcase.to_sym] }.reduce(:+)
+    return 0 unless valid_word?
+    word.each_char.map { |char| LETTER_VALUES[char.upcase.to_sym] }.reduce(:+)
   end
 
-  def valid_word
-      @word && @word.match(/^[[:alpha:]]+$/)
+  def valid_word?
+    word && word.match(/^[[:alpha:]]+$/)
   end
 
   def self.score(word)
-    self.new(word).score
+    new(word).score
   end
-
 end
