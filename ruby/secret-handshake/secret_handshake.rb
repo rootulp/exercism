18f0021b29409d944e433efa44d8c94ad4b74bf1
Rootul Patel <rootulp@gmail.com>
2016-02-26 18:41:53 -0500

    Secret-handshake: style fix
    Need to revisit and refactor this solution

--- ruby/secret-handshake/secret_handshake.rb
+++ ruby/secret-handshake/secret_handshake.rb
@@ -1,24 +1,28 @@
+# SecretHandshake
 class SecretHandshake
-
-  attr_reader :commands
+  attr_reader :decimal
   def initialize(decimal)
-    if decimal.is_a?(Integer)
-      binary = sprintf('%05d', decimal.to_s(2))
-      @commands = build_handshake(binary)
-    else
-      @commands = []
-    end
+    @decimal = decimal
+  end
+
+  def commands
+    return [] unless decimal.is_a?(Integer)
+    handshake
+  end
+
+  def binary
+    format('%05d', decimal.to_s(2))
   end
 
   private
 
-  def build_handshake(binary)
+  def handshake
     results = []
-    results << 'wink'            if binary.reverse[0] == "1"
-    results << 'double blink'    if binary.reverse[1] == "1"
-    results << 'close your eyes' if binary.reverse[2] == "1"
-    results << 'jump'            if binary.reverse[3] == "1"
-    results.reverse!             if binary.reverse[4] == "1"
+    results << 'wink'            if binary.reverse[0] == '1'
+    results << 'double blink'    if binary.reverse[1] == '1'
+    results << 'close your eyes' if binary.reverse[2] == '1'
+    results << 'jump'            if binary.reverse[3] == '1'
+    results.reverse!             if binary.reverse[4] == '1'
     results
   end
 end
