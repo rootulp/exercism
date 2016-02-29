a046793480ade364e932b1a400e98756fd8d2717
Rootul Patel <rootulp@gmail.com>
2016-02-29 16:11:56 -0500

    Wordy: style fix

--- ruby/wordy/word_problem.rb
+++ ruby/wordy/word_problem.rb
@@ -1,57 +1,64 @@
+# Word Problem
 class WordProblem
-
   OPERATORS = {
     'plus' => '+',
     'minus' => '-',
     'multiplied' => '*',
     'divided' => '/'
-  }
-
+  }.freeze
   attr_reader :question
   def initialize(question)
     @question = question.chomp('?')
   end
 
   def answer
-    raise ArgumentError if operator_stack.empty? || number_stack.empty?
+    raise ArgumentError if empty_stacks?
+    calculate
+  end
+
+  private
+
+  def calculate
     operators = operator_stack
     numbers = number_stack
-
     while operators.any?
       op = operators.shift
       num1, num2 = numbers.shift(2)
       result = evaluate(op, num1, num2)
       numbers.unshift(result)
     end
-
     numbers.pop
   end
 
-  private
+  def evaluate(op, num1, num2)
+    num1.send(op, num2)
+  end
+
+  def empty_stacks?
+    operator_stack.empty? || number_stack.empty?
+  end
 
   def words
     question.split(' ')
   end
 
-  def is_operator?(word)
-    OPERATORS.include?(word)
-  end
-
-  def is_number?(word)
-    true if Float(word) rescue false # taken from StackOverflow
-  end
-
   def operator_stack
-    words.keep_if { |word| is_operator?(word) }
+    words.keep_if { |word| operator?(word) }.map { |word| to_op(word) }
   end
 
   def number_stack
-    words.keep_if { |word| is_number?(word) }.map(&:to_i)
+    words.keep_if { |word| number?(word) }.map(&:to_i)
   end
 
-  def evaluate(op, num1, num2)
-    num1.send(OPERATORS[op], num2)
+  def operator?(word)
+    OPERATORS.include?(word)
   end
 
+  def to_op(op)
+    OPERATORS[op]
+  end
+
+  def number?(word)
+    Integer(word) rescue false # taken from StackOverflow
+  end
 end
-
