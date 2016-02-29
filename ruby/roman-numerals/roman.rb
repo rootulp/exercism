2b9d3b9d4b7e4155ff5a25604bbe5795d4977aff
Rootul Patel <rootulp@gmail.com>
2016-02-29 17:09:22 -0500

    Roman-numerals: style fix

--- ruby/roman-numerals/roman.rb
+++ ruby/roman-numerals/roman.rb
@@ -1,32 +1,31 @@
+# Fixnum
 class Fixnum # I don't like monkey patching
+  NUMERALS = {
+    1 => 'I',
+    4 => 'IV',
+    5 => 'V',
+    9 => 'IX',
+    10 => 'X',
+    40 => 'XL',
+    50 => 'L',
+    90 => 'XC',
+    100 => 'C',
+    400 => 'CD',
+    500 => 'D',
+    900 => 'CM',
+    1000 => 'M'
+  }.freeze
 
   def to_roman
-    numerals = {
-      1 => "I",
-      4 => "IV",
-      5 => "V",
-      9 => "IX",
-      10 => "X",
-      40 => "XL",
-      50 => "L",
-      90 => "XC",
-      100 => "C",
-      400 => "CD",
-      500 => "D",
-      900 => "CM",
-      1000 => "M"
-    }
-
-    result = ""
+    result = ''
     target = self
 
-    numerals.keys.reverse.each do |num|
+    NUMERALS.keys.reverse_each do |num|
       while target >= num
         target -= num
-        result += numerals[num]
+        result += NUMERALS[num]
       end
     end
     result
   end
-
 end
