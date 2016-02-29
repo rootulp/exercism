1b58397d4bc09f6b58d2ac2be63957604d590a55
Rootul Patel <rootulp@gmail.com>
2016-02-29 16:57:01 -0500

    Trinary: refactor

--- ruby/trinary/trinary.rb
+++ ruby/trinary/trinary.rb
@@ -1,24 +1,20 @@
+# Trinary
 class Trinary
-
+  attr_reader :trinary
   def initialize(trinary)
     @trinary = trinary
   end
 
   def to_decimal
-    total = 0
-    return total unless valid?
-
-    @trinary.split(//).reverse.each_with_index do |multiplier, power|
-      total += (3 ** power) * multiplier.to_i
-    end
-
-    total
+    return 0 unless valid?
+    trinary.chars.reverse.map.each_with_index do |multiplier, power|
+      (3**power) * multiplier.to_i
+    end.reduce(:+)
   end
+
+  private
 
   def valid?
-    regex = /[^0123]/
-    return false if @trinary.match(regex)
-    true
+    !trinary.match(/[^0123]/)
   end
-
 end
