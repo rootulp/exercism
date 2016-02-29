0b8315d29130b23b6c04f9ab914652a6a86efd4b
Rootul Patel <rootulp@gmail.com>
2016-02-29 17:14:03 -0500

    Rna-transcription: style fix

--- ruby/rna-transcription/rna_transcription.rb
+++ ruby/rna-transcription/rna_transcription.rb
@@ -1,27 +1,24 @@
 class Complement
-
   DNA_PAIRINGS = {
-    "G" => "C",
-    "C" => "G",
-    "A" => "U",
-    "T" => "A"
-  }
-
+    'G' => 'C',
+    'C' => 'G',
+    'A' => 'U',
+    'T' => 'A'
+  }.freeze
   RNA_PAIRINGS = {
-    "G" => "C",
-    "C" => "G",
-    "A" => "T",
-    "U" => "A"
-  }
-
+    'G' => 'C',
+    'C' => 'G',
+    'A' => 'T',
+    'U' => 'A'
+  }.freeze
   def self.of_dna(strand)
     raise ArgumentError if strand.include?('U')
-    strand.split(//).map { |nucleobase| self.dna_pairing_for(nucleobase) }.join
+    strand.split(//).map { |nucleobase| dna_pairing_for(nucleobase) }.join
   end
 
   def self.of_rna(strand)
     raise ArgumentError if strand.include?('T')
-    strand.split(//).map { |nucleobase| self.rna_pairing_for(nucleobase) }.join
+    strand.split(//).map { |nucleobase| rna_pairing_for(nucleobase) }.join
   end
 
   def self.dna_pairing_for(nucleobase)
@@ -31,5 +28,4 @@ class Complement
   def self.rna_pairing_for(nucleobase)
     RNA_PAIRINGS[nucleobase]
   end
-
 end
