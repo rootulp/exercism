05baef44b1a75bd691185af1e019b45d6391e3b8
Rootul Patel <rootulp@gmail.com>
2016-03-01 14:00:58 -0500

    Robot-name: style fix

--- ruby/robot-name/robot.rb
+++ ruby/robot-name/robot.rb
@@ -1,18 +1,25 @@
+# Robot
 class Robot
   attr_reader :name
-
   def initialize
-    @name = generate_name
+    reset
   end
 
   def reset
     @name = generate_name
   end
 
+  private
+
   def generate_name
-    prefix = (0...2).map { ('A'..'Z').to_a[rand(26)] }.join
-    suffix = (0...3).map { ('1'..'9').to_a[rand(9)] }.join
     prefix + suffix
   end
 
+  def prefix
+    (0...2).map { ('A'..'Z').to_a[rand(26)] }.join
+  end
+
+  def suffix
+    (0...3).map { ('1'..'9').to_a[rand(9)] }.join
+  end
 end
