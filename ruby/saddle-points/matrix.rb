85ec9229dea330b06bc8e0837bcd61b5255602a0
Rootul Patel <rootulp@gmail.com>
2016-03-01 13:36:18 -0500

    Saddle-points: style fix

--- ruby/saddle-points/matrix.rb
+++ ruby/saddle-points/matrix.rb
@@ -1,33 +1,31 @@
+# Matrix
 class Matrix
-
-  attr_reader :rows, :columns, :saddle_points
-  def initialize(string)
-    @rows = build_rows(string)
-    @columns = rows.transpose
-    @saddle_points = find_saddle_points
+  attr_reader :input
+  def initialize(input)
+    @input = input
   end
 
-  private
-
-  def build_rows(string)
-    string.split("\n").map do |row|
-      row.split(" ").map(&:to_i)
+  def rows
+    input.split("\n").map do |row|
+      row.split(' ').map(&:to_i)
     end
   end
 
-  def find_saddle_points
+  def columns
+    rows.transpose
+  end
+
+  def saddle_points
     results = []
     rows.each_with_index do |row, x|
       row.each_with_index do |val, y|
-        results << [x,y] if saddle_point?(val, x, y)
+        results << [x, y] if saddle_point?(val, x, y)
       end
     end
     results
   end
 
   def saddle_point?(val, x, y)
-    max_x = rows[x].max
-    min_y = columns[y].min
-    val == max_x && val == min_y 
+    val == rows[x].max && val == columns[y].min
   end
 end
