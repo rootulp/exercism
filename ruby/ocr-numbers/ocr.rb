5302f0c12ec74ead48384f8b7a638e7c60267ad4
Rootul Patel <rootulp@gmail.com>
2016-03-15 17:01:21 -0400

    ocr-numbers: solve
    ocr-numbers: one test failing

--- ruby/ocr-numbers/ocr.rb
+++ ruby/ocr-numbers/ocr.rb
@@ -1,41 +1,44 @@
 # OCR
 class OCR
+  GARBLE = '?'.freeze
   RAW_NUMS = <<-RAW_NUMS.chomp
  _     _  _     _  _  _  _  _
 | |  | _| _||_||_ |_   ||_||_|
 |_|  ||_  _|  | _||_|  ||_| _|
 
   RAW_NUMS
-  NUMS = split_blocks(RAW_NUMS)
 
-  attr_reader :input
+  attr_reader :input, :nums
   def initialize(input)
     @input = input
+    @nums = split_blocks(RAW_NUMS)
   end
 
   def convert
-    split_chunks(input).map do |blocks|
-      blocks.map do |block|
-        convert_block(block)
-      end.join(',')
-    end.join.chomp(',')
+    split_chunks(input).map do |chunk|
+      split_blocks(chunk).map do |block|
+        translate_block(block)
+      end.join
+    end.join(',')
   end
 
   private
 
-  def convert_block(block)
-    NUMS.include?(block) ? NUMS.index(block).to_s : '?'
+  def translate_block(block)
+    nums.include?(block) ? nums.index(block).to_s : GARBLE
   end
 
   def split_chunks(chunks)
-    chunks.split("\n\n").map { |blocks| split_blocks(blocks) }
+    chunks.split("\n\n")
   end
 
+  # split_blocks is too complex - consider splitting this up
   def split_blocks(blocks)
     blocks.split("\n")
-          .map { |x| x.scan(/.{1,3}/) }
-          .map { |x| x.map(&:rstrip) }
+          .map { |block| block.scan(/.{1,3}/) }
+          .map { |row| row.map { |val| val.ljust(3) } }
+          .map { |row| row.empty? ? ['   '] : row } # for 1 and 4
           .transpose
-          .map { |x| x.join("\n") << "\n" }
+          .map { |x| x.join("\n") }
   end
 end
