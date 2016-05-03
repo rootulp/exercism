537003ef6538e6ee151fd1a383f81d2923c5677d
Rootul Patel <rootulp@gmail.com>
2016-05-02 23:28:30 -0400

    largest-series-product: remove rubocop disable

--- ruby/largest-series-product/largest_series_product_test.rb
+++ ruby/largest-series-product/largest_series_product_test.rb
@@ -1,9 +1,6 @@
 require 'minitest/autorun'
 require_relative 'largest_series_product'
 
-# Rubocop directives
-# rubocop:disable Lint/ParenthesesAsGroupedExpression
-#
 class Seriestest < Minitest::Test
   def test_digits
     assert_equal (0..9).to_a, Series.new('0123456789').digits
