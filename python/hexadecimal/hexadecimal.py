5402ae54a8970053993ab8a1082de5cccb792506
Rootul Patel <rootulp@gmail.com>
2016-05-07 18:02:53 -0400

    Hexadecimal: remove convert_helper

--- python/hexadecimal/hexadecimal.py
+++ python/hexadecimal/hexadecimal.py
@@ -6,19 +6,14 @@ class Hexa:
                    'e': 14,
                    'f': 15}
     VALID_CHARS = set(map(str, range(0, 10)) + CHAR_VALUES.keys())
+    BASE = 16
 
     @classmethod
     def convert(cls, inp):
         if not cls.valid(inp):
             raise ValueError
-        return cls.convert_helper(inp)
-
-    @classmethod
-    def convert_helper(cls, inp):
-        total = 0
-        for index, char in enumerate(reversed(inp)):
-            total += cls.convert_char(char) * 16**index
-        return total
+        return sum([cls.convert_char(char) * cls.BASE**index for index, char in
+                    enumerate(reversed(inp))])
 
     @classmethod
     def valid(cls, inp):
