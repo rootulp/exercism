61460816f98c1174f763c69803596283db998e3c
Rootul Patel <rootulp@gmail.com>
2016-05-15 20:29:38 -0400

    Year: divisible_by method

--- ruby/leap/year.rb
+++ ruby/leap/year.rb
@@ -1,14 +1,13 @@
 # Year
 class Year
   def self.leap?(year)
-    if year % 400 == 0
-      true
-    elsif year % 100 == 0
-      false
-    elsif year % 4 == 0
-      true
-    else
-      false
-    end
+    return true if divisible_by(year, 400)
+    return false if divisible_by(year, 100)
+    return true if divisible_by(year, 4)
+    false
+  end
+
+  def self.divisible_by(year, divisor)
+    year % divisor == 0
   end
 end
