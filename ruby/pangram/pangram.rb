9df936cd7cb8732e0c17461e8b8131367ae6c936
Rootul Patel <rootulp@gmail.com>
2016-05-15 23:09:47 -0400

    Pangram: solve

---
+++ ruby/pangram/pangram.rb
@@ -0,0 +1,14 @@
+require 'set'
+
+class Pangram
+  VERSION = 1
+
+  def self.pangram?(str)
+    alphabet = ('a'..'z').to_set
+    str.downcase.chars.each do |char|
+      alphabet.delete(char)
+    end
+    alphabet.empty?
+  end
+  singleton_class.send(:alias_method, :is_pangram?, :pangram?)
+end
