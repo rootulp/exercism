c79ca09d2f0fe9eb00db227576e9df218d39c100
Rootul Patel <rootulp@gmail.com>
2016-07-30 18:42:59 -0400

    PigLatin: solve

--- java/pig-latin/src/main/java/PigLatin.java
+++ java/pig-latin/src/main/java/PigLatin.java
@@ -1,6 +1,6 @@
-import java.util.Set;
-import java.util.HashSet;
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class PigLatin {
 
@@ -9,17 +9,41 @@ public class PigLatin {
   private static final Set<Character> VOWELS_SET =
     new HashSet<Character>(Arrays.asList(VOWELS));
 
-  public static String translate(String input) {
-    if ((VOWELS_SET.contains(input.charAt(0))) ||
-        (input.startsWith("xr")) ||
-        (input.startsWith("yt"))) {
-      return input + "ay";
-    } else if (input.startsWith("squ") ||
-               input.startsWith("sch") ||
-               input.startsWith("thr")) {
-      return input.substring(3) + input.substring(0, 3) + "ay";
-    } else {
-      return "bla";
+  public static String translate(String phrase) {
+    String[] words = phrase.split(" ");
+    for (int i = 0; i < words.length; i++) {
+      words[i] = translateWord(words[i]);
     }
+    return String.join(" ", words);
+  }
+
+  private static String translateWord(String word) {
+    if (singleVowel(word)){
+      return word + "ay";
+    } else if (tripleConsonant(word)) {
+      return word.substring(3) + word.substring(0, 3) + "ay";
+    } else if (doubleConsonant(word)) {
+      return word.substring(2) + word.substring(0, 2) + "ay";
+    } else {
+      return word.substring(1) + word.substring(0, 1) + "ay";
+    }
+  }
+
+  private static boolean singleVowel(String word) {
+    return ((VOWELS_SET.contains(word.charAt(0))) ||
+            (word.startsWith("xr")) ||
+            (word.startsWith("yt")));
+  }
+
+  private static boolean tripleConsonant(String word) {
+    return (word.startsWith("squ") ||
+            word.startsWith("sch") ||
+            word.startsWith("thr"));
+  }
+
+  private static boolean doubleConsonant(String word) {
+    return (word.startsWith("ch") ||
+            word.startsWith("qu") ||
+            word.startsWith("th"));
   }
 }
