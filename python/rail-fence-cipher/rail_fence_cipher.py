ddb413097249aca48d8186907f12c9314590af94
Rootul Patel <rootulp@palantir.com>
2016-12-27 18:23:14 -0500

    rails_fence_cipher: custom iterators
    - add custom iterators for linear and zig_zag
    - remove redundant cipher class
    - could use more refactoring
    - particularly remove the passing of message or rail_lengths to
      iterators
    - also pop(0) should be replaced with deque and popLeft()

--- python/rail-fence-cipher/rail_fence_cipher.py
+++ python/rail-fence-cipher/rail_fence_cipher.py
@@ -2,71 +2,63 @@ class Rails:
 
     def __init__(self, num_rails):
         self.num_rails = num_rails
-        self.rails = [[] for _ in range(self.num_rails)]
+        self.rails = [[] for _ in xrange(num_rails)]
 
     def populate_rails_linear(self, message, rail_lengths):
         message_list = list(message)
-        for index in xrange(len(self.rails)):
-            for rail_length in xrange(rail_lengths[index]):
-                self.rails[index].append(message_list.pop(0))
+        for rail in self.linear_iterator(rail_lengths):
+            rail.append(message_list.pop(0))
 
     def populate_rails_zig_zag(self, message):
-        self.reset()
-        for data in message:
-            self.rails[self.rails_index].append(data)
-            self.increment_rails_index()
+        message_list = list(message)
+        for rail in self.zig_zag_iterator(message):
+            rail.append(message_list.pop(0))
 
     def to_string_linear(self):
         return ''.join([data for rail in self.rails for data in rail])
 
     def to_string_zig_zag(self, message):
-        self.reset()
-        output = []
-        for _ in range(len(message)):
-            output.append(self.rails[self.rails_index].pop(0))
-            self.increment_rails_index()
-        return ''.join(output)
+        return ''.join([rail.pop(0) for rail in self.zig_zag_iterator(message)])
 
-    def increment_rails_index(self):
-        self.rails_index_reverse_direction()
-        if self.rails_index_increasing:
-            self.rails_index += 1
+    def linear_iterator(self, rail_lengths):
+        for index in xrange(len(self.rails)):
+            for rail_length in xrange(rail_lengths[index]):
+                yield self.rails[index]
+
+    def zig_zag_iterator(self, message):
+        index = 0
+        increasing = True
+        for _ in message:
+            yield self.rails[index]
+            increasing = self.direction(index, increasing)
+            index = self.increment_index(index, increasing)
+
+    def increment_index(self, index, increasing):
+        if increasing:
+            return index + 1
         else:
-            self.rails_index -= 1
+            return index - 1
 
-    def rails_index_reverse_direction(self):
-        if self.rails_index == 0:
-            self.rails_index_increasing = True
-        elif self.rails_index == self.num_rails - 1:
-            self.rails_index_increasing = False
-
-    def reset(self):
-        self.rails_index = 0
-        self.rails_index_increasing = True
-
-class RailsFenceCipher:
-
-    def __init__(self, message, num_rails):
-        self.message = message
-        self.num_rails = num_rails
-        self.rails = Rails(num_rails)
-
-    def encode(self):
-        self.rails.populate_rails_zig_zag(self.message)
-        return self.rails.to_string_linear()
-
-    def decode(self):
-        faulty_rails = Rails(self.num_rails)
-        faulty_rails.populate_rails_zig_zag(self.message)
-        rail_lengths = [len(rail) for rail in faulty_rails.rails]
-
-        self.rails.populate_rails_linear(self.message, rail_lengths)
-        return self.rails.to_string_zig_zag(self.message)
+    def direction(self, index, increasing):
+        if index == 0:
+            return True
+        elif index == self.num_rails - 1:
+            return False
+        else:
+            return increasing
 
 
 def encode(message, num_rails):
-    return RailsFenceCipher(message, num_rails).encode()
+    rails = Rails(num_rails)
+    rails.populate_rails_zig_zag(message)
+    return rails.to_string_linear()
 
 
 def decode(message, num_rails):
-    return RailsFenceCipher(message, num_rails).decode()
+    faulty_rails = Rails(num_rails)
+    faulty_rails.populate_rails_zig_zag(message)
+    rail_lengths = [len(rail) for rail in faulty_rails.rails]
+
+    rails = Rails(num_rails)
+    rails.populate_rails_linear(message, rail_lengths)
+    return rails.to_string_zig_zag(message)
