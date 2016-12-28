69c46103d2ad97ee43541245021a80a6c513aaac
Rootul Patel <rootulp@palantir.com>
2016-12-27 19:48:10 -0500

    flatten_array: solve
    this solution seems unclean. In particular I want to perform this
    flatten in place and the several conditionals seem like not the most
    elegant way to do this

---
+++ python/flatten-array/flatten_array.py
@@ -0,0 +1,11 @@
+def flatten(nested_list):
+    return [element for element in flatten_list(nested_list)]
+
+def flatten_list(nested_list):
+    for element in nested_list:
+        if not isinstance(element, list):
+            if element is not None and not isinstance(element, tuple):
+                yield element
+        else:
+            for subelement in flatten_list(element):
+                    yield subelement
