734a3bf3a72e2c6ec7ffe15377965a3e8aebdb7f
Rootul Patel <rootulp@palantir.com>
2017-07-08 16:22:42 -0400

    solve perfect-numbers

---
+++ java/perfect-numbers/src/main/java/NaturalNumber.java
@@ -0,0 +1,55 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+class NaturalNumber {
+
+  private int naturalNumber;
+
+  public NaturalNumber(int naturalNumber) {
+    if (invalidNaturalNumber(naturalNumber)) {
+      throw new IllegalArgumentException("You must supply a natural number (positive integer)",
+              new Throwable(Integer.toString(naturalNumber)));
+    }
+    this.naturalNumber = naturalNumber;
+  }
+
+  public Classification getClassification() {
+    int aliquotSum = aliquotSum(naturalNumber);
+
+    if (aliquotSum > naturalNumber) {
+      return Classification.ABUNDANT;
+    } else if (aliquotSum < naturalNumber) {
+      return Classification.DEFICIENT;
+    } else {
+      return Classification.PERFECT;
+    }
+  }
+
+  private int aliquotSum(int number) {
+    return factorsExcludingNumber(number).stream().mapToInt(Integer::intValue).sum();
+  }
+
+  private Set<Integer> factorsExcludingNumber(int number) {
+    Set<Integer> factorsExcludingNumber = factorsOf(number);
+    factorsExcludingNumber.remove(number);
+    return factorsExcludingNumber;
+  }
+
+  private Set<Integer> factorsOf(int number) {
+    Set<Integer> factors = new HashSet<Integer>();
+    for (int potentialFactor = 1; potentialFactor < Math.ceil(Math.sqrt(number)); potentialFactor += 1) {
+      if (number % potentialFactor == 0) {
+        factors.add(potentialFactor);
+        factors.add(number / potentialFactor);
+      }
+    }
+    return factors;
+  }
+
+  private boolean invalidNaturalNumber(int number) {
+    return number <= 0;
+  }
+
+}
