009e74412c2532d8eeb3509f6137034ca5609bf2
Rootul Patel <rootulp@palantir.com>
2017-08-15 01:07:30 -0700

    Handle invalid input

--- typescript/rna-transcription/rna-transcription.test.ts
+++ typescript/rna-transcription/rna-transcription.test.ts
@@ -24,19 +24,19 @@ describe('Transcriptor', () => {
         .toEqual('UGCACCAGAAUU')
   })
 
-  xit('correctly handles invalid input', () => {
+  it('correctly handles invalid input', () => {
     expect(() => transcriptor.toRna('U')).toThrowError(
       'Invalid input DNA.'
     )
   })
 
-  xit('correctly handles completely invalid input', () => {
+  it('correctly handles completely invalid input', () => {
     expect(() => transcriptor.toRna('XXX')).toThrowError(
       'Invalid input DNA.'
     )
   })
 
-  xit('correctly handles partially invalid input', () => {
+  it('correctly handles partially invalid input', () => {
     expect(() => transcriptor.toRna('ACGTXXXCTTAA')).toThrowError(
       'Invalid input DNA.'
     )
--- typescript/rna-transcription/rna-transcription.ts
+++ typescript/rna-transcription/rna-transcription.ts
@@ -8,11 +8,27 @@ class Transcriptor {
     ]);
 
     public toRna(strand: string) {
+        if (this.invalidInputData(strand)) {
+            throw 'Invalid input DNA.'
+        }
+        return this.transcribeStrand(strand)
+    }
+
+    private transcribeStrand(strand: string) {
         return strand.split('').map(nucleotide => this.transcribeNucleotide(nucleotide)).join('')
     }
 
     private transcribeNucleotide(nucleotide: string) {
         return Transcriptor.NUCLEOTIDE_COMPLEMENTS.get(nucleotide)
+    }
+
+    private invalidInputData(strand: string): boolean {
+        for (let nucleotide of strand) {
+            if (!Transcriptor.NUCLEOTIDE_COMPLEMENTS.has(nucleotide)) {
+                return true
+            }
+        }
+        return false
     }
 }
 
