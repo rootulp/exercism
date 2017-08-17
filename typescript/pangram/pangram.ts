43e6ddad2b6d5f309fc1f2be3ad092be5e2e245a
Rootul Patel <rootulp@palantir.com>
2017-08-16 19:39:55 -0700

    solve pangram

---
+++ typescript/pangram/pangram.ts
@@ -0,0 +1,19 @@
+class Pangram {
+
+  sentence: string
+  alphabet = new Set("ABCDEFGHIJKLMNOPQRSTUVWXYZ".split(""))
+
+  constructor(sentence: string) {
+    this.sentence = sentence
+  }
+
+  isPangram(): boolean {
+    for (let character of this.sentence.toUpperCase()) {
+      this.alphabet.delete(character)
+    }
+    return this.alphabet.size === 0
+  }
+
+}
+
+export default Pangram
