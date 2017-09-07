71a03af59187fb4a5125f5dc5c1a495623cfbcec
Rootul Patel <rootulp@palantir.com>
2017-09-06 17:44:23 -0700

    Solve SpaceAge

--- typescript/space-age/space-age.ts
+++ typescript/space-age/space-age.ts
@@ -2,40 +2,60 @@ class SpaceAge {
 
   public seconds: number;
 
+    private ORBITAL_PERIOD_IN_EARTH_YEARS = Object.freeze({
+        'Mercury': 0.2408467,
+        'Venus': 0.61519726,
+        'Mars': 1.8808158,
+        'Jupiter': 11.862615,
+        'Saturn': 29.447498,
+        'Uranus': 84.016846,
+        'Neptune': 164.79132 
+    });
+
+    private SECONDS_IN_EARTH_YEAR = 31557600;
+
   constructor(seconds: number) {
     this.seconds = seconds;
   }
 
+  private earthYears(): number {
+    return this.seconds / this.SECONDS_IN_EARTH_YEAR;
+  }
+
   public onEarth(): number {
-    return 0;
+    return SpaceAge.roundToTwoDecimalPlaces(this.earthYears());
   }
 
   public onMercury(): number {
-    return 0;
+    return SpaceAge.roundToTwoDecimalPlaces(this.earthYears() / this.ORBITAL_PERIOD_IN_EARTH_YEARS.Mercury);
   }
 
   public onVenus(): number {
-    return 0;
+    return SpaceAge.roundToTwoDecimalPlaces(this.earthYears() / this.ORBITAL_PERIOD_IN_EARTH_YEARS.Venus);
   }
 
   public onMars(): number {
-    return 0;
+    return SpaceAge.roundToTwoDecimalPlaces(this.earthYears() / this.ORBITAL_PERIOD_IN_EARTH_YEARS.Mars);
   }
 
   public onJupiter(): number {
-    return 0;
+    return SpaceAge.roundToTwoDecimalPlaces(this.earthYears() / this.ORBITAL_PERIOD_IN_EARTH_YEARS.Jupiter);
   }
 
   public onSaturn(): number {
-    return 0;
+    return SpaceAge.roundToTwoDecimalPlaces(this.earthYears() / this.ORBITAL_PERIOD_IN_EARTH_YEARS.Saturn);
   }
 
   public onUranus(): number {
-    return 0;
+    return SpaceAge.roundToTwoDecimalPlaces(this.earthYears() / this.ORBITAL_PERIOD_IN_EARTH_YEARS.Uranus);
   }
 
   public onNeptune(): number {
-    return 0;
+    return SpaceAge.roundToTwoDecimalPlaces(this.earthYears() / this.ORBITAL_PERIOD_IN_EARTH_YEARS.Neptune);
+  }
+
+  private static roundToTwoDecimalPlaces(number: number): number {
+    return Math.round(number * 100) / 100;
   }
 
 }
