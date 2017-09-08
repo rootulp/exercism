55777a49f1c15905b0c69b92996dbdcfb10f3386
Rootul Patel <rootulp@palantir.com>
2017-09-07 22:56:56 -0700

    Code style fixes from tslint

--- typescript/rna-transcription/rna-transcription.ts
+++ typescript/rna-transcription/rna-transcription.ts
@@ -1,35 +1,36 @@
 class Transcriptor {
-
-    static readonly NUCLEOTIDE_COMPLEMENTS = new Map<string, string>([
-        ['G', 'C'],
-        ['C', 'G'],
-        ['T', 'A'],
-        ['A', 'U']
-    ]);
-
-    public toRna(strand: string) {
-        if (this.invalidInputData(strand)) {
-            throw 'Invalid input DNA.'
-        }
-        return this.transcribeStrand(strand)
+  
+  static readonly NUCLEOTIDE_COMPLEMENTS = new Map<string, string>([
+    ['G', 'C'],
+    ['C', 'G'],
+    ['T', 'A'],
+    ['A', 'U']
+  ])
+  
+  public toRna(strand: string) {
+    if (this.invalidInputData(strand)) {
+      throw 'Invalid input DNA.'
     }
-
-    private transcribeStrand(strand: string) {
-        return strand.split('').map(nucleotide => this.transcribeNucleotide(nucleotide)).join('')
+    return this.transcribeStrand(strand)
+  }
+  
+  private transcribeStrand(strand: string) {
+    return strand.split('').map((nucleotide) => this.transcribeNucleotide(nucleotide)).join('')
+  }
+  
+  private transcribeNucleotide(nucleotide: string) {
+    return Transcriptor.NUCLEOTIDE_COMPLEMENTS.get(nucleotide)
+  }
+  
+  private invalidInputData(strand: string): boolean {
+    for (const nucleotide of strand) {
+      if (!Transcriptor.NUCLEOTIDE_COMPLEMENTS.has(nucleotide)) {
+        return true
+      }
     }
+    return false
+  }
 
-    private transcribeNucleotide(nucleotide: string) {
-        return Transcriptor.NUCLEOTIDE_COMPLEMENTS.get(nucleotide)
-    }
-
-    private invalidInputData(strand: string): boolean {
-        for (let nucleotide of strand) {
-            if (!Transcriptor.NUCLEOTIDE_COMPLEMENTS.has(nucleotide)) {
-                return true
-            }
-        }
-        return false
-    }
 }
 
 export default Transcriptor
