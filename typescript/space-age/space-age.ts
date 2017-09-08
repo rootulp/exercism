43eef007772b9a9c77d1973a33c313269d744efe
Rootul Patel <rootulp@palantir.com>
2017-09-07 22:59:19 -0700

    Code style fixes from tslint

--- typescript/space-age/space-age.ts
+++ typescript/space-age/space-age.ts
@@ -1,61 +1,61 @@
 class SpaceAge {
 
-  public seconds: number;
+  public seconds: number
 
     private ORBITAL_PERIOD_IN_EARTH_YEARS = Object.freeze({
-        'Mercury': 0.2408467,
-        'Venus': 0.61519726,
-        'Mars': 1.8808158,
-        'Jupiter': 11.862615,
-        'Saturn': 29.447498,
-        'Uranus': 84.016846,
-        'Neptune': 164.79132 
-    });
+        Mercury: 0.2408467,
+        Venus: 0.61519726,
+        Mars: 1.8808158,
+        Jupiter: 11.862615,
+        Saturn: 29.447498,
+        Uranus: 84.016846,
+        Neptune: 164.79132
+    })
 
-    private SECONDS_IN_EARTH_YEAR = 31557600;
+    private SECONDS_IN_EARTH_YEAR = 31557600
 
   constructor(seconds: number) {
-    this.seconds = seconds;
+    this.seconds = seconds
   }
 
   private earthYears(): number {
-    return this.seconds / this.SECONDS_IN_EARTH_YEAR;
+    return this.seconds / this.SECONDS_IN_EARTH_YEAR
   }
 
   public onEarth(): number {
-    return SpaceAge.roundToTwoDecimalPlaces(this.earthYears());
+    return SpaceAge.roundToTwoDecimalPlaces(this.earthYears())
   }
 
   public onMercury(): number {
-    return SpaceAge.roundToTwoDecimalPlaces(this.earthYears() / this.ORBITAL_PERIOD_IN_EARTH_YEARS.Mercury);
+    return SpaceAge.roundToTwoDecimalPlaces(this.earthYears() / this.ORBITAL_PERIOD_IN_EARTH_YEARS.Mercury)
   }
 
   public onVenus(): number {
-    return SpaceAge.roundToTwoDecimalPlaces(this.earthYears() / this.ORBITAL_PERIOD_IN_EARTH_YEARS.Venus);
+    return SpaceAge.roundToTwoDecimalPlaces(this.earthYears() / this.ORBITAL_PERIOD_IN_EARTH_YEARS.Venus)
   }
 
   public onMars(): number {
-    return SpaceAge.roundToTwoDecimalPlaces(this.earthYears() / this.ORBITAL_PERIOD_IN_EARTH_YEARS.Mars);
+    return SpaceAge.roundToTwoDecimalPlaces(this.earthYears() / this.ORBITAL_PERIOD_IN_EARTH_YEARS.Mars)
   }
 
   public onJupiter(): number {
-    return SpaceAge.roundToTwoDecimalPlaces(this.earthYears() / this.ORBITAL_PERIOD_IN_EARTH_YEARS.Jupiter);
+    return SpaceAge.roundToTwoDecimalPlaces(this.earthYears() / this.ORBITAL_PERIOD_IN_EARTH_YEARS.Jupiter)
   }
 
   public onSaturn(): number {
-    return SpaceAge.roundToTwoDecimalPlaces(this.earthYears() / this.ORBITAL_PERIOD_IN_EARTH_YEARS.Saturn);
+    return SpaceAge.roundToTwoDecimalPlaces(this.earthYears() / this.ORBITAL_PERIOD_IN_EARTH_YEARS.Saturn)
   }
 
   public onUranus(): number {
-    return SpaceAge.roundToTwoDecimalPlaces(this.earthYears() / this.ORBITAL_PERIOD_IN_EARTH_YEARS.Uranus);
+    return SpaceAge.roundToTwoDecimalPlaces(this.earthYears() / this.ORBITAL_PERIOD_IN_EARTH_YEARS.Uranus)
   }
 
   public onNeptune(): number {
-    return SpaceAge.roundToTwoDecimalPlaces(this.earthYears() / this.ORBITAL_PERIOD_IN_EARTH_YEARS.Neptune);
+    return SpaceAge.roundToTwoDecimalPlaces(this.earthYears() / this.ORBITAL_PERIOD_IN_EARTH_YEARS.Neptune)
   }
 
-  private static roundToTwoDecimalPlaces(number: number): number {
-    return Math.round(number * 100) / 100;
+  private static roundToTwoDecimalPlaces(num: number): number {
+    return Math.round(num * 100) / 100
   }
 
 }
