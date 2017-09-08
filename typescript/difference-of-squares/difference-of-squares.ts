c07da30d1eb230435207615ab82e5b442e8fde48
Rootul Patel <rootulp@palantir.com>
2017-09-07 21:30:52 -0700

    Extract squares and sum

--- typescript/difference-of-squares/difference-of-squares.ts
+++ typescript/difference-of-squares/difference-of-squares.ts
@@ -1,33 +1,33 @@
 class Squares {
 
-  private number: number;
   public squareOfSums: number;
   public sumOfSquares: number;
   public difference: number;
 
   constructor(number: number) {
-    this.number = number;
     this.squareOfSums = Squares.squareOfSums(number)
     this.sumOfSquares = Squares.sumOfSquares(number)
     this.difference = this.squareOfSums - this.sumOfSquares
   }
 
   private static squareOfSums(number: number): number {
-    const range = Squares.range(number);
-    const sum = range.reduce((a, b) => a + b, 0);
-    const square = sum ** 2;
-    return square;
+    return Squares.square(Squares.sum(Squares.range(number)));
   }
 
   private static sumOfSquares(number: number): number {
-    const range = Squares.range(number);
-    const squares = range.map( x => x ** 2);
-    const sum = squares.reduce((a, b) => a + b, 0);
-    return sum;
+    return Squares.sum(Squares.range(number).map( x => Squares.square(x)));
   }
 
   private static range(number: number): Array<number> {
     return [...Array(number + 1).keys()]
+  }
+
+  private static sum(numbers: Array<number>): number {
+    return numbers.reduce((a, b) => a + b, 0)
+  }
+
+  private static square(number: number): number {
+    return number ** 2;
   }
 
 }
