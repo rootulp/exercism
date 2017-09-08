e5af0020da28251d5fd828862e9b7c5bc9deccaa
Rootul Patel <rootulp@palantir.com>
2017-09-07 22:49:57 -0700

    Code style fixes from tslint

--- typescript/difference-of-squares/difference-of-squares.ts
+++ typescript/difference-of-squares/difference-of-squares.ts
@@ -1,35 +1,35 @@
 class Squares {
 
-  public squareOfSums: number;
-  public sumOfSquares: number;
-  public difference: number;
+  public squareOfSums: number
+  public sumOfSquares: number
+  public difference: number
 
-  constructor(number: number) {
-    this.squareOfSums = Squares.squareOfSums(number)
-    this.sumOfSquares = Squares.sumOfSquares(number)
+  constructor(num: number) {
+    this.squareOfSums = Squares.squareOfSums(num)
+    this.sumOfSquares = Squares.sumOfSquares(num)
     this.difference = this.squareOfSums - this.sumOfSquares
   }
 
-  private static squareOfSums(number: number): number {
-    return Squares.square(Squares.sum(Squares.range(number)));
+  private static squareOfSums(num: number): number {
+    return Squares.square(Squares.sum(Squares.range(num)))
   }
 
-  private static sumOfSquares(number: number): number {
-    return Squares.sum(Squares.range(number).map( x => Squares.square(x)));
+  private static sumOfSquares(num: number): number {
+    return Squares.sum(Squares.range(num).map( (x) => Squares.square(x)))
   }
 
-  private static range(number: number): Array<number> {
-    return [...Array(number + 1).keys()]
+  private static range(num: number): number[] {
+    return [...Array(num + 1).keys()]
   }
 
-  private static sum(numbers: Array<number>): number {
-    return numbers.reduce((a, b) => a + b, 0)
+  private static sum(num: number[]): number {
+    return num.reduce((a, b) => a + b, 0)
   }
 
-  private static square(number: number): number {
-    return number ** 2;
+  private static square(num: number): number {
+    return num ** 2
   }
 
 }
 
-export default Squares;
+export default Squares
