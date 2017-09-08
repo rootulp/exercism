b4768c141fbb901257869e08aa76acc0e573e802
Rootul Patel <rootulp@palantir.com>
2017-09-07 22:52:26 -0700

    Code style fixes from tslint

--- typescript/hamming/hamming.ts
+++ typescript/hamming/hamming.ts
@@ -1,26 +1,28 @@
 class Hamming {
-    public compute(strand1: string, strand2:string) {
-        if (this.unequalLength(strand1, strand2)) {
-            throw 'DNA strands must be of equal length.'
-        }
-        return this.distanceBetweenStrands(strand1, strand2)
-    }
 
-    private distanceBetweenStrands(strand1: string, strand2: string) {
-        let distance = 0
-        for (let i = 0; i < strand1.length; i++) {
-            distance += this.distanceBetweenNucleotides(strand1.charAt(i), strand2.charAt(i))
-        }
-        return distance
+  public compute(strand1: string, strand2: string) {
+    if (this.unequalLength(strand1, strand2)) {
+      throw 'DNA strands must be of equal length.'
     }
+    return this.distanceBetweenStrands(strand1, strand2)
+  }
 
-    private distanceBetweenNucleotides(nucleotide1: string, nucleotide2: string) {
-        return nucleotide1 === nucleotide2 ? 0 : 1
+  private distanceBetweenStrands(strand1: string, strand2: string) {
+    let distance = 0
+    for (let i = 0; i < strand1.length; i++) {
+      distance += this.distanceBetweenNucleotides(strand1.charAt(i), strand2.charAt(i))
     }
+    return distance
+  }
 
-    private unequalLength(strand1: string, strand2: string) {
-        return strand1.length !== strand2.length
-    }
+  private distanceBetweenNucleotides(nucleotide1: string, nucleotide2: string) {
+    return nucleotide1 === nucleotide2 ? 0 : 1
+  }
+
+  private unequalLength(strand1: string, strand2: string) {
+    return strand1.length !== strand2.length
+  }
+
 }
 
 export default Hamming
