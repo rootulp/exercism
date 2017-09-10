391cf01dd6ec9bd7eef6be6f743210acf7e564eb
Rootul Patel <rootulp@palantir.com>
2017-09-10 12:27:44 -0700

    Solve rotational cipher

--- java/rotational-cipher/src/main/java/RotationalCipher.java
+++ java/rotational-cipher/src/main/java/RotationalCipher.java
@@ -1,13 +1,59 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class RotationalCipher {
 
   private int shift;
+  private Map<Character, Character> cipher;
+
+  private static int ALPHABET_LENGTH = 26;
+  private static char[] LOWERCASE_ALPHABET = "abcdefghijklmnopqrstuvwxyz".toCharArray();
+  private static char[] UPPERCASE_ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray();
 
   public RotationalCipher(int shift) {
     this.shift = shift;
+    this.cipher = generateCipher(shift);
+//    System.out.println(cipher);
+  }
+
+  private Map<Character, Character> generateCipher(int shift) {
+    char[] lowercase_encrypted_alphabet = encrypt_alphabet(LOWERCASE_ALPHABET);
+    char[] uppercase_encrypted_alphabet = encrypt_alphabet(UPPERCASE_ALPHABET);
+
+    Map<Character, Character> cipher = new HashMap<>();
+
+    for (int i = 0; i < ALPHABET_LENGTH; i++) {
+      cipher.put(lowercase_encrypted_alphabet[i], LOWERCASE_ALPHABET[i]);
+      cipher.put(uppercase_encrypted_alphabet[i], UPPERCASE_ALPHABET[i]);
+    }
+
+    return cipher;
+  }
+
+  private char[] encrypt_alphabet(char[] alphabet) {
+    char[] encrypted = new char[ALPHABET_LENGTH];
+
+    for (int i = 0; i < alphabet.length; i++) {
+      encrypted[(i + shift) % alphabet.length] = alphabet[i];
+    }
+
+    return encrypted;
   }
 
   public String rotate(String text) {
-    return "b";
+    StringBuilder encrypted = new StringBuilder(text.length());
+    for(char c: text.toCharArray()) {
+      encrypted.append(encrypteChar(c));
+    }
+    return encrypted.toString();
+  }
+
+  private Character encrypteChar(Character c) {
+    if (cipher.containsKey(c)) {
+      return this.cipher.get(c);
+    } else {
+      return c;
+    }
   }
 
 }
