d2fbb7e14ec3c6e937ab08183a450aa46c71a5c1
Rootul Patel <rootulp@palantir.com>
2017-09-10 11:28:03 -0700

    Pass first test

---
+++ java/rotational-cipher/src/main/java/RotationalCipher.java
@@ -0,0 +1,13 @@
+public class RotationalCipher {
+
+  private int shift;
+
+  public RotationalCipher(int shift) {
+    this.shift = shift;
+  }
+
+  public String rotate(String text) {
+    return "b";
+  }
+
+}
