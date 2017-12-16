981c1a5cc5e3cb84b0d68bc504834c8b44d17fb7
Rootul Patel <rootulp@palantir.com>
2017-12-16 16:30:31 +0000

    Solve bob

--- typescript/bob/bob.test.ts
+++ typescript/bob/bob.test.ts
@@ -9,82 +9,82 @@ describe('Bob', () => {
     expect(result).toEqual('Whatever.')
   })
 
-  xit('shouting', () => {
+  it('shouting', () => {
     const result = bob.hey('WATCH OUT!')
     expect(result).toEqual('Whoa, chill out!')
   })
 
-  xit('asking a question', () => {
+  it('asking a question', () => {
     const result = bob.hey('Does this cryogenic chamber make me look fat?')
     expect(result).toEqual('Sure.')
   })
 
-  xit('talking forcefully', () => {
+  it('talking forcefully', () => {
     const result = bob.hey("Let's go make out behind the gym!")
     expect(result).toEqual('Whatever.')
   })
 
-  xit('using acronyms in regular speech', () => {
+  it('using acronyms in regular speech', () => {
     const result = bob.hey("It's OK if you don't want to go to the DMV.")
     expect(result).toEqual('Whatever.')
   })
 
-  xit('forceful questions', () => {
+  it('forceful questions', () => {
     const result = bob.hey('WHAT THE HELL WERE YOU THINKING?')
     expect(result).toEqual('Whoa, chill out!')
   })
 
-  xit('shouting numbers', () => {
+  it('shouting numbers', () => {
     const result = bob.hey('1, 2, 3 GO!')
     expect(result).toEqual('Whoa, chill out!')
   })
 
-  xit('only numbers', () => {
+  it('only numbers', () => {
     const result = bob.hey('1, 2, 3')
     expect(result).toEqual('Whatever.')
   })
 
-  xit('question with only numbers', () => {
+  it('question with only numbers', () => {
     const result = bob.hey('4?')
     expect(result).toEqual('Sure.')
   })
 
-  xit('shouting with special characters', () => {
+  it('shouting with special characters', () => {
     const result = bob.hey('ZOMG THE %^*@#$(*^ ZOMBIES ARE COMING!!11!!1!')
     expect(result).toEqual('Whoa, chill out!')
   })
 
-  xit('shouting with umlauts', () => {
+  it('shouting with umlauts', () => {
     const result = bob.hey('\xdcML\xc4\xdcTS!')
     expect(result).toEqual('Whoa, chill out!')
   })
 
-  xit('calmly speaking about umlauts', () => {
+  it('calmly speaking about umlauts', () => {
     const result = bob.hey('\xfcML\xe4\xdcTS')
     expect(result).toEqual('Whatever.')
   })
 
-  xit('shouting with no exclamation mark', () => {
+  it('shouting with no exclamation mark', () => {
     const result = bob.hey('I HATE YOU')
     expect(result).toEqual('Whoa, chill out!')
   })
 
-  xit('statement containing question mark', () => {
+  it('statement containing question mark', () => {
     const result = bob.hey('Ending with a ? means a question.')
     expect(result).toEqual('Whatever.')
   })
 
-  xit('prattling on', () => {
+  it('prattling on', () => {
     const result = bob.hey('Wait! Hang on.  Are you going to be OK?')
     expect(result).toEqual('Sure.')
   })
 
-  xit('silence', () => {
+  it('silence', () => {
     const result = bob.hey('')
     expect(result).toEqual('Fine. Be that way!')
   })
 
-  xit('prolonged silence', () => {
+  it('prolonged silence', () => {
     const result = bob.hey('   ')
     expect(result).toEqual('Fine. Be that way!')
   })
--- typescript/bob/bob.ts
+++ typescript/bob/bob.ts
@@ -1,6 +1,28 @@
 class Bob {
-    hey( /* Parameters go here */ ) {
-        // Your code here
+    static readonly RESPONSE_TO_YELLING = "Whoa, chill out!"
+    static readonly RESPONSE_TO_QUESTION = "Sure."
+    static readonly RESPONSE_TO_SILENCE = "Fine. Be that way!"
+    public hey(input: string): string {
+        if (this.is_yelling(input)) {
+            return Bob.RESPONSE_TO_YELLING;
+        } else if (this.is_question(input)) {
+            return Bob.RESPONSE_TO_QUESTION;
+        } else if (this.is_silence(input)) {
+            return Bob.RESPONSE_TO_SILENCE;
+        }
+        return "Whatever.";
+    }
+
+    private is_yelling(input: string): boolean {
+        return input === input.toUpperCase() && input !== input.toLowerCase();
+    }
+
+    private is_question(input: string): boolean {
+        return input.endsWith("?");
+    }
+
+    private is_silence(input: string): boolean {
+        return input.trim().length === 0;
     }
 }
 
