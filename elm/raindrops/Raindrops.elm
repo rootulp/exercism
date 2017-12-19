fab1fb02800d0d845be2c30a18208c0e2c690a1a
Rootul Patel <rootulp@palantir.com>
2017-12-19 19:14:50 +0000

    Solve raindrops

--- elm/raindrops/Raindrops.elm
+++ elm/raindrops/Raindrops.elm
@@ -2,14 +2,18 @@ module Raindrops exposing (..)
 
 raindrops: Int -> String
 raindrops number =
-    if isFactor 3 number then
-        "Pling"
-    else if isFactor 5 number then
-        "Plang"
-    else if isFactor 7 number then
-        "Plong"
+    if isFactor 3 number || isFactor 5 number || isFactor 7 number then
+        plingPlangPlong number
     else
         toString number
+
+plingPlangPlong: Int -> String
+plingPlangPlong number =
+    String.join "" [
+        if isFactor 3 number then "Pling" else "",
+        if isFactor 5 number then "Plang" else "",
+        if isFactor 7 number then "Plong" else ""
+    ]
 
 isFactor: Int -> Int -> Bool
 isFactor potentialFactor number =
--- elm/raindrops/tests/Tests.elm
+++ elm/raindrops/tests/Tests.elm
@@ -22,34 +22,34 @@ tests =
         ,
             test "6" <|
                 \() -> Expect.equal "Pling" (raindrops 6)
-        , skip <|
+        ,
             test "9" <|
                 \() -> Expect.equal "Pling" (raindrops 9)
-        , skip <|
+        ,
             test "10" <|
                 \() -> Expect.equal "Plang" (raindrops 10)
-        , skip <|
+        ,
             test "14" <|
                 \() -> Expect.equal "Plong" (raindrops 14)
-        , skip <|
+        ,
             test "15" <|
                 \() -> Expect.equal "PlingPlang" (raindrops 15)
-        , skip <|
+        ,
             test "21" <|
                 \() -> Expect.equal "PlingPlong" (raindrops 21)
-        , skip <|
+        ,
             test "25" <|
                 \() -> Expect.equal "Plang" (raindrops 25)
-        , skip <|
+        ,
             test "35" <|
                 \() -> Expect.equal "PlangPlong" (raindrops 35)
-        , skip <|
+        ,
             test "49" <|
                 \() -> Expect.equal "Plong" (raindrops 49)
-        , skip <|
+        ,
             test "52" <|
                 \() -> Expect.equal "52" (raindrops 52)
-        , skip <|
+        ,
             test "105" <|
                 \() -> Expect.equal "PlingPlangPlong" (raindrops 105)
         ]
