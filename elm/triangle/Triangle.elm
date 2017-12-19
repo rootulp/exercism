e4633323959736bed837affd18a1cd803d9138d0
Rootul Patel <rootulp@palantir.com>
2017-12-19 21:52:19 +0000

    Solve Triangle

--- elm/triangle/Triangle.elm
+++ elm/triangle/Triangle.elm
@@ -4,9 +4,42 @@ module Triangle exposing (..)
 type Triangle = Equilateral | Isosceles | Scalene
 
 
-triangleKind: a -> b -> c -> Result String Triangle
+triangleKind: comparable -> comparable -> comparable -> Result String Triangle
 triangleKind a b c =
-    (Ok Equilateral)
+    if invalidLengths a b c then
+        (Err "Invalid lengths")
+    else if violatesInequality a b c then
+        (Err "Violates inequality")
+    else if isEquilateral a b c then
+        (Ok Equilateral)
+    else if isIsosceles a b c then
+        (Ok Isosceles)
+    else
+        (Ok Scalene)
+
+
+isEquilateral: comparable -> comparable -> comparable -> Bool
+isEquilateral a b c =
+    a == b  && b == c
+
+
+isIsosceles: comparable -> comparable -> comparable -> Bool
+isIsosceles a b c =
+    (a == b && b /= c) ||
+    (a == c && c /= b) ||
+    (b == c && c /= a)
+
+
+invalidLengths: comparable -> comparable -> comparable -> Bool
+invalidLengths a b c =
+    a <= 0 || b <= 0 || c <= 0
+
+
+violatesInequality: comparable -> comparable -> comparable -> Bool
+violatesInequality a b c =
+    (a + b <= c) ||
+    (a + c <= b) ||
+    (b + c <= a)
 
 
 version : Int
