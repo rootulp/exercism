4755a646d284a1bef719f1b47fba85515f3395b1
Rootul Patel <rootulp@palantir.com>
2017-12-19 19:37:40 +0000

    Solve Pangram

--- elm/pangram/Pangram.elm
+++ elm/pangram/Pangram.elm
@@ -1,1 +1,11 @@
 module Pangram exposing (..)
+
+import Set exposing (..)
+
+isPangram: String -> Bool
+isPangram sentence =
+    let
+        alphabet: Set Char
+        alphabet = Set.fromList (String.toList "ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+    in
+        Set.isEmpty (Set.diff alphabet (Set.fromList (String.toList (String.toUpper sentence))))
