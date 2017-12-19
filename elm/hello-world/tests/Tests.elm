4284eb9f85b2d240efe92920b46bc33b1d767677
Rootul Patel <rootulp@palantir.com>
2017-12-19 16:10:42 +0000

    Solve hello world

--- elm/hello-world/tests/Tests.elm
+++ elm/hello-world/tests/Tests.elm
@@ -11,15 +11,11 @@ tests =
         [ test "Hello with no name" <|
             \() ->
                 Expect.equal "Hello, World!" (helloWorld Nothing)
-
-        -- Once you get the first test passing, remove the
-        -- `skip <|` (just leave the comma) on the next two
-        -- lines to continue!
-        , skip <|
+        ,
             test "Hello to a sample name" <|
                 \() ->
                     Expect.equal "Hello, Alice!" (helloWorld (Just "Alice"))
-        , skip <|
+        ,
             test "Hello to another sample name" <|
                 \() ->
                     Expect.equal "Hello, Bob!" (helloWorld (Just "Bob"))
