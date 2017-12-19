aadb4b68f84d2c13f882ba36d3caa9d607d7fd07
Rootul Patel <rootulp@palantir.com>
2017-12-19 20:21:15 +0000

    Refactor to use backward function application

--- elm/pangram/Pangram.elm
+++ elm/pangram/Pangram.elm
@@ -2,10 +2,17 @@ module Pangram exposing (..)
 
 import Set exposing (..)
 
+
 isPangram: String -> Bool
 isPangram sentence =
-    Set.isEmpty (Set.diff alphabet (Set.fromList (String.toList (String.toUpper sentence))))
+    Set.isEmpty
+        <| Set.diff alphabet
+        <| Set.fromList
+        <| String.toList
+        <| String.toUpper sentence
+
 
 alphabet: Set Char
 alphabet =
-    Set.fromList (String.toList "ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+    Set.fromList
+        <| String.toList "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
