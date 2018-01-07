fccdcc092dbf5d196546c0e619a1a62354ab1469
Rootul Patel <rootulp@palantir.com>
2018-01-07 19:24:57 +0000

    Solve Anagram

--- elm/anagram/Anagram.elm
+++ elm/anagram/Anagram.elm
@@ -2,4 +2,14 @@ module Anagram exposing (..)
 
 detect: String -> List String -> List String
 detect word possibleAnagrams =
-    List.filter (\ possibleAnagram -> List.sort (String.toList possibleAnagram) == List.sort (String.toList word)) possibleAnagrams
+    List.filter (\ possibleAnagram ->  isAnagram (String.toLower possibleAnagram) (String.toLower word)) possibleAnagrams
+
+isAnagram: String -> String -> Bool
+isAnagram word possibleAnagram =
+     word /= possibleAnagram &&
+    sortedCharacters  word == sortedCharacters possibleAnagram
+
+sortedCharacters: String -> List Char
+sortedCharacters word =
+    String.toList word
+        |> List.sort
--- elm/anagram/tests/Tests.elm
+++ elm/anagram/tests/Tests.elm
@@ -42,67 +42,67 @@ tests =
                 \() ->
                     Expect.equal [ "gallery", "regally", "largely" ]
                         (detect "allergy" [ "gallery", "ballerina", "regally", "clergy", "largely", "leading" ])
-        , skip <|
+        ,
             test "does not detect indentical words" <|
                 \() ->
                     Expect.equal [ "cron" ]
                         (detect "corn" [ "corn", "dark", "Corn", "rank", "CORN", "cron", "park" ])
-        , skip <|
+        ,
             test "does not detect non-anagrams with identical checksum" <|
                 \() ->
                     Expect.equal []
                         (detect "mass" [ "last" ])
-        , skip <|
+        ,
             test "detects anagrams case-insensitively" <|
                 \() ->
                     Expect.equal [ "Carthorse" ]
                         (detect "Orchestra" [ "cashregister", "Carthorse", "radishes" ])
-        , skip <|
+        ,
             test "detects anagrams using case-insensitive subject" <|
                 \() ->
                     Expect.equal [ "carthorse" ]
                         (detect "Orchestra" [ "cashregister", "carthorse", "radishes" ])
-        , skip <|
+        ,
             test "detects anagrams using case-insensitve possible matches" <|
                 \() ->
                     Expect.equal [ "Carthorse" ]
                         (detect "orchestra" [ "cashregister", "Carthorse", "radishes" ])
-        , skip <|
+        ,
             test "does not detect a word as its own anagram" <|
                 \() ->
                     Expect.equal []
                         (detect "banana" [ "Banana" ])
-        , skip <|
+        ,
             test "does not detect a anagram if the original word is repeated" <|
                 \() ->
                     Expect.equal []
                         (detect "go" [ "go Go GO" ])
-        , skip <|
+        ,
             test "anagrams must use all letters exactly once (go)" <|
                 \() ->
                     Expect.equal []
                         (detect "tapper" [ "patter" ])
-        , skip <|
+        ,
             test "eliminates anagrams with the same checksum" <|
                 \() ->
                     Expect.equal []
                         (detect "mass" [ "last" ])
-        , skip <|
+        ,
             test "detects unicode anagrams" <|
                 \() ->
                     Expect.equal [ "ΒΓΑ", "γβα" ]
                         (detect "ΑΒΓ" [ "ΒΓΑ", "ΒΓΔ", "γβα" ])
-        , skip <|
+        ,
             test "eliminates misleading unicode anagrams" <|
                 \() ->
                     Expect.equal []
                         (detect "ΑΒΓ" [ "ABΓ" ])
-        , skip <|
+        ,
             test "capital word is not own anagram" <|
                 \() ->
                     Expect.equal []
                         (detect "BANANA" [ "Banana" ])
-        , skip <|
+        ,
             test "anagrams must use all letters exactly once (banana)" <|
                 \() ->
                     Expect.equal []
