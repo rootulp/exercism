6d31653d0230c8bac26d5c4f8981d612c1e31d59
Rootul Patel <rootulp@palantir.com>
2018-01-07 19:35:42 +0000

    Refactor Anagram

--- elm/anagram/Anagram.elm
+++ elm/anagram/Anagram.elm
@@ -2,11 +2,19 @@ module Anagram exposing (..)
 
 detect: String -> List String -> List String
 detect word possibleAnagrams =
-    List.filter (\ possibleAnagram ->  isAnagram (String.toLower possibleAnagram) (String.toLower word)) possibleAnagrams
+    List.filter (findAnagrams word) possibleAnagrams
+
+findAnagrams: String -> (String -> Bool)
+findAnagrams word =
+    (\ possibleAnagram -> isAnagramCaseInsensitive word possibleAnagram)
+
+isAnagramCaseInsensitive: String -> String -> Bool
+isAnagramCaseInsensitive word possibleAnagram =
+    isAnagram (String.toLower word) (String.toLower possibleAnagram)
 
 isAnagram: String -> String -> Bool
 isAnagram word possibleAnagram =
-     word /= possibleAnagram &&
+    word /= possibleAnagram &&
     sortedCharacters  word == sortedCharacters possibleAnagram
 
 sortedCharacters: String -> List Char
