b852e44f317e4ac5170142c28fd47c7487047319
Rootul Patel <rootulp@palantir.com>
2018-01-08 19:20:24 +0000

    Solve space age

--- elm/space-age/SpaceAge.elm
+++ elm/space-age/SpaceAge.elm
@@ -10,6 +10,18 @@ type Planet
   | Uranus
   | Neptune
 
-ageOn : Planet -> Int -> Float
+ageOn: Planet -> Float -> Float
 ageOn planet ageInSeconds =
-  32
+  ageInSeconds / orbitalPeriod planet
+
+orbitalPeriod: Planet -> Float
+orbitalPeriod planet =
+  case planet of
+    Mercury -> 7600530.24
+    Venus -> 19413907.2
+    Earth -> 31558149.76
+    Mars -> 59354294.4
+    Jupiter -> 374335776.0
+    Saturn -> 929596608.0
+    Uranus -> 2661041808.0
+    Neptune -> 5200418592.0
