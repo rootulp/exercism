13a3df200e57c169cea6a4278713d92c6bcd3f97
Rootul Patel <rootulp@palantir.com>
2018-01-09 20:50:19 +0000

    Implement filter

--- elm/strain/Strain.elm
+++ elm/strain/Strain.elm
@@ -4,8 +4,13 @@ type alias Predicate comparable = (comparable -> Bool)
 
 keep: Predicate comparable -> List comparable -> List comparable
 keep predicate collection =
-    List.filter predicate collection
-
+    case collection of
+        [] -> []
+        comparable::rest ->
+            if predicate comparable then
+                comparable :: keep predicate rest
+            else
+                keep predicate rest
 
 discard: Predicate comparable -> List comparable -> List comparable
 discard predicate collection =
