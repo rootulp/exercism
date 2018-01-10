92124214032f56747ef19ae5fc133b3a62cfe5de
Rootul Patel <rootulp@palantir.com>
2018-01-10 19:44:10 +0000

    Pass first 3 tests

---
+++ elm/difference-of-squares/DifferenceOfSquares.elm
@@ -0,0 +1,13 @@
+module DifferenceOfSquares exposing (..)
+
+squareOfSum: Int -> Int
+squareOfSum n =
+    List.sum(List.range 0 n)^2
+
+sumOfSquares: number -> number
+sumOfSquares n =
+    n
+
+difference: number -> number
+difference n =
+    n
