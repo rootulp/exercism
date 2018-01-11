ea6f440e8fa77c2b3e0bab122666de2849e10d63
Rootul Patel <rootulp@palantir.com>
2018-01-11 23:10:19 +0000

    WIP

--- elm/word-count/WordCount.elm
+++ elm/word-count/WordCount.elm
@@ -5,4 +5,4 @@ wordCount: String -> Dict String Int
 wordCount sentence =
     String.words sentence
         |> List.map (\word -> (word, 1))
-        |> Dict.fromList
+        |> List.foldl (\word dict -> Dict.update word Dict.get word dict)
