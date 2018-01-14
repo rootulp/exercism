2c0662e87d3e939e5094953aa344eff4036c593d
Rootul Patel <rootulp@palantir.com>
2018-01-14 20:56:33 +0000

    Pass 12 tests

--- elm/hamming/Hamming.elm
+++ elm/hamming/Hamming.elm
@@ -2,4 +2,15 @@ module Hamming exposing (..)
 
 distance: String -> String -> Maybe Int
 distance strand1 strand2 =
-    Just 0
+    List.map2 nucleotideDistance
+        (String.toList strand1)
+        (String.toList strand2)
+        |> List.sum
+        |> Just
+
+nucleotideDistance: Char -> Char -> Int
+nucleotideDistance nucleotide1 nucleotide2 =
+    if nucleotide1 == nucleotide2 then
+        0
+    else
+        1
