6a5da2aab4f304751d642da1557116e48946d832
Rootul Patel <rootulp@palantir.com>
2018-01-17 22:18:26 +0000

    Pass first two tests

--- elm/run-length-encoding/RunLengthEncoding.elm
+++ elm/run-length-encoding/RunLengthEncoding.elm
@@ -1,6 +1,29 @@
 module RunLengthEncoding exposing (..)
+import Maybe
+
+encode: String -> String
+encode data =
+  data
+    |> String.toList
+    |> List.foldl splitIntoChunks []
+    |> List.reverse
+    |> List.map (\chunk -> String.append (toString (String.length chunk)) (String.left 1 chunk))
+    |> String.join ""
+    -- |> String.fromList
+
+splitIntoChunks: (Char -> List String -> List String)
+splitIntoChunks char chunks =
+  if String.startsWith (String.fromChar char) (Maybe.withDefault "" (List.head chunks)) then
+    String.cons char (Maybe.withDefault "" (List.head chunks)) :: Maybe.withDefault [] (List.tail chunks)
+  else
+    String.fromChar char :: chunks
+
+
+decode: String -> String
+decode data =
+  data
 
 
 version : Int
 version =
-  2  
+  2
