50dcfd4640f121ba71f9ce5bea37adf2ba279b58
Rootul Patel <rootulp@palantir.com>
2018-01-17 22:29:28 +0000

    Extract encodeRun

--- elm/run-length-encoding/RunLengthEncoding.elm
+++ elm/run-length-encoding/RunLengthEncoding.elm
@@ -5,23 +5,33 @@ encode: String -> String
 encode data =
   data
     |> String.toList
-    |> List.foldl splitRuns []
-    |> List.reverse
-    |> List.map (\chunk -> String.append (toString (String.length chunk)) (String.left 1 chunk))
+    |> List.foldr splitRuns []
+    |> List.map encodeRun
     |> String.join ""
+
+encodeRun: String -> String
+encodeRun run =
+  String.append (toString (String.length run)) (String.left 1 run)
+
 
 splitRuns: (Char -> List String -> List String)
 splitRuns char runs =
-  if String.startsWith (String.fromChar char) (lastRun runs) then
-    String.cons char (lastRun runs) :: Maybe.withDefault [] (List.tail runs)
+  if String.startsWith (String.fromChar char) (previousRun runs) then
+    String.cons char (previousRun runs) :: (restOfRuns runs)
   else
     String.fromChar char :: runs
 
-lastRun: List String -> String
-lastRun runs =
+previousRun: List String -> String
+previousRun runs =
   runs
     |> List.head
     |> Maybe.withDefault ""
+
+restOfRuns: List String -> List String
+restOfRuns runs =
+  runs
+    |> List.tail
+    |> Maybe.withDefault []
 
 
 decode: String -> String
