6b0c881d81e20e3caf9b1546e1b88ffcb9111a56
Rootul Patel <rootulp@palantir.com>
2018-01-22 19:14:46 +0000

    Pass first test

--- elm/sublist/Sublist.elm
+++ elm/sublist/Sublist.elm
@@ -1,6 +1,14 @@
 module Sublist exposing (..)
 
 
+type ListComparison = Equal | Unequal | Sublist | Superlist
+
+
+sublist: List Int -> List Int -> ListComparison
+sublist list1 list2 =
+    Equal
+
+
 version : Int
 version =
     2
