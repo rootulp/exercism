8d0b23675c6a090faf8e8068ae1b49c3962fc2e0
Rootul Patel <rootulp@palantir.com>
2018-01-23 18:02:27 +0000

    Pass 17 tests

--- elm/sublist/Sublist.elm
+++ elm/sublist/Sublist.elm
@@ -6,7 +6,14 @@ type ListComparison = Equal | Unequal | Sublist | Superlist
 
 sublist: List Int -> List Int -> ListComparison
 sublist list1 list2 =
-    Equal
+    if list1 == list2 then
+        Equal
+    else if List.all (\element -> List.member element list2) list1 then
+        Sublist
+    else if List.all (\element -> List.member element list1) list2 then
+        Superlist
+    else
+        Unequal
 
 
 version : Int
