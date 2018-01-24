522ec714d22dc646492ed84142b0925697adce1f
Rootul Patel <rootulp@palantir.com>
2018-01-24 19:29:04 +0000

    Extract isSublist method

--- elm/sublist/Sublist.elm
+++ elm/sublist/Sublist.elm
@@ -8,12 +8,18 @@ sublist: List Int -> List Int -> ListComparison
 sublist list1 list2 =
     if list1 == list2 then
         Equal
-    else if List.all (\element -> List.member element list2) list1 then
+    else if isSublist list1 list2 then
         Sublist
-    else if List.all (\element -> List.member element list1) list2 then
+    else if isSublist list2 list1 then
         Superlist
     else
         Unequal
+
+
+isSublist: List Int -> List Int -> Bool
+isSublist list1 list2 =
+    list1
+        |> List.all (\element -> List.member element list2)
 
 
 version : Int
