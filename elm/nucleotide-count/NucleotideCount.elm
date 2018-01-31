d7dbbf8d601d6896e6492a4838777fdf10c57bd0
Rootul Patel <rootulp@palantir.com>
2018-01-31 18:12:14 +0000

    Pass first test

--- elm/nucleotide-count/NucleotideCount.elm
+++ elm/nucleotide-count/NucleotideCount.elm
@@ -1,5 +1,20 @@
 module NucleotideCount exposing (..)
 
+-- import Record exposing (Record)
+type alias NucleotideCounts = {
+  a : Int,
+  t : Int,
+  c : Int,
+  g : Int
+}
+
+nucleotideCounts: String -> NucleotideCounts
+nucleotideCounts strand =
+  { a = 0
+  , t = 0
+  , c = 0
+  , g = 0
+  }
 
 version : Int
 version =
