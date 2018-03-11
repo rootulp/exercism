0493bb4899b96c5263ec2b48614cd73fb63c71cd
Rootul Patel <rootulp@palantir.com>
2018-03-11 18:46:30 +0000

    More class methods

--- python/isbn-verifier/isbn_verifier.py
+++ python/isbn-verifier/isbn_verifier.py
@@ -9,7 +9,7 @@ class IsbnVerifier(object):
             return False
         sum_so_far = 0
         for i, c in enumerate(cls.remove_slashes(string)):
-            sum_so_far += IsbnVerifier.convert_char_to_int(c) * (10 - i)
+            sum_so_far += cls.convert_char_to_int(c) * (10 - i)
         return sum_so_far % 11 == 0
 
     @classmethod
@@ -28,17 +28,17 @@ class IsbnVerifier(object):
     def remove_invalid_characters(cls, string):
         return "".join(filter(lambda char: char in cls.VALID_CHARACTERS, string))
 
+    @classmethod
+    def convert_char_to_int(cls, char):
+        return int(cls.convert_x_to_ten(char))
+
+    @classmethod
+    def convert_x_to_ten(cls, char):
+        return 10 if char == 'X' else char
+
     @staticmethod
     def remove_slashes(string):
         return "".join(filter(lambda char: char != "-" , string))
-
-    @staticmethod
-    def convert_char_to_int(char):
-        return int(IsbnVerifier.convert_x_to_ten(char))
-
-    @staticmethod
-    def convert_x_to_ten(char):
-        return 10 if char == 'X' else char
 
 
 def verify(isbn):
