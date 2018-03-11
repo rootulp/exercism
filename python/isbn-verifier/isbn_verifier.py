972dc52d62a573add4cf2ebcfea20ed47d92c4a7
Rootul Patel <rootulp@palantir.com>
2018-03-11 18:38:31 +0000

    Fail if not 10 characters

--- python/isbn-verifier/isbn_verifier.py
+++ python/isbn-verifier/isbn_verifier.py
@@ -9,20 +9,24 @@ class IsbnVerifier(object):
         if self.invalid():
             return False
         sum_so_far = 0
-        for i, c in enumerate(IsbnVerifier.remove_slashes(self.string)):
+        for i, c in enumerate(self.remove_slashes(self.string)):
             sum_so_far += IsbnVerifier.convert_char_to_int(c) * (10 - i)
         return sum_so_far % 11 == 0
 
     def invalid(self):
-        return self.invalid_character()
+        return self.invalid_character() or self.invalid_length()
 
     def invalid_character(self):
         return any(char not in self.VALID_CHARACTERS for char in self.string)
 
+    def invalid_length(self):
+        return len(self.remove_invalid_characters(self.string)) != 10
 
-    @staticmethod
-    def remove_slashes(string):
-        return "".join(filter(lambda char: char != "-", string))
+    def remove_invalid_characters(self, string):
+        return "".join(filter(lambda char: char in self.VALID_CHARACTERS, string))
+
+    def remove_slashes(self, string):
+        return "".join(filter(lambda char: char != "-" , string))
 
     @staticmethod
     def convert_char_to_int(char):
