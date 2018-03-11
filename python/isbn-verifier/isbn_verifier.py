25cb7b109bb5cd53d7297070579268382693116b
Rootul Patel <rootulp@palantir.com>
2018-03-11 18:44:17 +0000

    Convert to cls methods

--- python/isbn-verifier/isbn_verifier.py
+++ python/isbn-verifier/isbn_verifier.py
@@ -1,31 +1,35 @@
 class IsbnVerifier(object):
 
     VALID_CHARACTERS = set(["-", "X", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"])
+    VALID_LENGTH = 10
 
-    def __init__(self, string):
-        self.string = string
-
-    def is_valid(self):
-        if self.invalid():
+    @classmethod
+    def is_valid(cls, string):
+        if cls.invalid(string):
             return False
         sum_so_far = 0
-        for i, c in enumerate(self.remove_slashes(self.string)):
+        for i, c in enumerate(cls.remove_slashes(string)):
             sum_so_far += IsbnVerifier.convert_char_to_int(c) * (10 - i)
         return sum_so_far % 11 == 0
 
-    def invalid(self):
-        return self.invalid_character() or self.invalid_length()
+    @classmethod
+    def invalid(cls, string):
+        return cls.invalid_character(string) or cls.invalid_length(string)
 
-    def invalid_character(self):
-        return any(char not in self.VALID_CHARACTERS for char in self.string)
+    @classmethod
+    def invalid_character(cls, string):
+        return any(char not in cls.VALID_CHARACTERS for char in string)
 
-    def invalid_length(self):
-        return len(self.remove_invalid_characters(self.string)) != 10
+    @classmethod
+    def invalid_length(cls, string):
+        return len(cls.remove_invalid_characters(string)) != cls.VALID_LENGTH
 
-    def remove_invalid_characters(self, string):
-        return "".join(filter(lambda char: char in self.VALID_CHARACTERS, string))
+    @classmethod
+    def remove_invalid_characters(cls, string):
+        return "".join(filter(lambda char: char in cls.VALID_CHARACTERS, string))
 
-    def remove_slashes(self, string):
+    @staticmethod
+    def remove_slashes(string):
         return "".join(filter(lambda char: char != "-" , string))
 
     @staticmethod
@@ -37,7 +41,5 @@ class IsbnVerifier(object):
         return 10 if char == 'X' else char
 
 
-
-
 def verify(isbn):
-    return IsbnVerifier(isbn).is_valid()
+    return IsbnVerifier.is_valid(isbn)
