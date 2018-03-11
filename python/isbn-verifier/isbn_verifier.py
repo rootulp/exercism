d133913d11e5384eddc472bda63ca781d3adc532
Rootul Patel <rootulp@palantir.com>
2018-03-11 18:19:42 +0000

    Handle basic ISBN

--- python/isbn-verifier/isbn_verifier.py
+++ python/isbn-verifier/isbn_verifier.py
@@ -4,7 +4,23 @@ class IsbnVerifier(object):
         self.string = string
 
     def is_valid(self):
-        return True
+        sum_so_far = 0
+        for i, c in enumerate(IsbnVerifier.remove_slashes(self.string)):
+            sum_so_far += IsbnVerifier.convert_char_to_int(c) * (10 - i)
+        return sum_so_far % 11 == 0
+
+    @staticmethod
+    def remove_slashes(string):
+        return "".join(filter(lambda char: char != "-", string))
+
+    @staticmethod
+    def convert_char_to_int(char):
+        return int(IsbnVerifier.convert_x_to_ten(char))
+
+    @staticmethod
+    def convert_x_to_ten(char):
+        return 10 if char == 'X' else char
+
 
 
 
