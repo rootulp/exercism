2163b5a9d940a0481aea96ab10d4cf2da3d112ba
Rootul Patel <rootulp@palantir.com>
2018-03-11 19:04:00 +0000

    Use constants

--- python/isbn-verifier/isbn_verifier.py
+++ python/isbn-verifier/isbn_verifier.py
@@ -11,7 +11,7 @@ class IsbnVerifier(object):
         if cls.invalid(string):
             return False
         sum_so_far = 0
-        for i, c in enumerate(cls.remove_slashes(string)):
+        for i, c in enumerate(cls.remove_seperator(string)):
             sum_so_far += cls.convert_char_to_int(c) * (10 - i)
         return sum_so_far % 11 == 0
 
@@ -33,7 +33,7 @@ class IsbnVerifier(object):
 
     @classmethod
     def remove_invalid_characters_and_slashes(cls, string):
-        return cls.remove_slashes(cls.remove_invalid_characters(string))
+        return cls.remove_seperator(cls.remove_invalid_characters(string))
 
     @classmethod
     def remove_invalid_characters(cls, string):
@@ -47,9 +47,9 @@ class IsbnVerifier(object):
     def convert_x_to_ten(cls, char):
         return 10 if char == cls.VALID_CHECK_DIGIT else char
 
-    @staticmethod
-    def remove_slashes(string):
-        return "".join(filter(lambda char: char != "-" , string))
+    @classmethod
+    def remove_seperator(cls, string):
+        return "".join(filter(lambda char: char != cls.VALID_SEPERATOR , string))
 
 
 def verify(isbn):
