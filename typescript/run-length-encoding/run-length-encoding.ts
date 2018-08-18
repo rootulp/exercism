15a41d0060a2005799b1096b255a1f48d5cdd03f
Rootul Patel <rootulp@palantir.com>
2018-08-18 15:41:20 +0100

    Remove interface because encod and decode are static

--- typescript/run-length-encoding/run-length-encoding.ts
+++ typescript/run-length-encoding/run-length-encoding.ts
@@ -1,15 +1,9 @@
-interface IRunLengthEncoding {
-    encode(data: string): string
-    decode(encodedData: string): string
-
-}
-
-export default class RunLengthEncoding implements IRunLengthEncoding {
-    encode(data: string): string {
+export default class RunLengthEncoding {
+    static encode(data: string): string {
         throw new Error("Method not implemented.")
     }
 
-    decode(encodedData: string): string {
+    static decode(encodedData: string): string {
         throw new Error("Method not implemented.")
     }
 
