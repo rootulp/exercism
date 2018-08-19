5f5e7923c0dc07a7341eb99219bf05049d05ef2d
Rootul Patel <rootulp@palantir.com>
2018-08-19 12:06:53 +0100

    Initial impl of encode

--- typescript/run-length-encoding/run-length-encoding.test.ts
+++ typescript/run-length-encoding/run-length-encoding.test.ts
@@ -33,39 +33,39 @@ describe('run-length encode a string', () => {
 })
 
 describe('run-length decode a string', () => {
-    it('empty string', () => {
+    xit('empty string', () => {
         const expected = ''
         expect(RunLengthEncoding.decode('')).toEqual(expected)
     })
 
-    it('single characters only', () => {
+    xit('single characters only', () => {
         const expected = 'XYZ'
         expect(RunLengthEncoding.decode('XYZ')).toEqual(expected)
     })
 
-    it('string with no single characters', () => {
+    xit('string with no single characters', () => {
         const expected = 'AABBBCCCC'
         expect(RunLengthEncoding.decode('2A3B4C')).toEqual(expected)
     })
 
-    it('single characters with repeated characters', () => {
+    xit('single characters with repeated characters', () => {
         const expected = 'WWWWWWWWWWWWBWWWWWWWWWWWWBBBWWWWWWWWWWWWWWWWWWWWWWWWB'
         expect(RunLengthEncoding.decode('12WB12W3B24WB')).toEqual(expected)
     })
 
-    it('multiple whitespace mixed in string', () => {
+    xit('multiple whitespace mixed in string', () => {
         const expected = '  hsqq qww  '
         expect(RunLengthEncoding.decode('2 hs2q q2w2 ')).toEqual(expected)
     })
 
-    it('lower case string', () => {
+    xit('lower case string', () => {
         const expected = 'aabbbcccc'
         expect(RunLengthEncoding.decode('2a3b4c')).toEqual(expected)
     })
 })
 
 describe('encode and then decode', () => {
-    it('encode followed by decode gives original string', () => {
+    xit('encode followed by decode gives original string', () => {
         expect(RunLengthEncoding.decode(RunLengthEncoding.encode('zzz ZZ  zZ'))).toEqual('zzz ZZ  zZ')
     })
 })
--- typescript/run-length-encoding/run-length-encoding.ts
+++ typescript/run-length-encoding/run-length-encoding.ts
@@ -1,9 +1,33 @@
+import {last, isUndefined, startsWith, concat, slice} from "lodash"
+
 export default class RunLengthEncoding {
     static encode(data: string): string {
-        return data
+        const chunks = RunLengthEncoding.splitIntoConsecutiveCharacters(data)
+        return chunks.map((chunk) => RunLengthEncoding.encodeChunk(chunk)).join('')
     }
 
     static decode(encodedData: string): string {
         return encodedData
+    }
+
+    static splitIntoConsecutiveCharacters(data: string): string[] {
+        return data.split("").reduce((chunks: string[], currentCharacter: string) => {
+            const lastChunkOfConsecutiveCharacters: string | undefined = last(chunks)
+
+            if (!isUndefined(lastChunkOfConsecutiveCharacters) && startsWith(lastChunkOfConsecutiveCharacters, currentCharacter)) {
+                return concat(slice(chunks, 0, -1), last(chunks) + currentCharacter)
+            } else {
+                return concat(chunks, currentCharacter)
+            }
+        }, [])
+    }
+
+    static encodeChunk(chunk: string): string {
+        if (chunk.length === 1) {
+            return chunk
+        } else {
+            return `${chunk.length}${chunk.charAt(0)}`
+        }
+
     }
 }
