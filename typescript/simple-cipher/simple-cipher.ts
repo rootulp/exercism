2845c4355ca6cff0fda41314758a9959ca048971
Rootul Patel <rootulp@palantir.com>
2018-09-15 16:52:43 -0700

    Implement simple-cipher constructor

--- typescript/simple-cipher/simple-cipher.ts
+++ typescript/simple-cipher/simple-cipher.ts
@@ -1,10 +1,30 @@
 class SimpleCipher {
-    encode( /* Parameters go here */ ) {
-        // Your code here
+
+    static isValidKey(_inputKey: string): boolean {
+        return true
     }
 
-    decode( /* Parameters go here */ ) {
-        // Your code here
+    static generateRandomKey(): string {
+        return "foo"
+    }
+
+    public key: string
+
+    constructor(inputKey ?: string) {
+        if (inputKey === undefined) {
+            this.key = SimpleCipher.generateRandomKey()
+        } else if (SimpleCipher.isValidKey(inputKey)) {
+            this.key = inputKey
+        } else {
+            throw "Bad key"
+        }
+    }
+    public encode(data: string): string {
+        return data
+    }
+
+    public decode(encodedData: string): string {
+        return encodedData
     }
 }
 
