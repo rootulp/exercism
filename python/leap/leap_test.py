66536d01244afe2cd69c459b940346dff0fbb880
Rootul Patel <rootulp@palantir.com>
2018-11-18 15:44:21 -0800

    Download leap (solution file name changed)

--- python/leap/leap_test.py
+++ python/leap/leap_test.py
@@ -1,23 +1,26 @@
 import unittest
 
-from year import is_leap_year
+from leap import is_leap_year
 
 
-class YearTest(unittest.TestCase):
-    def test_leap_year(self):
+# Tests adapted from `problem-specifications//canonical-data.json` @ v1.4.0
+
+class LeapTest(unittest.TestCase):
+    def test_year_not_divisible_by_4(self):
+        self.assertIs(is_leap_year(2015), False)
+
+    def test_year_divisible_by_4_not_divisible_by_100(self):
         self.assertIs(is_leap_year(1996), True)
 
-    def test_non_leap_year(self):
-        self.assertIs(is_leap_year(1997), False)
+    def test_year_divisible_by_100_not_divisible_by_400(self):
+        self.assertIs(is_leap_year(2100), False)
 
-    def test_non_leap_even_year(self):
-        self.assertIs(is_leap_year(1998), False)
+    def test_year_divisible_by_400(self):
+        self.assertIs(is_leap_year(2000), True)
 
-    def test_century(self):
-        self.assertIs(is_leap_year(1900), False)
+    def test_year_divisible_by_200_not_divisible_by_400(self):
+        self.assertIs(is_leap_year(1800), False)
 
-    def test_exceptional_century(self):
-        self.assertIs(is_leap_year(2400), True)
 
 if __name__ == '__main__':
     unittest.main()
