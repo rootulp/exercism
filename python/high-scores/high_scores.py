adb13bb07377cb862b6ecac988fc9593ac7294f4
Rootul Patel <rootulp@palantir.com>
2018-11-18 16:28:06 -0800

    Solve high scores

--- python/high-scores/high_scores.py
+++ python/high-scores/high_scores.py
@@ -1,3 +1,23 @@
+import heapq
+
 class HighScores(object):
     def __init__(self, scores):
-        pass
+        self.scores = scores
+
+    def highest(self):
+        return max(self.scores)
+
+    def latest(self):
+        return self.scores[-1]
+
+    def report(self):
+        return f"Your latest score was {self.latest()}. That's {self.amount_short()}"
+
+    def amount_short(self):
+        if self.highest() == self.latest():
+            return "your personal best!"
+        else:
+            return f"{self.highest() - self.latest()} short of your personal best!"
+
+    def top(self):
+        return heapq.nlargest(3, self.scores)
