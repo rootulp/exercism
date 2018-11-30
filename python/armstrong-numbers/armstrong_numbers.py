ac2c1ce03a2afcb4ec09454178a399347a0aaf52
Rootul Patel <rootulp@palantir.com>
2018-11-29 20:55:48 -0800

    Convert to list comprehension
    - Seems more pythonic
    - Also add newline between functions b/c pycodestyle

--- python/armstrong-numbers/armstrong_numbers.py
+++ python/armstrong-numbers/armstrong_numbers.py
@@ -1,9 +1,10 @@
 def get_digits(number):
     return list(map(int, list(str(number))))
 
+
 def is_armstrong(number):
     digits = get_digits(number)
     num_digits = len(digits)
-    digits_raised_to_num_digits = map(lambda digit: digit ** num_digits, digits)
+    digits_raised_to_num_digits = [digit ** num_digits for digit in digits]
 
     return number == sum(digits_raised_to_num_digits)
