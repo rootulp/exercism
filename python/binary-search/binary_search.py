f067e93be21f3cbe17a252de5453e3248e4dd287
Rootul Patel <rootulp@palantir.com>
2018-12-02 18:12:00 -0800

    Fail fast on base case

--- python/binary-search/binary_search.py
+++ python/binary-search/binary_search.py
@@ -2,15 +2,15 @@ def binary_search(array, num_to_find):
     return binary_search_with_bounds(array, num_to_find, 0, len(array) - 1)
 
 def binary_search_with_bounds(array, num_to_find, left, right):
-    if (left <= right):
-        middle = left + (right - left) // 2
-        middle_value = array[middle]
-
-        if (num_to_find == middle_value):
-            return middle
-        elif (num_to_find < middle_value):
-            return binary_search_with_bounds(array, num_to_find, left, middle - 1)
-        elif (num_to_find > middle_value):
-            return binary_search_with_bounds(array, num_to_find, middle + 1, right)
-    else:
+    if (left > right):
         raise ValueError("Value not found")
+
+    middle = left + (right - left) // 2
+    middle_value = array[middle]
+
+    if (num_to_find == middle_value):
+        return middle
+    elif (num_to_find < middle_value):
+        return binary_search_with_bounds(array, num_to_find, left, middle - 1)
+    elif (num_to_find > middle_value):
+        return binary_search_with_bounds(array, num_to_find, middle + 1, right)
