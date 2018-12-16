956740bdec525f3713d0b6e1a5f3e1fb232b2694
Rootul Patel <rootulp@palantir.com>
2018-12-16 15:37:41 -0800

    Translate a single codon

--- python/protein-translation/protein_translation.py
+++ python/protein-translation/protein_translation.py
@@ -1,2 +1,32 @@
+# Codon                 | Protein
+# :---                  | :---
+# AUG                   | Methionine
+# UUU, UUC              | Phenylalanine
+# UUA, UUG              | Leucine
+# UCU, UCC, UCC, UCC    | Serine
+# UAU, UAC              | Tyrosine
+# UGU, UGC              | Cysteine
+# UGG                   | Tryptophan
+# UAA, UAG, UGA         | STOP
+
+CODON_TO_PROTEIN = {
+    "AUG": "Methionine",
+    "UUU": "Phenylalanine",
+    "UUC": "Phenylalanine",
+    "UUA": "Leucine",
+    "UUG": "Leucine",
+    "UCU": "Serine",
+    "UCC": "Serine",
+    "UCC": "Serine",
+    "UCC": "Serine",
+    "UGU": "Cysteine",
+    "UGC": "Cysteine",
+    "UGG": "Tryptophan",
+    "UAA": "STOP",
+    "UAG": "STOP",
+    "UGA": "STOP"
+}
+
+
 def proteins(strand):
-    pass
+    return [CODON_TO_PROTEIN[strand]]
