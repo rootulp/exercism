b7b3f280e0494af79340821f49b46dda648ea052
Rootul Patel <rootulp@palantir.com>
2018-12-28 16:52:07 -0500

    Extract conversion

--- python/raindrops/raindrops.py
+++ python/raindrops/raindrops.py
@@ -1,4 +1,10 @@
 def raindrops(number):
+    raindropMessage = convert_to_raindrop_message(number)
+    if raindropMessage:
+        return raindropMessage
+    return "{}".format(number)
+
+def convert_to_raindrop_message(number):
     raindropMessage = ""
     if is_three_a_factor(number):
         raindropMessage += "Pling"
@@ -6,9 +12,7 @@ def raindrops(number):
         raindropMessage += "Plang"
     if is_seven_a_factor(number):
         raindropMessage += "Plong"
-    if raindropMessage:
-        return raindropMessage
-    return "{}".format(number)
+    return raindropMessage
 
 def is_three_a_factor(number):
     return number % 3 == 0
