f88c2135ddc197283bbfb8b481774deb613571cf
Rootul Patel <rootulp@palantir.com>
2018-12-28 16:43:46 -0500

    Handle 5 as a factor

--- python/raindrops/raindrops.py
+++ python/raindrops/raindrops.py
@@ -1,7 +1,12 @@
 def raindrops(number):
     if is_three_a_factor(number):
         return "Pling"
+    if is_five_a_factor(number):
+        return "Plang"
     return "{}".format(number)
 
 def is_three_a_factor(number):
     return number % 3 == 0
+
+def is_five_a_factor(number):
+    return number % 5 == 0
