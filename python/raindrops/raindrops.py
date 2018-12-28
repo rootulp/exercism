f802111f1f241444f874119e5949f3da4abd1c85
Rootul Patel <rootulp@palantir.com>
2018-12-28 16:44:15 -0500

    Handle 7 as a factor

--- python/raindrops/raindrops.py
+++ python/raindrops/raindrops.py
@@ -3,6 +3,8 @@ def raindrops(number):
         return "Pling"
     if is_five_a_factor(number):
         return "Plang"
+    if is_seven_a_factor(number):
+        return "Plong"
     return "{}".format(number)
 
 def is_three_a_factor(number):
@@ -10,3 +12,6 @@ def is_three_a_factor(number):
 
 def is_five_a_factor(number):
     return number % 5 == 0
+
+def is_seven_a_factor(number):
+    return number % 7 == 0
