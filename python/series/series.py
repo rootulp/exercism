e72a40b4dec723da1f6b582b1d6f25bda3b79a05
Rootul Patel <rootulp@palantir.com>
2019-01-05 13:48:20 -0800

    Solve again

--- python/series/series.py
+++ python/series/series.py
@@ -2,13 +2,4 @@ def slices(string, size):
     if size <= 0 or size > len(string):
         raise ValueError('Invalid slice size')
 
-    slices = []
-    for indx, elem in enumerate(string):
-        if len(string) - indx >= size:
-            curr_slice = []
-            curr_indx = indx
-            while len(curr_slice) < size:
-                curr_slice.append(int(string[curr_indx]))
-                curr_indx += 1
-            slices.append(curr_slice)
-    return slices
+    return [string[i:i+size] for i in range(len(string) - size + 1)]
