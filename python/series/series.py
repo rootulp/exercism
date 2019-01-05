450f4de523671c163b704ab3caf1b7db37e408d8
Rootul Patel <rootulp@palantir.com>
2019-01-05 13:37:48 -0800

    Refactor series to use list comprehension

--- python/series/series.py
+++ python/series/series.py
@@ -2,13 +2,5 @@ def slices(string, length):
     if length <= 0 or length > len(string):
         raise ValueError('Invalid slice length')
 
-    slices = []
-    for indx, _elem in enumerate(string):
-        if len(string) - indx >= length:
-            curr_slice = []
-            curr_indx = indx
-            while len(curr_slice) < length:
-                curr_slice.append(int(string[curr_indx]))
-                curr_indx += 1
-            slices.append(curr_slice)
-    return slices
+    nums = list(map(int, list(string)))
+    return [nums[i : i + length] for i in range(len(nums) - length + 1)]
