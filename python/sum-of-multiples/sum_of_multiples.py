b4adf443c32d6bdcfff81c517728daab02dea878
Rootul Patel <rootulp@palantir.com>
2019-01-12 14:27:36 -0800

    Start from scratch

--- python/sum-of-multiples/sum_of_multiples.py
+++ python/sum-of-multiples/sum_of_multiples.py
@@ -1,10 +1,2 @@
-def sum_of_multiples(limit, factors=[3, 5]):
-    mult = set()
-    for factor in factors:
-        if factor <= 0:
-            continue
-        for curr_mult in range(1, limit / factor + 1):
-            if factor * curr_mult < limit:
-                mult.add(factor * curr_mult)
-
-    return 0 if len(mult) == 0 else reduce(lambda x, y: x + y, mult)
+def sum_of_multiples():
+    pass
