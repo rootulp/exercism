d9ec7a60fc910caaef3e278736e30a8c9f2ce171
Rootul Patel <rootulp@palantir.com>
2019-01-12 15:18:40 -0800

    Refactor: flatten list / list comprehension

--- python/sum-of-multiples/sum_of_multiples.py
+++ python/sum-of-multiples/sum_of_multiples.py
@@ -1,16 +1,16 @@
+from itertools import chain
+
+
 def sum_of_multiples(limit, factors):
     return sum(all_multiples(limit, factors))
 
 
 def all_multiples(limit, factors):
-    multiples = set()
-    for factor in factors:
-        multiples = multiples.union(get_multiples(limit, factor))
-    return multiples
+    multiples = [get_multiples(limit, factor) for factor in factors]
+    return set(list(chain(*multiples)))  # remove duplicates
+
 
 def get_multiples(limit, factor):
     if factor == 0:
         return []
-
     return [multiple for multiple in range(limit) if multiple % factor == 0]
-
