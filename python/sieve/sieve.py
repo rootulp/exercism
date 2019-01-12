2c9e6827e8951c0a2e6bb9e1d8d6c415e1db4a8c
Rootul Patel <rootulp@palantir.com>
2019-01-12 14:23:37 -0800

    Re-solve sieve

--- python/sieve/sieve.py
+++ python/sieve/sieve.py
@@ -3,8 +3,8 @@ def sieve(limit):
     unmarked = list(range(2, limit + 1))
     while len(unmarked) > 0:
         prime = unmarked.pop(0)
-        for mult in range(2, limit / prime + 1):
-            if mult * prime in unmarked:
-                unmarked.remove(mult * prime)
+        for multiplier in range(2, limit // prime + 1):
+            if prime * multiplier in unmarked:
+                unmarked.remove(prime * multiplier)
         primes.append(prime)
     return primes
