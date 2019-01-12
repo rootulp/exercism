81e35a4cd97f34420b9b9bf0b59f245783df457d
Rootul Patel <rootulp@palantir.com>
2019-01-12 14:56:41 -0800

    Solve sum of multiples

--- python/sum-of-multiples/sum_of_multiples.py
+++ python/sum-of-multiples/sum_of_multiples.py
@@ -1,2 +1,20 @@
-def sum_of_multiples():
-    pass
+def sum_of_multiples(limit, factors):
+    return sum(all_multiples(limit, factors))
+
+
+def all_multiples(limit, factors):
+    multiples = set()
+    for factor in factors:
+        multiples = multiples.union(get_multiples(limit, factor))
+    return multiples
+
+def get_multiples(limit, factor):
+    if factor == 0:
+        return []
+
+    multiples = set()
+    for i in range(0, limit):
+        if i % factor == 0:
+            multiples.add(i)
+    return multiples
+
