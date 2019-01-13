32cf1981b1e523f6bdab2b22c7112fa78edd627d
Rootul Patel <rootulp@palantir.com>
2019-01-12 16:11:20 -0800

    [wip] try adapting old solution

--- python/pythagorean-triplet/pythagorean_triplet.py
+++ python/pythagorean-triplet/pythagorean_triplet.py
@@ -1,59 +1,44 @@
 from math import gcd
-from itertools import combinations
+from itertools import product
 
 
-def is_triplet(sides):
-    return Triplet(sides).valid()
 
+def triplets(n):
+    valid_triplets = set()
 
-def triplets_in_range(low, high):
-    return Triplet.triplets_in_range(low, high)
+    potential_triplets = set([generate_sides(pair) for pair in
+                valid_pairs(n // 2)])
 
+    for potential_triplet in potential_triplets:
+        if valid_sides(*potential_triplet):
+            valid_triplets.add(potential_triplet)
 
-class Triplet:
-    def __init__(self, sides):
-        self.sides = sides
-        self.sorted_sides = sorted(sides)
-        self.a = self.sorted_sides[0]
-        self.b = self.sorted_sides[1]
-        self.c = self.sorted_sides[2]
+    return valid_triplets
 
-    def valid(self):
-        return self.a**2 + self.b**2 == self.c**2
+def valid_pairs(n):
+    return [pair for pair in potential_pairs(n) if valid_pair(pair)]
 
-    def valid_in_range(self, low, high):
-        valid_range = list(range(low, high + 1))
-        return self.valid() and (self.a in valid_range and
-                                 self.b in valid_range and
-                                 self.c in valid_range)
+def valid_pair(pair):
+    n, m = pair
+    return odd(m - n)
 
-    @classmethod
-    def triplets(cls, n):
-        return set([cls.generate_sides(pair) for pair in
-                    cls.valid_pairs(n / 2)])
+def generate_sides(pair):
+    n, m = pair
+    a = m ** 2 - n ** 2
+    b = 2 * m * n
+    c = m ** 2 + n ** 2
+    if (a <= 0 or b <= 0 or c <= 0):
+        return None
+    return tuple(sorted((a, b, c)))
 
-    @classmethod
-    def valid_pairs(cls, n):
-        return [pair for pair in cls.factor_pairs(n) if cls.valid_pair(pair)]
+def valid_sides(a, b, c):
+    return a ** 2 + b ** 2 == c ** 2
 
-    @classmethod
-    def valid_pair(cls, pair):
-        n, m = pair
-        return cls.odd(m - n)
+def potential_pairs(n):
+    return product(range(n), range(n))
 
-    @staticmethod
-    def generate_sides(pair):
-        n, m = pair
-        return tuple(sorted((m**2 - n**2, 2 * m * n, m**2 + n**2)))
+def odd(n):
+    return n % 2 == 1
 
-    @staticmethod
-    def factor_pairs(n):
-        return [(i, n / i) for i in range(1, int(n**0.5) + 1) if n % i == 0]
-
-    @staticmethod
-    def possible_triplets(low, high):
-        return combinations(list(range(low, high)), 3)
-
-    @staticmethod
-    def odd(n):
-        return n % 2 == 1
+triplets_with_sum_12 = triplets(12)
+print("triplets_with_sum_12 {}".format(triplets_with_sum_12))
