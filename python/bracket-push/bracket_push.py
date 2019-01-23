a003b3f10b3f5d7a3632fc10160b9ab76779e8d3
Rootul Patel <rootulp@palantir.com>
2019-01-22 20:44:02 -0500

    Done refactoring

--- python/bracket-push/bracket_push.py
+++ python/bracket-push/bracket_push.py
@@ -1,12 +1,12 @@
-def isEmpty(stack):
+def is_empty(stack):
     return stack == []
 
 class CheckBrackets:
     BRACKETS = {'{': '}',
                '[': ']',
                '(': ')'}
-    OPENERS = set(BRACKETS.keys())
-    CLOSERS = set(BRACKETS.values())
+    OPENING_BRACKET = set(BRACKETS.keys())
+    CLOSING_BRACKET = set(BRACKETS.values())
 
     def __init__(self, inp):
         self.inp = inp
@@ -19,8 +19,9 @@ class CheckBrackets:
             elif self.is_closing_bracket(bracket) and self.closes_existing_bracket(bracket, stack):
                 stack.pop()
             else:
-                return False
-        return isEmpty(stack)
+                return False # This is an invalid closing bracket
+
+        return is_empty(stack) # There are more open brackets left to close
 
     def closes_existing_bracket(self, char, stack):
         return stack and self.matching_brackets(stack[-1], char)
@@ -32,10 +33,10 @@ class CheckBrackets:
         return [char for char in string if self.is_bracket(char)]
 
     def is_opening_bracket(self, bracket):
-        return bracket in self.OPENERS
+        return bracket in self.OPENING_BRACKET
 
     def is_closing_bracket(self, bracket):
-        return bracket in self.CLOSERS
+        return bracket in self.CLOSING_BRACKET
 
     def is_bracket(self, char):
         return self.is_opening_bracket(char) or self.is_closing_bracket(char)
