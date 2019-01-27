bfa93f8c8f1d26559549d3d83cf608d3cf736300
Rootul Patel <rootulp@palantir.com>
2019-01-27 15:24:37 -0500

    Implement results table header

--- python/tournament/tournament.py
+++ python/tournament/tournament.py
@@ -1,2 +1,15 @@
+class Tournament:
+
+    def __init__(self, results):
+        self.results = results
+
+    def header(self):
+        return 'Team                           | MP |  W |  D |  L |  P'
+
+    def results_table(self):
+        if (self.results == ''):
+            return self.header()
+
 def tally(tournament_results):
-    pass
+    tournament = Tournament(tournament_results)
+    return tournament.results_table()
