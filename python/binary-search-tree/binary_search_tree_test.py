c11ce2512506602acae44c17ac93d4c5a83884c3
Rootul Patel <rootulp@palantir.com>
2019-02-25 18:57:08 -0500

    Revert data -> node_data rename in test

--- python/binary-search-tree/binary_search_tree_test.py
+++ python/binary-search-tree/binary_search_tree_test.py
@@ -9,20 +9,20 @@ class BinarySearchTreeTest(unittest.TestCase):
 
     def test_data_is_retained(self):
         expected = TreeNode('4', None, None)
-        self.assertTreeEqual(BinarySearchTree(['4']).node_data(), expected)
+        self.assertTreeEqual(BinarySearchTree(['4']).data(), expected)
 
     # Test inserting data at proper node
     def test_smaller_data_at_left_node(self):
         expected = TreeNode('4', TreeNode('2', None, None), None)
-        self.assertTreeEqual(BinarySearchTree(['4', '2']).node_data(), expected)
+        self.assertTreeEqual(BinarySearchTree(['4', '2']).data(), expected)
 
     def test_same_number_at_left_node(self):
         expected = TreeNode('4', TreeNode('4', None, None), None)
-        self.assertTreeEqual(BinarySearchTree(['4', '4']).node_data(), expected)
+        self.assertTreeEqual(BinarySearchTree(['4', '4']).data(), expected)
 
     def test_greater_number_at_right_node(self):
         expected = TreeNode('4', None, TreeNode('5', None, None))
-        self.assertTreeEqual(BinarySearchTree(['4', '5']).node_data(), expected)
+        self.assertTreeEqual(BinarySearchTree(['4', '5']).data(), expected)
 
     def test_can_create_complex_tree(self):
         expected = TreeNode(
@@ -39,7 +39,7 @@ class BinarySearchTreeTest(unittest.TestCase):
             )
         )
         self.assertTreeEqual(
-            BinarySearchTree(['4', '2', '6', '1', '3', '5', '7']).node_data(),
+            BinarySearchTree(['4', '2', '6', '1', '3', '5', '7']).data(),
             expected
         )
 
