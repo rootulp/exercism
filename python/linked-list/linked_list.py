94c3169138dd676acd92f98d9a15168dab1706a8
Rootul Patel <rootulp@palantir.com>
2019-02-25 17:36:34 -0500

    Implement push and pop

--- python/linked-list/linked_list.py
+++ python/linked-list/linked_list.py
@@ -1,8 +1,26 @@
 class Node(object):
     def __init__(self, value, succeeding=None, previous=None):
-        pass
+        self.value = value
+        self.succeeding = succeeding
+        self.previous = previous
 
 
 class LinkedList(object):
+
     def __init__(self):
-        pass
+        self.head = None
+        self.tail = None
+
+    def push(self, value):
+        new_node = Node(value)
+        if self.tail is not None:
+            self.tail.succeeding = new_node
+            new_node.previous = self.tail
+        self.tail = new_node
+
+    def pop(self):
+        if self.tail is not None:
+            popped_node = self.tail
+            self.tail = popped_node.previous
+            return popped_node.value
+
