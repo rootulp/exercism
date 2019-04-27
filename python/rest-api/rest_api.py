8e7114a81c1a52f8867d9ee7ce828312e343fb93
Rootul Patel <rootulp@palantir.com>
2019-04-26 20:28:08 -0400

    Pass second text

--- python/rest-api/rest_api.py
+++ python/rest-api/rest_api.py
@@ -10,4 +10,23 @@ class RestAPI(object):
         return json.dumps(self.database)
 
     def post(self, url, payload=None):
-        pass
+        payload = json.loads(payload)
+        username = payload['user']
+        self.create_user(username)
+        return self.get_user(username)
+
+    def create_user(self, username):
+        new_user = {
+            'name': username,
+            'owes': {},
+            'owed_by': {},
+            'balance': 0,
+        }
+
+        self.database['users'].append(new_user)
+
+    def get_user(self, username):
+        users = self.database['users']
+        for user in users:
+            if user['name'] == username:
+                return json.dumps(user)
