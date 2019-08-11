65219e0716925a4df22a16558559ffa1011b17fe
Rootul Patel <rootulp@palantir.com>
2019-08-11 12:04:10 -0400

    Extract method getWords()

--- typescript/word-count/word-count.ts
+++ typescript/word-count/word-count.ts
@@ -1,12 +1,16 @@
 
 export default class Words {
     public count(sentence: string): Map<string | number, number> {
-        return this.removeFormatting(sentence).split(" ").map((word) => word.trim()).filter((word) => word !== "").reduce((counts: Map<string | number, number>, word: string) => {
+        return this.getWords(sentence).reduce((counts: Map<string | number, number>, word: string) => {
             const currentCountForWord = counts.get(word)
             const newCountForWord = currentCountForWord ? currentCountForWord + 1 : 1
             counts.set(word, newCountForWord)
             return counts
         }, new Map<string | number, number>())
+    }
+
+    private getWords(sentence: string): string[] {
+        return this.removeFormatting(sentence).split(" ").map((word) => word.trim()).filter((word) => word !== "")
     }
 
     private removeFormatting(sentence: string): string {
