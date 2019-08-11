2155131dac6637f4ca045abde449eaa83ed6880a
Rootul Patel <rootulp@palantir.com>
2019-08-11 11:52:54 -0400

    Pass all tests

--- typescript/word-count/word-count.test.ts
+++ typescript/word-count/word-count.test.ts
@@ -38,27 +38,27 @@ describe('words()', () => {
     expect(words.count('¡Hola! ¿Qué tal? Привет!')).toEqual(expectedCounts)
   })
 
-  xit('counts multiline', () => {
+  it('counts multiline', () => {
     const expectedCounts = new Map(Object.entries({ hello: 1, world: 1 }))
     expect(words.count('hello\nworld')).toEqual(expectedCounts)
   })
 
-  xit('counts tabs', () => {
+  it('counts tabs', () => {
     const expectedCounts = new Map(Object.entries({ hello: 1, world: 1 }))
     expect(words.count('hello\tworld')).toEqual(expectedCounts)
   })
 
-  xit('counts multiple spaces as one', () => {
+  it('counts multiple spaces as one', () => {
     const expectedCounts = new Map(Object.entries({ hello: 1, world: 1 }))
     expect(words.count('hello  world')).toEqual(expectedCounts)
   })
 
-  xit('does not count leading or trailing whitespace', () => {
+  it('does not count leading or trailing whitespace', () => {
     const expectedCounts = new Map(Object.entries({ introductory: 1, course: 1 }))
     expect(words.count('\t\tIntroductory Course      ')).toEqual(expectedCounts)
   })
 
-  xit('handles properties that exist on Object’s prototype', () => {
+  it('handles properties that exist on Object’s prototype', () => {
     const expectedCounts = new Map(Object.entries({ reserved: 1, words : 1, like : 1,  constructor: 1, and : 1, tostring: 1,  'ok?': 1}))
     expect(words.count('reserved words like constructor and toString ok?')).toEqual(expectedCounts)
   })
--- typescript/word-count/word-count.ts
+++ typescript/word-count/word-count.ts
@@ -2,12 +2,23 @@
 export default class Words {
     public count(sentence: string): Map<string | number, number> {
         const counts: Map<string | number, number> = new Map()
-        sentence.toLowerCase().split(" ").forEach((word) => {
+        this.removeFormatting(sentence.toLowerCase()).split(" ").map((word) => word.trim()).filter((word) => word !== "").forEach((word) => {
             const currentCountForWord = counts.get(word)
             const newCountForWord = currentCountForWord ? currentCountForWord + 1 : 1
             counts.set(word, newCountForWord)
         })
         return counts
+    }
+
+    private removeFormatting(sentence: string): string {
+        return this.replaceNewlinesWithSpaces(this.replaceTabsWithSpaces(sentence))
+    }
+    private replaceNewlinesWithSpaces(sentence: string): string {
+        return sentence.replace("\n", " ")
+    }
+
+    private replaceTabsWithSpaces(sentence: string): string {
+        return sentence.replace("\t", " ")
     }
 
 }
