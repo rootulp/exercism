8905d31e749630efcf2f464ee5e40c62babbca9d
Rootul Patel <rootulp@palantir.com>
2019-08-11 12:06:37 -0400

    Extract removeMultipleSpaces

--- typescript/word-count/word-count.ts
+++ typescript/word-count/word-count.ts
@@ -1,16 +1,20 @@
 
 export default class Words {
     public count(sentence: string): Map<string | number, number> {
-        return this.getWords(sentence).reduce((counts: Map<string | number, number>, word: string) => {
-            const currentCountForWord = counts.get(word)
+        return this.getWords(sentence).reduce((counts: Map<string | number, number>, currentWord: string) => {
+            const currentCountForWord = counts.get(currentWord)
             const newCountForWord = currentCountForWord ? currentCountForWord + 1 : 1
-            counts.set(word, newCountForWord)
+            counts.set(currentWord, newCountForWord)
             return counts
         }, new Map<string | number, number>())
     }
 
     private getWords(sentence: string): string[] {
-        return this.removeFormatting(sentence).split(" ").map((word) => word.trim()).filter((word) => word !== "")
+        return this.removeMultipleSpaces(this.removeFormatting(sentence).split(" "))
+    }
+
+    private removeMultipleSpaces(words: string[]): string[] {
+        return words.map((word) => word.trim()).filter((word) => word !== "")
     }
 
     private removeFormatting(sentence: string): string {
