5a2ceabc7996ad2501d015e3b4e0aa17ecd92f12
Rootul Patel <rootulp@palantir.com>
2019-08-11 11:57:00 -0400

    Remove two helper functions

--- typescript/word-count/word-count.ts
+++ typescript/word-count/word-count.ts
@@ -2,7 +2,7 @@
 export default class Words {
     public count(sentence: string): Map<string | number, number> {
         const counts: Map<string | number, number> = new Map()
-        this.removeFormatting(sentence.toLowerCase()).split(" ").map((word) => word.trim()).filter((word) => word !== "").forEach((word) => {
+        this.removeFormatting(sentence).split(" ").map((word) => word.trim()).filter((word) => word !== "").forEach((word) => {
             const currentCountForWord = counts.get(word)
             const newCountForWord = currentCountForWord ? currentCountForWord + 1 : 1
             counts.set(word, newCountForWord)
@@ -11,14 +11,6 @@ export default class Words {
     }
 
     private removeFormatting(sentence: string): string {
-        return this.replaceNewlinesWithSpaces(this.replaceTabsWithSpaces(sentence))
+        return sentence.toLowerCase().replace("\n", " ").replace("\t", " ")
     }
-    private replaceNewlinesWithSpaces(sentence: string): string {
-        return sentence.replace("\n", " ")
-    }
-
-    private replaceTabsWithSpaces(sentence: string): string {
-        return sentence.replace("\t", " ")
-    }
-
 }
