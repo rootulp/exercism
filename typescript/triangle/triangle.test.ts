2c51c8c0ac1a5dfcb47d2993e6946e9e87a628d7
Rootul Patel <rootulp@palantir.com>
2019-08-13 19:56:14 -0400

    Pass isosceles and scalene tests

--- typescript/triangle/triangle.test.ts
+++ typescript/triangle/triangle.test.ts
@@ -6,47 +6,47 @@ describe('Triangle', () => {
         expect(triangle.kind()).toEqual("equilateral")
     })
 
-    xit("larger equilateral triangles also have equal sides", () => {
+    it("larger equilateral triangles also have equal sides", () => {
         const triangle = new Triangle(10, 10, 10)
         expect(triangle.kind()).toEqual('equilateral')
     })
 
-    xit('isosceles triangles have last two sides equal', () => {
+    it('isosceles triangles have last two sides equal', () => {
         const triangle = new Triangle(3, 4, 4)
         expect(triangle.kind()).toEqual('isosceles')
     })
 
-    xit('isosceles trianges have first and last sides equal', () => {
+    it('isosceles trianges have first and last sides equal', () => {
         const triangle = new Triangle(4, 3, 4)
         expect(triangle.kind()).toEqual('isosceles')
     })
 
-    xit('isosceles triangles have two first sides equal', () => {
+    it('isosceles triangles have two first sides equal', () => {
         const triangle = new Triangle(4, 4, 3)
         expect(triangle.kind()).toEqual('isosceles')
     })
 
-    xit('isosceles triangles have in fact exactly two sides equal', () => {
+    it('isosceles triangles have in fact exactly two sides equal', () => {
         const triangle = new Triangle(10, 10, 2)
         expect(triangle.kind()).toEqual('isosceles')
     })
 
-    xit('scalene triangles have no equal sides', () => {
+    it('scalene triangles have no equal sides', () => {
         const triangle = new Triangle(3, 4, 5)
         expect(triangle.kind()).toEqual('scalene')
     })
 
-    xit('scalene triangles have no equal sides at a larger scale too', () => {
+    it('scalene triangles have no equal sides at a larger scale too', () => {
         const triangle = new Triangle(10, 11, 12)
         expect(triangle.kind()).toEqual('scalene')
     })
 
-    xit('scalene triangles have no equal sides in descending order either', () => {
+    it('scalene triangles have no equal sides in descending order either', () => {
         const triangle = new Triangle(5, 4, 2)
         expect(triangle.kind()).toEqual('scalene')
     })
 
-    xit('very small triangles are legal', () => {
+    it('very small triangles are legal', () => {
         const triangle = new Triangle(0.4, 0.6, 0.3)
         expect(triangle.kind()).toEqual('scalene')
     })
--- typescript/triangle/triangle.ts
+++ typescript/triangle/triangle.ts
@@ -9,11 +9,27 @@ export default class Triangle {
     public kind(): string {
         if (this.is_equilateral()){
             return "equilateral"
+        } else if (this.is_isosceles()) {
+            return "isosceles"
+        } else if (this.is_scalene()) {
+            return "scalene"
         }
         return "error: not a valid triangle"
     }
 
     private is_equilateral(): boolean {
-        return true;
+        return this.sides[0] === this.sides[1]  && this.sides[1] === this.sides[2]
+    }
+
+    private is_isosceles(): boolean {
+        return this.sides[0] === this.sides[1] ||
+               this.sides[1] === this.sides[2] ||
+               this.sides[0] === this.sides[2]
+    }
+
+    private is_scalene(): boolean {
+        return this.sides[0] !== this.sides[1] &&
+               this.sides[1] !== this.sides[2] &&
+               this.sides[0] !== this.sides[2]
     }
 }
