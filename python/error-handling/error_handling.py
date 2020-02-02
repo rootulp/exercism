bf9515961f8a95e8006cf10f930e89f0b9fe4e04
Rootul Patel <rootulp@palantir.com>
2020-02-01 19:32:37 -0500

    Pass third test

--- python/error-handling/error_handling.py
+++ python/error-handling/error_handling.py
@@ -9,7 +9,10 @@ def handle_error_by_returning_none(input_data):
 
 
 def handle_error_by_returning_tuple(input_data):
-    pass
+    if (input_data == '1'):
+        return (True, 1)
+    return (False, None)
+
 
 
 def filelike_objects_are_closed_on_exception(filelike_object):
