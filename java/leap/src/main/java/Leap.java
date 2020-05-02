224d3c87193e824c3e06256e2034b73cb1c06d1b
Rootul Patel <rootulp@palantir.com>
2020-05-02 12:10:01 -0400

    Solve leap

--- java/leap/src/main/java/Leap.java
+++ java/leap/src/main/java/Leap.java
@@ -1,7 +1,13 @@
 class Leap {
 
     boolean isLeapYear(int year) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        if (year % 400 == 0) {
+            return true;
+        } else if (year % 100 == 0) {
+            return false;
+        } else if (year % 4 == 0) {
+            return true;
+        }
+        return false;
     }
-
 }
