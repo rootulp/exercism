254bbda862c50faeae32a63c0476ce65bcb146da
Rootul Patel <rootulp@gmail.com>
2020-05-18 16:04:59 -0400

    Solve first six tests

--- java/grains/src/main/java/Grains.java
+++ java/grains/src/main/java/Grains.java
@@ -1,9 +1,10 @@
+import java.math.BigDecimal;
 import java.math.BigInteger;
 
 class Grains {
 
     BigInteger grainsOnSquare(final int square) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+		return BigDecimal.valueOf(Math.pow(2, square - 1)).toBigInteger();
     }
 
     BigInteger grainsOnBoard() {
--- java/grains/src/test/java/GrainsTest.java
+++ java/grains/src/test/java/GrainsTest.java
@@ -10,42 +10,37 @@ import java.math.BigInteger;
 public class GrainsTest {
 
     private Grains grains = new Grains();
-    
+
     @Test
     public void countAtSquare1() {
         BigInteger result = grains.grainsOnSquare(1);
         assertEquals(new BigInteger("1"), result);
     }
 
-    @Ignore("Remove to run test")
     @Test
     public void countAtSquare2() {
         BigInteger result = grains.grainsOnSquare(2);
         assertEquals(new BigInteger("2"), result);
     }
 
-    @Ignore("Remove to run test")
     @Test
     public void countAtSquare3() {
         BigInteger result = grains.grainsOnSquare(3);
         assertEquals(new BigInteger("4"), result);
     }
 
-    @Ignore("Remove to run test")
     @Test
     public void countAtSquare4() {
         BigInteger result = grains.grainsOnSquare(4);
         assertEquals(new BigInteger("8"), result);
     }
 
-    @Ignore("Remove to run test")
     @Test
     public void countAtSquare16() {
         BigInteger result = grains.grainsOnSquare(16);
         assertEquals(new BigInteger("32768"), result);
     }
 
-    @Ignore("Remove to run test")
     @Test
     public void countAtSquare32() {
         BigInteger result = grains.grainsOnSquare(32);
