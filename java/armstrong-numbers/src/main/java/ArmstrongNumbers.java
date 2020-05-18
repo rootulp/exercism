545e5b797a2d9ed1f8f4b0e28a5d84e530cb2f1a
Rootul Patel <rootulp@gmail.com>
2020-05-18 15:02:48 -0400

    Refactor

--- java/armstrong-numbers/src/main/java/ArmstrongNumbers.java
+++ java/armstrong-numbers/src/main/java/ArmstrongNumbers.java
@@ -6,35 +6,34 @@ import java.util.stream.Collectors;
 class ArmstrongNumbers {
 
     boolean isArmstrongNumber(int numberToCheck) {
-		List<Integer> digits = getDigits(numberToCheck);
-		double sumOfDigitsRaisedToPower = digits.stream().map(digit -> {
-			return Math.pow(digit, numberOfDigits(numberToCheck));
-		}).collect(Collectors.summingDouble(x -> x));
-		return sumOfDigitsRaisedToPower == numberToCheck;
-	}
+        List<Integer> digits = getDigits(numberToCheck);
+        return numberToCheck == sumOfDigitsRaisedToNumberOfDigits(digits);
+    }
 
-	int numberOfDigits(int numberToCheck) {
-		return Integer.toString(numberToCheck).length();
-	}
+    private static double sumOfDigitsRaisedToNumberOfDigits(List<Integer> digits) {
+        return digits.stream().map(digit -> {
+            return Math.pow(digit, digits.size());
+        }).collect(Collectors.summingDouble(x -> x));
+    }
 
-	/**
-	 * Split an integer into its individual digits
-	 * NOTE: digits order is maintained - i.e. Least significant digit is at index[0]
-	 * @param num positive integer
-	 * @return array of digits
-	 */
-	public static List<Integer> getDigits(int num) {
-		if (num < 0) { return new ArrayList<>(0); }
-		List<Integer> digits = new ArrayList<Integer>();
-		collectDigits(num, digits);
-		Collections.reverse(digits);
-		return digits;
-	}
+    /**
+     * Split an integer into its individual digits
+     * NOTE: digits order is maintained - i.e. Least significant digit is at index[0]
+     * @param num positive integer
+     * @return array of digits
+     */
+    private static List<Integer> getDigits(int num) {
+        if (num < 0) { return new ArrayList<>(0); }
+        List<Integer> digits = new ArrayList<Integer>();
+        collectDigits(num, digits);
+        Collections.reverse(digits);
+        return digits;
+    }
 
-	private static void collectDigits(int num, List<Integer> digits) {
-		if(num / 10 > 0) {
-			collectDigits(num / 10, digits);
-		}
-		digits.add(num % 10);
-	}
+    private static void collectDigits(int num, List<Integer> digits) {
+        if(num / 10 > 0) {
+            collectDigits(num / 10, digits);
+        }
+        digits.add(num % 10);
+    }
 }
