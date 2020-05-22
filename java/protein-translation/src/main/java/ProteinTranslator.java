639330cb279d315bfc6720e00af3de60f3b86c06
Rootul Patel <rootulp@gmail.com>
2020-05-22 13:08:12 -0400

    Pass first test

--- java/protein-translation/src/main/java/ProteinTranslator.java
+++ java/protein-translation/src/main/java/ProteinTranslator.java
@@ -1,8 +1,22 @@
 import java.util.List;
+import java.util.Map;
 
 class ProteinTranslator {
+	// Codon                 | Protein
+	// :---                  | :---
+	// AUG                   | Methionine
+	// UUU, UUC              | Phenylalanine
+	// UUA, UUG              | Leucine
+	// UCU, UCC, UCA, UCG    | Serine
+	// UAU, UAC              | Tyrosine
+	// UGU, UGC              | Cysteine
+	// UGG                   | Tryptophan
+	// UAA, UAG, UGA         | STOP
+	private static final Map<String, String> CODON_TO_PROTEIN = Map.ofEntries(
+		Map.entry("AUG", "Methionine")
+	);
 
     List<String> translate(String rnaSequence) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+		return List.of(CODON_TO_PROTEIN.get(rnaSequence));
     }
 }
