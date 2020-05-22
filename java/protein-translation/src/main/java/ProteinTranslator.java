157858bae968a03b88eed30ee755290adc3206c2
Rootul Patel <rootulp@gmail.com>
2020-05-22 13:12:20 -0400

    Pass first 14 tests

--- java/protein-translation/src/main/java/ProteinTranslator.java
+++ java/protein-translation/src/main/java/ProteinTranslator.java
@@ -1,5 +1,6 @@
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 class ProteinTranslator {
 	// Codon                 | Protein
@@ -13,8 +14,22 @@ class ProteinTranslator {
 	// UGG                   | Tryptophan
 	// UAA, UAG, UGA         | STOP
 	private static final Map<String, String> CODON_TO_PROTEIN = Map.ofEntries(
-		Map.entry("AUG", "Methionine")
+		Map.entry("AUG", "Methionine"),
+		Map.entry("UUU", "Phenylalanine"),
+		Map.entry("UUC", "Phenylalanine"),
+		Map.entry("UUA", "Leucine"),
+		Map.entry("UUG", "Leucine"),
+		Map.entry("UCU", "Serine"),
+		Map.entry("UCC", "Serine"),
+		Map.entry("UCA", "Serine"),
+		Map.entry("UCG", "Serine"),
+		Map.entry("UAU", "Tyrosine"),
+		Map.entry("UAC", "Tyrosine"),
+		Map.entry("UGU", "Cysteine"),
+		Map.entry("UGC", "Cysteine"),
+		Map.entry("UGG", "Tryptophan")
 	);
+	private static final Set<String> STOP_CODONS = Set.of("UAA", "UAG", "UGA");
 
     List<String> translate(String rnaSequence) {
 		return List.of(CODON_TO_PROTEIN.get(rnaSequence));
