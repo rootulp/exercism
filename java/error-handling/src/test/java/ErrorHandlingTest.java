1a96a0c2e2b64c23c524bb0c77270be2d08dd8fb
Rootul Patel <rootulp@gmail.com>
2020-05-24 12:15:35 -0400

    Remove unused import

--- java/error-handling/src/test/java/ErrorHandlingTest.java
+++ java/error-handling/src/test/java/ErrorHandlingTest.java
@@ -4,7 +4,6 @@ import static org.junit.Assert.assertThrows;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-import org.junit.Ignore;
 import org.junit.Test;
 
 import java.util.Optional;
