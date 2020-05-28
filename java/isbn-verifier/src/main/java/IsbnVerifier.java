c0162cb7a6a69768d8868f75c53c5edb78c11462
Rootul Patel <rootulp@gmail.com>
2020-05-28 12:49:23 -0400

    Make static methods private

--- java/isbn-verifier/src/main/java/IsbnVerifier.java
+++ java/isbn-verifier/src/main/java/IsbnVerifier.java
@@ -1,12 +1,12 @@
 class IsbnVerifier {
 
-	static final char VALID_CHECK_DIGIT = 'X';
+	private static final char VALID_CHECK_DIGIT = 'X';
 
 	/**
 	 * Determine if the provided string is a valid ISBN according to the following formula
 	 * (x1 * 10 + x2 * 9 + x3 * 8 + x4 * 7 + x5 * 6 + x6 * 5 + x7 * 4 + x8 * 3 + x9 * 2 + x10 * 1) mod 11 == 0
 	 */
-	static boolean isValidIsbn(String stringToVerify) {
+	public static boolean isValidIsbn(String stringToVerify) {
 		String parsed = removeHyphens(stringToVerify);
 		if(!isValidParsed(parsed)) {
 			return false;
@@ -15,7 +15,7 @@ class IsbnVerifier {
 		return computeValueForIsbn(parsed) % 11 == 0;
 	}
 
-	static Integer computeValueForIsbn(String isbn) {
+	private static Integer computeValueForIsbn(String isbn) {
 		String reversed = new StringBuilder(isbn).reverse().toString();
 		int result = 0;
 		for (int i = 0; i < isbn.length(); i++) {
@@ -25,37 +25,36 @@ class IsbnVerifier {
 		return result;
 	}
 
-	static Integer getValueFrom(char c, int i) {
+	private static Integer getValueFrom(char c, int i) {
 		if(i == 0 && c == VALID_CHECK_DIGIT) {
 			return 10;
 		}
 		return Integer.parseInt(String.valueOf(c)) * (i + 1);
 	}
 
-	static String removeHyphens(String s) {
+	private static String removeHyphens(String s) {
 		return s.replaceAll("-", "");
 	}
 
-	static boolean isValidParsed(String parsed) {
+	private static boolean isValidParsed(String parsed) {
 		return isValidLength(parsed) &&
 			   isValidCheckDigit(parsed.charAt((parsed.length() - 1))) &&
 			   doesContainOnlyDigits(parsed.substring(0, parsed.length() - 1));
 	}
 
-	static boolean isValidCheckDigit(char checkDigit) {
+	private static boolean isValidCheckDigit(char checkDigit) {
 		return Character.isDigit(checkDigit) || checkDigit == VALID_CHECK_DIGIT;
 	}
 
-	static boolean isValidLength(String s) {
+	private static boolean isValidLength(String s) {
 		return s.length() == 10;
 	}
 
-	static boolean doesContainOnlyDigits(String s) {
+	private static boolean doesContainOnlyDigits(String s) {
 		return s.codePoints().mapToObj(c -> (char) c).allMatch(c -> Character.isDigit(c));
 	}
 
-    boolean isValid(String stringToVerify) {
+    public boolean isValid(String stringToVerify) {
 		return isValidIsbn(stringToVerify);
     }
-
 }
