7d3ab66ccfee8eed7d116ea8a1b5a82c4d7caf00
Rootul Patel <rootulp@gmail.com>
2020-05-31 14:32:47 -0400

    Solve acronym

--- go/acronym/acronym.go
+++ go/acronym/acronym.go
@@ -1,13 +1,30 @@
 package acronym
 
-import "strings"
+import (
+	"strings"
+	"unicode"
+)
 
 // Abbreviate should return an abbreviated string based on s.
 func Abbreviate(s string) string {
-	fields := strings.Fields(s)
+	parsed := replaceNonLettersWithWhitespace(s)
+
+	fields := strings.Fields(parsed)
 	result := ""
 	for _, v := range fields {
 		result += v[:1]
 	}
 	return strings.ToUpper(result)
+}
+
+func replaceNonLettersWithWhitespace(s string) string {
+	return strings.Map(func(r rune) rune {
+		if r == '-' || r == ' ' {
+			return ' '
+		} else if unicode.IsLetter(r) {
+			return r
+		} else {
+			return -1
+		}
+	}, s)
 }
