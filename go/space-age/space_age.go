97f5783d9bcd8791d8ba19d0ede6b54c2a8beed3
Rootul Patel <rootulp@gmail.com>
2020-06-01 17:57:33 -0400

    Use orbital periods map

--- go/space-age/space_age.go
+++ go/space-age/space_age.go
@@ -5,7 +5,7 @@ type Planet string
 
 const secondsInEarthYear = 31557600
 
-var orbitalPeriods = map[Planet]float64{"Mercury": 0.2408467}
+var orbitalPeriods = map[Planet]float64{"Mercury": 0.2408467, "Venus": 0.61519726}
 
 // Age returns the age in years of someone on planet given their age in seconds.
 func Age(ageInSeconds float64, planet Planet) float64 {
@@ -14,7 +14,7 @@ func Age(ageInSeconds float64, planet Planet) float64 {
 	} else if planet == "Mercury" {
 		return ageOnEarth(ageInSeconds) / orbitalPeriods[planet]
 	} else if planet == "Venus" {
-		return ageOnEarth(ageInSeconds) / 0.61519726
+		return ageOnEarth(ageInSeconds) / orbitalPeriods[planet]
 	}
 	return 0
 }
