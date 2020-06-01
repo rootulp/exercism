270e8e0b3f38406149cc50f13ecbd76269390a1b
Rootul Patel <rootulp@gmail.com>
2020-06-01 17:53:08 -0400

    Extract ageOnEarth

--- go/space-age/space_age.go
+++ go/space-age/space_age.go
@@ -3,14 +3,19 @@ package space
 // Planet names
 type Planet string
 
-// Age returns the age in seconds of someone on a different planet.
+// Age returns the age in years of someone on planet given their age in seconds.
 func Age(ageInSeconds float64, planet Planet) float64 {
 	if planet == "Earth" {
-		return ageInSeconds / 31557600
+		return ageOnEarth(ageInSeconds)
 	} else if planet == "Mercury" {
-		return ageInSeconds / 31557600 / 0.2408467
+		return ageOnEarth(ageInSeconds) / 0.2408467
 	} else if planet == "Venus" {
-		return ageInSeconds / 31557600 / 0.61519726
+		return ageOnEarth(ageInSeconds) / 0.61519726
 	}
 	return 0
+}
+
+// AgeOnEarth returns the age of somone on Earth given their age in seconds.
+func ageOnEarth(ageInSeconds float64) float64 {
+	return ageInSeconds / 31557600
 }
