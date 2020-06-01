b1dbaed4bbcd324754a604657224b2484f8df2fb
Rootul Patel <rootulp@gmail.com>
2020-06-01 18:01:10 -0400

    Remove special case for Earth

--- go/space-age/space_age.go
+++ go/space-age/space_age.go
@@ -8,6 +8,7 @@ const secondsInEarthYear = 31557600
 var orbitalPeriods = map[Planet]float64{
 	"Mercury": 0.2408467,
 	"Venus":   0.61519726,
+	"Earth":   1,
 	"Mars":    1.8808158,
 	"Jupiter": 11.862615,
 	"Saturn":  29.447498,
@@ -17,9 +18,6 @@ var orbitalPeriods = map[Planet]float64{
 
 // Age returns the age in years of someone on planet given their age in seconds.
 func Age(ageInSeconds float64, planet Planet) float64 {
-	if planet == "Earth" {
-		return ageOnEarth(ageInSeconds)
-	}
 	return ageOnEarth(ageInSeconds) / orbitalPeriods[planet]
 }
 
