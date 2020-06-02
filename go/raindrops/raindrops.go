606a438ea116e5d0ffb74f97c83c5e3a9d2242c7
Rootul Patel <rootulp@gmail.com>
2020-06-02 16:51:34 -0400

    Solve raindrops

--- go/raindrops/raindrops.go
+++ go/raindrops/raindrops.go
@@ -9,5 +9,34 @@ import "strconv"
 // - has 7 as a factor, add 'Plong' to the result.
 // - does not have any of 3, 5, or 7 as a factor, the result should be the digits of the number.
 func Convert(n int) string {
-	return strconv.Itoa(n)
+	if !isDivisibleByPlingPlangPlong(n) {
+		return strconv.Itoa(n)
+	}
+	var result string
+	if isPling(n) {
+		result += "Pling"
+	}
+	if isPlang(n) {
+		result += "Plang"
+	}
+	if isPlong(n) {
+		result += "Plong"
+	}
+	return result
+}
+
+func isDivisibleByPlingPlangPlong(n int) bool {
+	return isPling(n) || isPlang(n) || isPlong(n)
+}
+
+func isPling(n int) bool {
+	return n%3 == 0
+}
+
+func isPlang(n int) bool {
+	return n%5 == 0
+}
+
+func isPlong(n int) bool {
+	return n%7 == 0
 }
