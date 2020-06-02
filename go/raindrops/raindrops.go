aa6fbe2deb844bee96df2a52965f2d09671c0c34
Rootul Patel <rootulp@gmail.com>
2020-06-02 16:47:10 -0400

    Pass first test

--- go/raindrops/raindrops.go
+++ go/raindrops/raindrops.go
@@ -1,1 +1,13 @@
 package raindrops
+
+import "strconv"
+
+// Convert returns a string  generated based on the raindrops rules below.
+// The rules of raindrops are that if a given number:
+// - has 3 as a factor, add 'Pling' to the result.
+// - has 5 as a factor, add 'Plang' to the result.
+// - has 7 as a factor, add 'Plong' to the result.
+// - does not have any of 3, 5, or 7 as a factor, the result should be the digits of the number.
+func Convert(n int) string {
+	return strconv.Itoa(n)
+}
