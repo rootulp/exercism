9c04beca622c6be461609dae499459a5df1a0f13
Rootul Patel <rootulp@gmail.com>
2020-06-02 19:20:02 -0400

    Solve scrabble score

--- go/scrabble-score/scrabble_score.go
+++ go/scrabble-score/scrabble_score.go
@@ -1,6 +1,22 @@
 package scrabble
 
+import "unicode"
+
+var scrabbleLetterValues = map[rune]int{
+	'A': 1, 'E': 1, 'I': 1, 'O': 1, 'U': 1, 'L': 1, 'N': 1, 'R': 1, 'S': 1, 'T': 1,
+	'D': 2, 'G': 2,
+	'B': 3, 'C': 3, 'M': 3, 'P': 3,
+	'F': 4, 'H': 4, 'V': 4, 'W': 4, 'Y': 4,
+	'K': 5,
+	'J': 8, 'X': 8,
+	'Q': 10, 'Z': 10,
+}
+
 // Score returns the Scrabble score for the provided word.
 func Score(word string) int {
-	return 0
+	score := 0
+	for _, c := range word {
+		score += scrabbleLetterValues[unicode.ToUpper(c)]
+	}
+	return score
 }
