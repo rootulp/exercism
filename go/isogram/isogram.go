f19b18388d00afa83a91ea37def51cdc1ab892dd
Rootul Patel <rootulp@gmail.com>
2020-06-02 18:57:40 -0400

    Solve isogram

--- go/isogram/isogram.go
+++ go/isogram/isogram.go
@@ -1,7 +1,26 @@
 package isogram
 
+import "strings"
+
 // IsIsogram returns whether the provided string is an isogram.
 // In other words, whether the string does not contain any duplicate characters.
 func IsIsogram(s string) bool {
+	parsed := strings.ToLower(removeWhitespaceAndHyphens(s))
+	seen := make(map[rune]bool)
+	for _, c := range parsed {
+		if (seen[c]) == true {
+			return false
+		}
+		seen[c] = true
+	}
 	return true
+}
+
+func removeWhitespaceAndHyphens(s string) string {
+	return strings.Map(func(r rune) rune {
+		if r == ' ' || r == '-' {
+			return -1
+		}
+		return r
+	}, s)
 }
