8676abb1b59b23877a231f953800915208f7ade4
Rootul Patel <rootulp@gmail.com>
2020-06-02 18:50:00 -0400

    Initial isogram solution

--- go/isogram/isogram.go
+++ go/isogram/isogram.go
@@ -1,1 +1,7 @@
 package isogram
+
+// IsIsogram returns whether the provided string is an isogram.
+// In other words, whether the string does not contain any duplicate characters.
+func IsIsogram(s string) bool {
+	return true
+}
