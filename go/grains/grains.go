fa6aff9ef2b4c4f175e8bfc2c30145654750bb70
Rootul Patel <rootulp@gmail.com>
2020-06-03 13:46:10 -0400

    Pass first few tests

--- go/grains/grains.go
+++ go/grains/grains.go
@@ -1,1 +1,18 @@
 package grains
+
+import "math"
+
+// Square returns the number of grains on the provided square.
+func Square(n int) (uint64, error) {
+	return uint64(math.Pow(2, float64(n-1))), nil
+}
+
+// Total returns the sum of all grains on the chess board.
+func Total() uint64 {
+	var sum uint64 = 0
+	for i := 1; i <= 64; i++ {
+		var result, _ = Square(i)
+		sum += result
+	}
+	return sum
+}
