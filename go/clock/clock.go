27cda27de983293d749a1ae83e4cc2c239d09e4e
Rootul Patel <rootulp@gmail.com>
2020-06-09 20:03:01 -0400

    Solve clock

--- go/clock/clock.go
+++ go/clock/clock.go
@@ -7,36 +7,35 @@ import (
 
 // Clock defines a type for keeping time.
 type Clock struct {
-	time time.Time
+	hour int
+	min  int
 }
 
 // New is a constructor to create instances of Clock.
 func New(hour, min int) Clock {
-	return Clock{time: time.Date(0, 0, 0, hour, min, 0, 0, time.UTC)}
+	t := time.Date(0, 0, 0, hour, min, 0, 0, time.UTC)
+	return Clock{hour: t.Hour(), min: t.Minute()}
 }
 
 func (c Clock) String() string {
-	return c.time.Format("15:04")
+	return c.time().Format("15:04")
 }
 
 // Add moves the clock forward by the provided number of minutes.
 func (c Clock) Add(minutes int) Clock {
 	d, _ := time.ParseDuration(fmt.Sprintf("%dm", minutes)) // WARNING: Ignoring errors
-	new := c.time.Add(d)
+	new := c.time().Add(d)
 	return New(new.Hour(), new.Minute())
 }
 
 // Subtract moves the clock backward by the provided number of minutes.
 func (c Clock) Subtract(minutes int) Clock {
 	d, _ := time.ParseDuration(fmt.Sprintf("%dm", minutes)) // WARNING: Ignoring errors
-	new := c.time.Add(-d)
+	new := c.time().Add(-d)
 	return New(new.Hour(), new.Minute())
 }
 
-// It seems strange Go does not include a default implementation of absolute value for ints
-func abs(x int) int {
-	if x < 0 {
-		return -x
-	}
-	return x
+// time converts the clock to a time.Time instance.
+func (c Clock) time() time.Time {
+	return time.Date(0, 0, 0, c.hour, c.min, 0, 0, time.UTC)
 }
