d0fdf9d3dfe6d90ea6e95d803f9956d9efcb02c2
Rootul Patel <rootulp@amazon.com>
2021-05-17 10:47:36 -0400

    Solve matrix using transpose and clone

--- typescript/matrix/matrix.ts
+++ typescript/matrix/matrix.ts
@@ -1,8 +1,10 @@
 class Matrix {
 
   private matrix: number[][];
+  private transposed: number[][];
   constructor(input: string) {
     this.matrix = input.split('\n').map(row => row.split(' ')).map(row => row.map(val => Number.parseInt(val)))
+    this.transposed = this.transpose(this.clone(this.matrix))
   }
 
   public get rows(): number[][] {
@@ -10,8 +12,22 @@ class Matrix {
   }
 
   public get columns(): number[][] {
-    // TODO transpose
-    return this.matrix;
+    return this.transposed;
+  }
+
+  private transpose(matrix: number[][]): number[][] {
+    return matrix[0].map((_, colIndex) => matrix.map(row => row[colIndex]));
+  }
+  private clone(matrix: number[][]): number[][] {
+    let clone: number[][] = [];
+    matrix.forEach(row => {
+      let clonedRow: number[] = [];
+      row.forEach(val => {
+        clonedRow.push(val)
+      })
+      clone.push(clonedRow);
+    });
+    return clone;
   }
 }
 
