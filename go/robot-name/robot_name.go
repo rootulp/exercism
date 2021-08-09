4ac8b346023ea66a442fc23d5c47089b9909fef0
Rootul Patel <rootulp@amazon.com>
2021-08-09 10:23:41 -0400

    Define methods needed on Robot

--- go/robot-name/robot_name.go
+++ go/robot-name/robot_name.go
@@ -1,1 +1,17 @@
 package robotname
+
+type Robot struct {
+	name string
+}
+
+func (r *Robot) Name() (name string, err error) {
+	return r.name, nil
+}
+
+func (r *Robot) Reset() {
+	r.name = generateName()
+}
+
+func generateName() string {
+	return "AA111"
+}
