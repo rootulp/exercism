e154787675a80776bfde53090b7bcf54eb3f4df7
Rootul Patel <rootulp@amazon.com>
2021-08-09 10:53:13 -0400

    Solve robot_name

--- go/robot-name/robot_name.go
+++ go/robot-name/robot_name.go
@@ -1,25 +1,36 @@
 package robotname
 
+import "errors"
+
 type Robot struct {
 	name string
 }
 
+const maxNumberOfNames = 26 * 26 * 10 * 10 * 10
+
 var robotNames = map[string]bool{}
 
 func (r *Robot) Name() (name string, err error) {
+	if len(robotNames) == maxNumberOfNames {
+		return "", errors.New("namespace exhausted")
+	}
 	if r.name == "" {
 		r.name = generateName()
-		robotNames[r.name] = true
 	}
 	return r.name, nil
 }
 
 func (r *Robot) Reset() {
-	delete(robotNames, r.name)
+	// delete(robotNames, r.name)
 	r.name = generateName()
-	robotNames[r.name] = true
 }
 
 func generateName() string {
-	return generateNamePrefix(2) + generateNameSuffix(3)
+	for {
+		name := generateNamePrefix(2) + generateNameSuffix(3)
+		if !robotNames[name] {
+			robotNames[name] = true
+			return name
+		}
+	}
 }
