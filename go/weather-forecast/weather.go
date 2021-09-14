1d037b9ad2bf092501830cef575755724ce891ce
Rootul Patel <rootulp@amazon.com>
2021-09-14 11:17:03 -0400

    Solve weather

--- go/weather-forecast/weather.go
+++ go/weather-forecast/weather.go
@@ -1,10 +1,14 @@
+// Package weather contains utilities for logging weath conditions
 package weather
 
 var (
+	// CurrentCondition describes the current weather condition (e.g. "rainy")
 	CurrentCondition string
-	CurrentLocation  string
+	// CurrentLocation describes the current city (e.g. "New York")
+	CurrentLocation string
 )
 
+// Log returns a string that describes the weath conditions in a particular city
 func Log(city, condition string) string {
 	CurrentLocation, CurrentCondition = city, condition
 	return CurrentLocation + " - current weather condition: " + CurrentCondition
