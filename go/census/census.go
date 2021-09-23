2bc57aee1296e4d881a1e3bad95a6c4fb1e987ee
Rootul Patel <rootulp@amazon.com>
2021-09-23 18:31:15 -0400

    Implement HasRequiredInfo

--- go/census/census.go
+++ go/census/census.go
@@ -1,6 +1,8 @@
 // Packages census simulates a system used to collect census data.
 package census
 
+import "reflect"
+
 // Resident represents a resident in this city.
 type Resident struct {
 	Name    string
@@ -10,12 +12,16 @@ type Resident struct {
 
 // NewResident registers a new resident in this city.
 func NewResident(name string, age int, address map[string]string) *Resident {
-	panic("Please implement NewResident.")
+	return &Resident{
+		Name:    name,
+		Age:     age,
+		Address: address,
+	}
 }
 
 // HasRequiredInfo determines if a given resident has all of the required information.
 func (r *Resident) HasRequiredInfo() bool {
-	panic("Please implement HasRequiredInfo.")
+	return r.Name != "" && r.Age != 0 && !reflect.DeepEqual(r.Address, map[string]string{})
 }
 
 // Delete deletes a resident's information.
