3f91a14d35dab84383e165e9aaa2c92803a71c0a
Rootul Patel <rootulp@amazon.com>
2021-09-23 18:56:37 -0400

    Current impl has a recursion bug

--- go/tree-building/tree_building.go
+++ go/tree-building/tree_building.go
@@ -18,6 +18,34 @@ func Build(records []Record) (*Node, error) {
 	if len(records) == 0 {
 		return nil, nil
 	}
-	root := Node{ID: 0}
-	return &root, nil
+	nodes := map[int]*Node{}
+	nodes[0] = &Node{ID: 0}
+
+	for _, record := range records {
+		// Initialize node if it doesn't exist
+		if _, ok := nodes[record.ID]; !ok {
+			nodes[record.ID] = &Node{ID: record.ID}
+		}
+		// Initialize parent if it doesn't exist
+		if _, ok := nodes[record.Parent]; !ok {
+			nodes[record.Parent] = &Node{ID: record.Parent}
+		}
+		// Populate children for parent
+		children := nodes[record.Parent].Children
+		node := nodes[record.ID]
+		if !contains(children, node) {
+			children = append(children, node)
+			nodes[record.Parent].Children = children
+		}
+	}
+	return nodes[0], nil
+}
+
+func contains(slice []*Node, node *Node) bool {
+	for _, v := range slice {
+		if v == node {
+			return true
+		}
+	}
+	return false
 }
