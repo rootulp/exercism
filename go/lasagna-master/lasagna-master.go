8f9836c96447a527331ef5c3e3d7f86780253c07
Rootul Patel <rootulp@gmail.com>
2021-10-09 12:39:46 -0400

    Implement PreparationTime

--- go/lasagna-master/lasagna-master.go
+++ go/lasagna-master/lasagna-master.go
@@ -1,6 +1,13 @@
 package lasagna
 
-// TODO: define the 'PreparationTime()' function
+func PreparationTime(layers []string, avgPrepTimePerLayer int) int {
+	const DEFAULT_PREP_TIME_PER_LAYER = 2
+
+	if avgPrepTimePerLayer == 0 {
+		return len(layers) * DEFAULT_PREP_TIME_PER_LAYER
+	}
+	return len(layers) * avgPrepTimePerLayer
+}
 
 // TODO: define the 'Quantities()' function
 
