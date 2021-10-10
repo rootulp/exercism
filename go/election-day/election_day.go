9c19431d44c3b1622b63d5df0f1f928810e51e31
Rootul Patel <rootulp@gmail.com>
2021-10-09 21:29:46 -0400

    Solve election day

--- go/election-day/election_day.go
+++ go/election-day/election_day.go
@@ -1,32 +1,40 @@
 package electionday
 
+import "fmt"
+
 // NewVoteCounter returns a new vote counter with
 // a given number of inital votes.
 func NewVoteCounter(initialVotes int) *int {
-	panic("Please implement the NewVoteCounter() function")
+	return &initialVotes
 }
 
 // VoteCount extracts the number of votes from a counter.
 func VoteCount(counter *int) int {
-	panic("Please implement the VoteCount() function")
+	if counter == nil {
+		return 0
+	}
+	return *counter
 }
 
 // IncrementVoteCount increments the value in a vote counter
 func IncrementVoteCount(counter *int, increment int) {
-	panic("Please implement the IncrementVoteCount() function")
+	*counter += increment
 }
 
 // NewElectionResult creates a new election result
 func NewElectionResult(candidateName string, votes int) *ElectionResult {
-	panic("Please implement the NewElectionResult() function")
+	return &ElectionResult{
+		Name:  candidateName,
+		Votes: votes,
+	}
 }
 
 // DisplayResult creates a message with the result to be displayed
 func DisplayResult(result *ElectionResult) string {
-	panic("Please implement the DisplayResult() function")
+	return fmt.Sprintf("%s (%d)", result.Name, result.Votes)
 }
 
 // DecrementVotesOfCandidate decrements by one the vote count of a candidate in a map
 func DecrementVotesOfCandidate(results map[string]int, candidate string) {
-	panic("Please implement the DecrementVotesOfCandidate() function")
+	results[candidate] -= 1
 }
