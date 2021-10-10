85f570a18d1b94ada36a46f78a072930e4bd5dc7
Rootul Patel <rootulp@gmail.com>
2021-10-10 13:05:44 -0400

    Sort teams by points then alphabetically

--- go/tournament/tournament.go
+++ go/tournament/tournament.go
@@ -3,10 +3,12 @@ package tournament
 import (
 	"fmt"
 	"io"
+	"sort"
 	"strings"
 )
 
 type stat struct {
+	Name          string
 	MatchesPlayed int
 	Wins          int
 	Draws         int
@@ -30,24 +32,70 @@ func Tally(r io.Reader, w io.Writer) error {
 		if len(parts) < 3 {
 			continue
 		}
-		fmt.Printf("parts: %s", parts)
+		// fmt.Printf("parts: %s", parts)
 		a := parts[0]
 		b := parts[1]
-		// _ := parts[2]
+		outcome := parts[2]
 
 		if _, ok := teamsToScores[a]; !ok {
-			teamsToScores[a] = &stat{}
+			teamsToScores[a] = &stat{
+				Name: a,
+			}
 		}
 		if _, ok := teamsToScores[b]; !ok {
-			teamsToScores[b] = &stat{}
+			teamsToScores[b] = &stat{
+				Name: b,
+			}
 		}
 
-		teamsToScores[a].MatchesPlayed += 1
-		teamsToScores[b].MatchesPlayed += 1
+		incrementMatchesPlayed(teamsToScores, a)
+		incrementMatchesPlayed(teamsToScores, b)
+
+		switch outcome {
+		case "win":
+			teamsToScores[a].Wins += 1
+			teamsToScores[a].Points += 3
+			teamsToScores[b].Loses += 1
+		case "loss":
+			teamsToScores[a].Loses += 1
+			teamsToScores[b].Wins += 1
+			teamsToScores[b].Points += 3
+		case "draw":
+			teamsToScores[a].Draws += 1
+			teamsToScores[b].Draws += 1
+			teamsToScores[a].Points += 1
+			teamsToScores[b].Points += 1
+		default:
+			return fmt.Errorf("unexpected outcome %s", outcome)
+		}
 	}
 
 	io.WriteString(w, getTable(teamsToScores))
 	return nil
+}
+
+func getTable(teamsToScores map[string]*stat) (result string) {
+	sorted := sortTeamsByPointsThenAlphabetically(teamsToScores)
+	var header = fmt.Sprintf("%-30s |%3s |%3s |%3s |%3s |%3s\n", "Team", "MP", "W", "D", "L", "P")
+	result += header
+	for _, stat := range sorted {
+		result += fmt.Sprintf("%-30s |%3d |%3d |%3d |%3d |%3d\n", stat.Name, stat.MatchesPlayed, stat.Wins, stat.Draws, stat.Loses, stat.Points)
+	}
+	return result
+}
+
+func sortTeamsByPointsThenAlphabetically(teamsToScores map[string]*stat) (result []*stat) {
+	for _, stat := range teamsToScores {
+		result = append(result, stat)
+	}
+	sort.SliceStable(result, func(i, j int) bool {
+		if result[i].Points != result[j].Points {
+			return result[i].Points > result[j].Points
+		}
+		return result[i].Name < result[j].Name
+	})
+
+	return result
 }
 
 func isComment(line string) bool {
@@ -63,12 +111,6 @@ func getLines(r io.Reader) ([]string, error) {
 	return lines, nil
 }
 
-func getTable(teamsToScores map[string]*stat) (result string) {
-	// const header = "Team                           | MP |  W |  D |  L |  P"
-	var header = fmt.Sprintf("%-30s |%3s |%3s |%3s |%3s |%3s\n", "Team", "MP", "W", "D", "L", "P")
-	result += header
-	for team, stat := range teamsToScores {
-		result += fmt.Sprintf("%-30s |%3d |%3d |%3d |%3d |%3d\n", team, stat.MatchesPlayed, stat.Wins, stat.Draws, stat.Loses, stat.Points)
-	}
-	return result
+func incrementMatchesPlayed(teamsToScores map[string]*stat, team string) {
+	teamsToScores[team].MatchesPlayed += 1
 }
