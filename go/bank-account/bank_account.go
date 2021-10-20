6c1d5acc7abb933fe315b5fe419b7442d95baaf4
Rootul Patel <rootulp@gmail.com>
2021-10-19 21:16:37 -0400

    Non concurrent tests passing

--- go/bank-account/bank_account.go
+++ go/bank-account/bank_account.go
@@ -1,20 +1,38 @@
 package account
 
 type Account struct {
+	balance  int64
+	isClosed bool
 }
 
 func Open(initialDeposit int64) *Account {
-	return &Account{}
+	if initialDeposit < 0 {
+		return nil
+	}
+	return &Account{
+		balance: initialDeposit,
+	}
 }
 
-func (*Account) Close() (payout int64, ok bool) {
-	return 0, true
+func (a *Account) Close() (payout int64, ok bool) {
+	a.isClosed = true
+	return a.balance, true
 }
 
-func (*Account) Balance() (balance int64, ok bool) {
-	return 0, true
+func (a *Account) Balance() (balance int64, ok bool) {
+	if a.isClosed {
+		return 0, false
+	}
+	return a.balance, true
 }
 
-func (*Account) Deposit(amount int64) (newBalance int64, ok bool) {
-	return 0, true
+func (a *Account) Deposit(amount int64) (newBalance int64, ok bool) {
+	if a.isClosed {
+		return 0, false
+	}
+	if a.balance+amount < 0 {
+		return 0, false
+	}
+	a.balance += amount
+	return a.balance, true
 }
