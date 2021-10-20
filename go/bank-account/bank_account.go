581e221d13dea6b94f73b9b1bb36ee51f802c621
Rootul Patel <rootulp@gmail.com>
2021-10-19 21:12:12 -0400

    Initial function signatures

--- go/bank-account/bank_account.go
+++ go/bank-account/bank_account.go
@@ -1,1 +1,20 @@
 package account
+
+type Account struct {
+}
+
+func Open(initialDeposit int64) *Account {
+	return &Account{}
+}
+
+func (*Account) Close() (payout int64, ok bool) {
+	return 0, true
+}
+
+func (*Account) Balance() (balance int64, ok bool) {
+	return 0, true
+}
+
+func (*Account) Deposit(amount int64) (newBalance int64, ok bool) {
+	return 0, true
+}
