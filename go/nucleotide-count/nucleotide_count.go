40b5d018a1b0e9e3fada813e6afb01b267bda88c
Rootul Patel <rootulp@gmail.com>
2021-10-22 11:21:54 -0400

    Count valid nucleotides

--- go/nucleotide-count/nucleotide_count.go
+++ go/nucleotide-count/nucleotide_count.go
@@ -1,21 +1,21 @@
 package dna
 
 // Histogram is a mapping from nucleotide to its count in given DNA.
-// Choose a suitable data type.
 type Histogram map[rune]int
 
-// DNA is a list of nucleotides. Choose a suitable data type.
 type DNA string
 
 // Counts generates a histogram of valid nucleotides in the given DNA.
 // Returns an error if d contains an invalid nucleotide.
-///
 func (d DNA) Counts() (Histogram, error) {
 	h := Histogram{
 		'A': 0,
 		'C': 0,
 		'G': 0,
 		'T': 0,
+	}
+	for _, r := range d {
+		h[r] += 1
 	}
 	return h, nil
 }
