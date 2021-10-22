66306253961fbff152d00755910bb1327368055b
Rootul Patel <rootulp@gmail.com>
2021-10-22 11:25:41 -0400

    Throw error if DNA strand is invalid

--- go/nucleotide-count/nucleotide_count.go
+++ go/nucleotide-count/nucleotide_count.go
@@ -1,5 +1,7 @@
 package dna
 
+import "fmt"
+
 // Histogram is a mapping from nucleotide to its count in given DNA.
 type Histogram map[rune]int
 
@@ -8,6 +10,9 @@ type DNA string
 // Counts generates a histogram of valid nucleotides in the given DNA.
 // Returns an error if d contains an invalid nucleotide.
 func (d DNA) Counts() (Histogram, error) {
+	if !d.isValid() {
+		return Histogram{}, fmt.Errorf("DNA stand %s contains invalid nucleotides", d)
+	}
 	h := Histogram{
 		'A': 0,
 		'C': 0,
@@ -18,4 +23,15 @@ func (d DNA) Counts() (Histogram, error) {
 		h[r] += 1
 	}
 	return h, nil
+}
+
+func (d DNA) isValid() bool {
+	for _, r := range d {
+		if r == 'A' || r == 'C' || r == 'G' || r == 'T' {
+			continue
+		} else {
+			return false
+		}
+	}
+	return true
 }
