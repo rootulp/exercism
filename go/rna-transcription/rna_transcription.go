64da7047e9d0cc3be9d694160e4a39e1f2bffc14
Rootul Patel <rootulp@gmail.com>
2021-10-25 20:15:06 -0400

    Solve rna transcription

--- go/rna-transcription/rna_transcription.go
+++ go/rna-transcription/rna_transcription.go
@@ -1,5 +1,16 @@
 package strand
 
+var dnaToRna = map[rune]rune{
+	'C': 'G',
+	'G': 'C',
+	'T': 'A',
+	'A': 'U',
+}
+
 func ToRNA(dna string) string {
-	panic("Please implement the ToRNA function")
+	result := ""
+	for _, r := range dna {
+		result += string(dnaToRna[r])
+	}
+	return result
 }
