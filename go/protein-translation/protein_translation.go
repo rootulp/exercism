b4683f4a5b6829ab7d409516239ed8c5b0a367f5
Rootul Patel <rootulp@gmail.com>
2021-10-27 23:04:16 -0400

    Define necessary errors and functions

--- go/protein-translation/protein_translation.go
+++ go/protein-translation/protein_translation.go
@@ -1,1 +1,17 @@
 package protein
+
+import "errors"
+
+// ErrStop represents a STOP codon
+var ErrStop error = errors.New("stop codon")
+
+// ErrInvalidBase represents an invalid base that cannot me mapped to an amino acid.
+var ErrInvalidBase error = errors.New("invalid base")
+
+func FromCodon(codon string) (protein string, e error) {
+	return "foo", nil
+}
+
+func FromRNA(codons string) (proteins string, e error) {
+	return "bar", nil
+}
