e5a0f8ed4a2943f24c72caa0379a888e61cf9707
Rootul Patel <rootulp@gmail.com>
2021-11-12 18:59:06 -0500

    Solve proverb

--- go/proverb/proverb.go
+++ go/proverb/proverb.go
@@ -1,15 +1,32 @@
-// This is a "stub" file.  It's a little start on your solution.
-// It's not a complete solution though; you have to write some code.
-
-// Package proverb should have a package comment that summarizes what it's about.
-// https://golang.org/doc/effective_go.html#commentary
 package proverb
 
-// Proverb should have a comment documenting it.
+import "fmt"
+
 func Proverb(rhyme []string) []string {
-	// Write some code here to pass the test suite.
-	// Then remove all the stock comments.
-	// They're here to help you get started but they only clutter a finished solution.
-	// If you leave them in, reviewers may protest!
-	return []string{}
+	if len(rhyme) == 0 {
+		return []string{}
+	} else if len(rhyme) == 1 {
+		return []string{footer(rhyme[0])}
+	}
+	return multilineProverb(rhyme)
+}
+
+func multilineProverb(rhyme []string) (result []string) {
+	for i, item := range rhyme {
+		if i == len(rhyme)-1 {
+			result = append(result, footer(rhyme[0]))
+		} else {
+			result = append(result, verse(item, rhyme[i+1]))
+		}
+	}
+	return result
+}
+
+func verse(itemA string, itemB string) string {
+	return fmt.Sprintf("For want of a %s the %s was lost.", itemA, itemB)
+
+}
+
+func footer(item string) string {
+	return fmt.Sprintf("And all for the want of a %s.", item)
 }
