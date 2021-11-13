ad2d7f4688a4b0fa392e5982e7b698dbf8672569
Rootul Patel <rootulp@gmail.com>
2021-11-13 14:40:40 -0500

    Handle error cases

--- go/roman-numerals/roman_numerals.go
+++ go/roman-numerals/roman_numerals.go
@@ -1,5 +1,12 @@
 package romannumerals
 
+import "errors"
+
 func ToRomanNumeral(input int) (string, error) {
-	panic("Please implement the ToRomanNumeral function")
+	if input <= 0 {
+		return "", errors.New("input must be greater than 0")
+	} else if input >= 3000 {
+		return "", errors.New("input must be less than or equal to 3000")
+	}
+	return "0", nil
 }
