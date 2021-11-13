70fcb05a00cd4b485a094daefeeb05be7c271e5c
Rootul Patel <rootulp@gmail.com>
2021-11-13 14:44:03 -0500

    Convert digit to numeral

--- go/roman-numerals/roman_numerals.go
+++ go/roman-numerals/roman_numerals.go
@@ -5,8 +5,24 @@ import "errors"
 func ToRomanNumeral(input int) (string, error) {
 	if input <= 0 {
 		return "", errors.New("input must be greater than 0")
-	} else if input >= 3000 {
+	} else if input > 3000 {
 		return "", errors.New("input must be less than or equal to 3000")
 	}
-	return "0", nil
+	output := convertDigitToRomanNumeral(input)
+	return output, nil
+}
+
+func convertDigitToRomanNumeral(digit int) string {
+	digitToRomanNumeral := map[int]string{
+		1: "I",
+		2: "II",
+		3: "III",
+		4: "IV",
+		5: "V",
+		6: "VI",
+		7: "VII",
+		8: "VIII",
+		9: "IX",
+	}
+	return digitToRomanNumeral[digit]
 }
