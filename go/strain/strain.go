acf26f3dcd11bc33ccd70d23c4a0a559ce7e1b32
Rootul Patel <rootulp@gmail.com>
2021-11-14 19:24:58 -0500

    Start strain

---
+++ go/strain/strain.go
@@ -0,0 +1,33 @@
+package strain
+
+type Ints []int
+type Lists [][]int
+type Strings []string
+
+func (i Ints) Keep(filter func(int) bool) Ints {
+	if i == nil {
+		return nil
+	}
+	panic("Please implement the Keep function")
+}
+
+func (i Ints) Discard(filter func(int) bool) Ints {
+	if i == nil {
+		return nil
+	}
+	panic("Please implement the Discard function")
+}
+
+func (l Lists) Keep(filter func([]int) bool) Lists {
+	if l == nil {
+		return nil
+	}
+	panic("Please implement the Keep function")
+}
+
+func (s Strings) Keep(filter func(string) bool) Strings {
+	if s == nil {
+		return nil
+	}
+	panic("Please implement the Keep function")
+}
