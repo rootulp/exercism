dc16c5c995a45c364796d3f445193bd725cf7ed7
Rootul Patel <rootulp@gmail.com>
2021-11-18 20:48:05 -0500

    Solve etl

--- go/etl/etl.go
+++ go/etl/etl.go
@@ -1,5 +1,13 @@
 package etl
 
-func Transform(in map[int][]string) map[string]int {
-	panic("Please implement the Transform function")
+import "strings"
+
+func Transform(in map[int][]string) (result map[string]int) {
+	result = map[string]int{}
+	for score, letters := range in {
+		for _, letter := range letters {
+			result[strings.ToLower(letter)] = score
+		}
+	}
+	return result
 }
