e222197ee773d5ceaf5f6f9ba6a6c1485deb9344
Rootul Patel <rootulp@gmail.com>
2021-11-19 21:26:53 -0500

    Solve pangram

--- go/pangram/pangram.go
+++ go/pangram/pangram.go
@@ -1,5 +1,24 @@
 package pangram
 
+import "strings"
+
+const alphabet = "abcdefghijklmnopqrstuvwxyz"
+
 func IsPangram(input string) bool {
-	panic("Please implement the IsPangram function")
+	set := getSetOfUsedRunes(strings.ToLower(input))
+	alphabetSet := getSetOfUsedRunes(alphabet)
+	for r, v := range alphabetSet {
+		if set[r] != v {
+			return false
+		}
+	}
+	return true
+}
+
+func getSetOfUsedRunes(input string) map[rune]bool {
+	set := map[rune]bool{}
+	for _, r := range input {
+		set[r] = true
+	}
+	return set
 }
