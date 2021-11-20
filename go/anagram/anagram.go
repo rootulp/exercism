8a7afd865f5e2625fdf5bf1405e4032abd6f242a
Rootul Patel <rootulp@gmail.com>
2021-11-20 14:11:17 -0500

    Solve anagram

--- go/anagram/anagram.go
+++ go/anagram/anagram.go
@@ -1,5 +1,25 @@
 package anagram
 
-func Detect(subject string, candidates []string) []string {
-	panic("Please implement the Detect function")
+import (
+	"reflect"
+	"strings"
+)
+
+func Detect(subject string, candidates []string) (anagrams []string) {
+	occurences := getOccurences(strings.ToLower(subject))
+	for _, c := range candidates {
+		candidate := getOccurences(strings.ToLower(c))
+		if reflect.DeepEqual(occurences, candidate) && strings.ToLower(subject) != strings.ToLower(c) {
+			anagrams = append(anagrams, c)
+		}
+	}
+	return anagrams
+}
+
+func getOccurences(input string) (occurences map[rune]int) {
+	occurences = map[rune]int{}
+	for _, r := range input {
+		occurences[r] += 1
+	}
+	return occurences
 }
