a2f51b2d5a7253d707b0970f4389c26714342d4b
Rootul Patel <rootulp@gmail.com>
2021-11-21 12:40:29 -0500

    Solve word count

--- go/word-count/word_count.go
+++ go/word-count/word_count.go
@@ -1,7 +1,30 @@
 package wordcount
 
+import (
+	"strings"
+	"unicode"
+)
+
 type Frequency map[string]int
 
-func WordCount(phrase string) Frequency {
-	panic("Please implement the WordCount function")
+func WordCount(phrase string) (result Frequency) {
+	result = Frequency{}
+	splitter := func(c rune) bool {
+		return unicode.IsSpace(c) || (unicode.IsPunct(c) && c != '\'')
+	}
+	fields := strings.FieldsFunc(phrase, splitter)
+	for _, word := range fields {
+		if doesContainsLetterOrNumber(word) {
+			w := strings.Trim(strings.ToLower(word), "'")
+			result[strings.ToLower(w)] += 1
+		}
+	}
+	return result
+}
+
+func doesContainsLetterOrNumber(word string) bool {
+	containsLetterOrNumber := func(c rune) bool {
+		return unicode.IsLetter(c) || unicode.IsNumber(c)
+	}
+	return strings.IndexFunc(word, containsLetterOrNumber) != -1
 }
