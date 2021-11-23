e0cc29b12d2e17a838899b3a74caab48109c772f
Rootul Patel <rootulp@gmail.com>
2021-11-23 10:23:29 -0500

    Solve first few test cases

--- go/largest-series-product/largest_series_product.go
+++ go/largest-series-product/largest_series_product.go
@@ -1,5 +1,31 @@
 package lsproduct
 
-func LargestSeriesProduct(digits string, span int) (int64, error) {
-	panic("Please implement the LargestSeriesProduct function")
+import (
+	"fmt"
+	"log"
+	"strconv"
+)
+
+func LargestSeriesProduct(digits string, span int) (largestProduct int64, err error) {
+	fmt.Printf("LargestSeriesProduct(%v, %v)\n", digits, span)
+	for i := 0; i <= len(digits)-span; i++ {
+		product := getProduct(digits[i : i+span])
+		if product > largestProduct {
+			largestProduct = product
+		}
+	}
+	return largestProduct, nil
+}
+
+func getProduct(digits string) (product int64) {
+	product = 1
+	for _, digit := range digits {
+		d, err := strconv.Atoi(string(digit))
+		if err != nil {
+			log.Fatalf("cannot convert %v to int", digit)
+		}
+		product *= int64(d)
+	}
+	fmt.Printf("getProduct(%v)=%v\n", digits, product)
+	return product
 }
