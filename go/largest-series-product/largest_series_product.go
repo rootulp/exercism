f6549a5db40094a61d5a8799ab9d4d8b5e73a457
Rootul Patel <rootulp@gmail.com>
2021-11-23 10:26:40 -0500

    Solve largest series product

--- go/largest-series-product/largest_series_product.go
+++ go/largest-series-product/largest_series_product.go
@@ -1,15 +1,24 @@
 package lsproduct
 
 import (
+	"errors"
 	"fmt"
-	"log"
 	"strconv"
 )
 
-func LargestSeriesProduct(digits string, span int) (largestProduct int64, err error) {
+func LargestSeriesProduct(digits string, span int) (largestProduct int64, e error) {
+	if span < 0 {
+		return -1, errors.New("span must not be negative")
+	}
+	if span > len(digits) {
+		return -1, errors.New("error span can not be larger than the number of digits provided")
+	}
 	fmt.Printf("LargestSeriesProduct(%v, %v)\n", digits, span)
 	for i := 0; i <= len(digits)-span; i++ {
-		product := getProduct(digits[i : i+span])
+		product, err := getProduct(digits[i : i+span])
+		if err != nil {
+			return -1, err
+		}
 		if product > largestProduct {
 			largestProduct = product
 		}
@@ -17,15 +26,15 @@ func LargestSeriesProduct(digits string, span int) (largestProduct int64, err er
 	return largestProduct, nil
 }
 
-func getProduct(digits string) (product int64) {
+func getProduct(digits string) (product int64, e error) {
 	product = 1
 	for _, digit := range digits {
 		d, err := strconv.Atoi(string(digit))
 		if err != nil {
-			log.Fatalf("cannot convert %v to int", digit)
+			return -1, fmt.Errorf("cannot conver %v to int", digit)
 		}
 		product *= int64(d)
 	}
 	fmt.Printf("getProduct(%v)=%v\n", digits, product)
-	return product
+	return product, nil
 }
