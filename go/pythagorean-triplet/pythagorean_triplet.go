fadd7cfa3417360fc660e16a4ed642749f2e36ed
Rootul Patel <rootulp@gmail.com>
2021-11-25 14:29:43 -0500

    Implement Range

--- go/pythagorean-triplet/pythagorean_triplet.go
+++ go/pythagorean-triplet/pythagorean_triplet.go
@@ -1,11 +1,26 @@
 package pythagorean
 
+import "math"
+
 type Triplet [3]int
 
 // Range returns a list of all Pythagorean triplets with sides in the
 // range min to max inclusive.
-func Range(min, max int) []Triplet {
-	panic("Please implement the Range function")
+func Range(min, max int) (result []Triplet) {
+	for i := min; i <= max; i++ {
+		for j := i + 1; j <= max; j++ {
+			for k := j + 1; k <= max; k++ {
+				if isTriplet(i, j, k) {
+					result = append(result, Triplet{i, j, k})
+				}
+			}
+		}
+	}
+	return result
+}
+
+func isTriplet(a int, b int, c int) bool {
+	return math.Pow(float64(a), 2)+math.Pow(float64(b), 2) == math.Pow(float64(c), 2)
 }
 
 // Sum returns a list of all Pythagorean triplets where the sum a+b+c
