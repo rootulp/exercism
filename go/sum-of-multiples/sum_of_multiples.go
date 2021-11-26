67d498a5a2805d937731c9df8ee6a3991912ffc8
Rootul Patel <rootulp@gmail.com>
2021-11-26 13:40:48 -0500

    Solve sum of multiples

--- go/sum-of-multiples/sum_of_multiples.go
+++ go/sum-of-multiples/sum_of_multiples.go
@@ -1,5 +1,38 @@
 package summultiples
 
-func SumMultiples(limit int, divisors ...int) int {
-	panic("Please implement the SumMultiples function")
+import "fmt"
+
+func SumMultiples(limit int, divisors ...int) (sum int) {
+	multiples := getMultiples(limit, divisors)
+	for multiple := range multiples {
+		sum += multiple
+	}
+	return sum
+}
+
+func getMultiples(limit int, divisors []int) (multiples map[int]bool) {
+	multiples = map[int]bool{}
+	for _, divisor := range divisors {
+		multiplesForDivisor := getMultiplesForDivisor(limit, divisor)
+		for _, multiple := range multiplesForDivisor {
+			multiples[multiple] = true
+		}
+	}
+	return multiples
+}
+
+func getMultiplesForDivisor(limit int, divisor int) (multiples []int) {
+	if divisor == 0 {
+		return multiples
+	}
+	multiplyer := 1
+	candidate := divisor * multiplyer
+	for candidate < limit {
+		fmt.Printf("candidate %d, multiplyer %d, limit %d\n", candidate, multiplyer, limit)
+		multiples = append(multiples, candidate)
+		multiplyer += 1
+		candidate = divisor * multiplyer
+	}
+	fmt.Printf("returning multiples %v\n", multiples)
+	return multiples
 }
