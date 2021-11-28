a62365be79880dbe92c79ccfc3935b0e7221b087
Rootul Patel <rootulp@gmail.com>
2021-11-28 15:20:01 -0500

    Solve atbash

--- go/atbash-cipher/atbash_cipher.go
+++ go/atbash-cipher/atbash_cipher.go
@@ -1,5 +1,67 @@
 package atbash
 
-func Atbash(s string) string {
-	panic("Please implement the Atbash function")
+import (
+	"math"
+	"strings"
+	"unicode"
+)
+
+// Plain:  abcdefghijklmnopqrstuvwxyz
+// Cipher: zyxwvutsrqponmlkjihgfedcba
+var atbash = map[rune]rune{
+	'a': 'z',
+	'b': 'y',
+	'c': 'x',
+	'd': 'w',
+	'e': 'v',
+	'f': 'u',
+	'g': 't',
+	'h': 's',
+	'i': 'r',
+	'j': 'q',
+	'k': 'p',
+	'l': 'o',
+	'm': 'n',
+	'n': 'm',
+	'o': 'l',
+	'p': 'k',
+	'q': 'j',
+	'r': 'i',
+	's': 'h',
+	't': 'g',
+	'u': 'f',
+	'v': 'e',
+	'w': 'd',
+	'x': 'c',
+	'y': 'b',
+	'z': 'a',
+}
+
+func Atbash(message string) (ciphertext string) {
+	for _, r := range removeNonAlphanumeric(strings.ToLower(message)) {
+		if unicode.IsDigit(r) {
+			ciphertext += string(r)
+		} else {
+			ciphertext += string(atbash[r])
+		}
+	}
+	return splitEveryN(ciphertext, 5)
+}
+
+func removeNonAlphanumeric(s string) (result string) {
+	for _, r := range s {
+		if unicode.IsDigit(r) || unicode.IsLetter(r) {
+			result += string(r)
+		}
+	}
+	return result
+}
+
+func splitEveryN(message string, n int) (result string) {
+	arr := []string{}
+	for i := 0; i < len(message); i += n {
+		upperBound := math.Min(float64(len(message)), float64(i+n))
+		arr = append(arr, message[i:int(upperBound)])
+	}
+	return strings.Join(arr, " ")
 }
