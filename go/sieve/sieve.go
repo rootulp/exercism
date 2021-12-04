1673dbadbaeda40e7ac66b23cee110fc50135c19
Rootul Patel <rootulp@gmail.com>
2021-12-04 16:50:20 -0500

    Solve sieve

--- go/sieve/sieve.go
+++ go/sieve/sieve.go
@@ -1,5 +1,46 @@
 package sieve
 
-func Sieve(limit int) []int {
-	panic("Please implement the Sieve function")
+func Sieve(limit int) (primes []int) {
+	if limit < 2 {
+		return []int{}
+	}
+	unprocessed := createRange(2, limit)
+	for len(unprocessed) != 0 {
+		prime := unprocessed[0]
+		unprocessed = unprocessed[1:]
+
+		unprocessed = removeMultiples(unprocessed, prime)
+		primes = append(primes, prime)
+	}
+	return primes
+}
+
+func removeMultiples(slice []int, prime int) (filtered []int) {
+	if len(slice) == 0 {
+		return slice
+	}
+	multiplyer := 2
+	multiple := multiplyer * prime
+	for len(slice) != 0 && multiple <= slice[len(slice)-1] {
+		slice = remove(slice, multiple)
+		multiplyer += 1
+		multiple = multiplyer * prime
+	}
+	return slice
+}
+
+func remove(slice []int, value int) (result []int) {
+	for _, v := range slice {
+		if v != value {
+			result = append(result, v)
+		}
+	}
+	return result
+}
+
+func createRange(lower int, upper int) (result []int) {
+	for i := lower; i <= upper; i++ {
+		result = append(result, i)
+	}
+	return result
 }
