d341650904a9bbdc85ed1a782612c657e4b70231
Rootul Patel <rootulp@gmail.com>
2021-12-05 16:58:35 -0500

    Implement encode

--- go/run-length-encoding/run_length_encoding.go
+++ go/run-length-encoding/run_length_encoding.go
@@ -1,7 +1,34 @@
 package encode
 
+import "fmt"
+
 func RunLengthEncode(input string) (encoded string) {
-	return input
+	if input == "" {
+		return input
+	}
+	var prevChar rune
+	prevOccurences := 0
+	for _, char := range input {
+		if prevChar == 0 { // an uninitizlied rune is 0
+			prevChar = char
+			prevOccurences += 1
+		} else if char != prevChar {
+			encoded += encodeSegment(prevChar, prevOccurences)
+			prevChar = char
+			prevOccurences = 1
+		} else if char == prevChar {
+			prevOccurences += 1
+		}
+	}
+	encoded += encodeSegment(prevChar, prevOccurences)
+	return encoded
+}
+
+func encodeSegment(char rune, occurences int) string {
+	if occurences == 1 {
+		return string(char)
+	}
+	return fmt.Sprintf("%d%v", occurences, string(char))
 }
 
 func RunLengthDecode(input string) (decoded string) {
