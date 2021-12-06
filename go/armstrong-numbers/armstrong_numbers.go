71340d4d3c1fa26431b9a9c9e9536b0dece3a00b
Rootul Patel <rootulp@gmail.com>
2021-12-06 18:42:51 -0500

    Solve armstrong

--- go/armstrong-numbers/armstrong_numbers.go
+++ go/armstrong-numbers/armstrong_numbers.go
@@ -1,5 +1,24 @@
 package armstrong
 
+import (
+	"fmt"
+	"math"
+	"strconv"
+)
+
 func IsNumber(n int) bool {
-	panic("Please implement the IsNumber function")
+	sum := 0
+	digits := fmt.Sprint(n)
+	for _, digit := range digits {
+		d, err := strconv.Atoi(string(digit))
+		if err != nil {
+			panic(err)
+		}
+		sum += intPow(d, len(digits))
+	}
+	return sum == n
+}
+
+func intPow(n int, power int) int {
+	return int(math.Pow(float64(n), float64(power)))
 }
