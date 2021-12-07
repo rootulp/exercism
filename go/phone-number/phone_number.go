69269e6470f0bf9bcd78770fea6b6c99945fd32f
Rootul Patel <rootulp@gmail.com>
2021-12-07 17:31:30 -0500

    Throw error if exchange code starts with 0 or 1

--- go/phone-number/phone_number.go
+++ go/phone-number/phone_number.go
@@ -23,15 +23,27 @@ func Number(phoneNumber string) (number string, e error) {
 	if err != nil {
 		return "", err
 	}
+	_, err = exchangeCode(number)
+	if err != nil {
+		return "", err
+	}
 	return number, nil
 }
 
 func AreaCode(phoneNumber string) (areaCode string, e error) {
 	areaCode = phoneNumber[0:3]
 	if strings.HasPrefix(areaCode, "0") || strings.HasPrefix(areaCode, "1") {
-		return "", fmt.Errorf("area code %v can not start with 0", areaCode)
+		return "", fmt.Errorf("area code %v can not start with 0 or 1", areaCode)
 	}
 	return areaCode, nil
+}
+
+func exchangeCode(phoneNumber string) (exchangeCode string, e error) {
+	exchangeCode = phoneNumber[3:6]
+	if strings.HasPrefix(exchangeCode, "0") || strings.HasPrefix(exchangeCode, "1") {
+		return "", fmt.Errorf("exchange code %v can not start with 0 or 1", exchangeCode)
+	}
+	return exchangeCode, nil
 }
 
 func Format(phoneNumber string) (string, error) {
