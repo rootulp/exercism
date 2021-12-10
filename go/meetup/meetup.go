b2e60bee5f79003b93cf2d482625b5581094ff76
Rootul Patel <rootulp@gmail.com>
2021-12-10 01:09:44 -0500

    Pass first few test cases

--- go/meetup/meetup.go
+++ go/meetup/meetup.go
@@ -1,9 +1,53 @@
 package meetup
 
-import "time"
+import (
+	"fmt"
+	"time"
+)
 
-// Define the WeekSchedule type here.
+type WeekSchedule string
+
+const (
+	First  WeekSchedule = "first"
+	Second              = "second"
+	Third               = "third"
+	Fourth              = "fourth"
+	Teenth              = "teenth"
+	Last                = "last"
+)
 
 func Day(week WeekSchedule, weekday time.Weekday, month time.Month, year int) int {
-	panic("Please implement the Day function")
+	time := time.Date(year, month, 1, 0, 0, 0, 0, time.UTC)
+	fmt.Printf("weekday %v and week %v so weekSchedule(%v) %v\n", weekday, week, time, weekSchedule(time))
+	for time.Weekday() != weekday || weekSchedule(time) != week {
+		fmt.Printf("weekSchedule(%v) %v\n", time, weekSchedule(time))
+		time = time.AddDate(0, 0, 1) // move forward one day
+	}
+	fmt.Printf("%v\n", time)
+	return time.Day()
+}
+
+func weekSchedule(t time.Time) WeekSchedule {
+	if t.Day() == 1 {
+		return First
+	}
+	if t.Day() == 2 {
+		return Second
+	}
+	if t.Day() == 3 {
+		return Third
+	}
+	if t.Day() == 4 {
+		return Fourth
+	}
+	if t.Day() > 12 && t.Day() < 20 {
+		return Teenth
+	}
+	lastDay := time.Date(t.Year(), t.Month()+1, 0, 0, 0, 0, 0, time.UTC).Day()
+	fmt.Printf("Last day in month %v is %v\n", t, lastDay)
+	if t.Day() == lastDay {
+		fmt.Printf("t.Day() = %v and lastDay %v", t.Day(), lastDay)
+		return Last
+	}
+	return "none"
 }
