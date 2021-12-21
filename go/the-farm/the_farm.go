697b5b67f46e17fa4ec0dac8212ddedfbc966395
Rootul Patel <rootulp@gmail.com>
2021-12-21 18:39:33 -0500

    Solve the farm
    Really low quality exercise. The narrative doesn't make any sense and
    the test cases want error strings that are capitalized.
    
    The error handling logic isn't sane.

--- go/the-farm/the_farm.go
+++ go/the-farm/the_farm.go
@@ -1,10 +1,29 @@
 package thefarm
 
-// See types.go for the types defined for this exercise.
-
-// TODO: Define the SillyNephewError type here.
+import (
+	"errors"
+	"fmt"
+)
 
 // DivideFood computes the fodder amount per cow for the given cows.
 func DivideFood(weightFodder WeightFodder, cows int) (float64, error) {
-	panic("Please implement DivideFood")
+	fodderAmount, err := weightFodder.FodderAmount()
+	if fodderAmount < 0 {
+		// This exercism problem is flawed because it expects a capitalized
+		// error string which violate go lint
+		return 0, errors.New("Negative fodder")
+	}
+	if err == ErrScaleMalfunction {
+		return fodderAmount * 2 / float64(cows), nil
+	}
+	if err != nil {
+		return 0, err
+	}
+	if cows == 0 {
+		return 0, errors.New("Division by zero")
+	}
+	if cows < 0 {
+		return 0, fmt.Errorf("silly nephew, there cannot be %d cows", cows)
+	}
+	return fodderAmount / float64(cows), nil
 }
