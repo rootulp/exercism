fe72809f1b298359a7e17bbe3baad5db55aafafc
Rootul Patel <rootulp@gmail.com>
2021-12-22 18:21:32 -0500

    Implement DescribeNumber

--- go/sorting-room/sorting_room.go
+++ go/sorting-room/sorting_room.go
@@ -1,8 +1,9 @@
 package sorting
 
-// DescribeNumber should return a string describing the number.
+import "fmt"
+
 func DescribeNumber(f float64) string {
-	panic("Please implement DescribeNumber")
+	return fmt.Sprintf("This is the number %.1f", f)
 }
 
 type NumberBox interface {
