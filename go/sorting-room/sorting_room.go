84b2a284ae8d7b03181cb4e072b336d8bb70e5ee
Rootul Patel <rootulp@gmail.com>
2021-12-22 18:26:13 -0500

    Implement DescribeFancyNumberBox

--- go/sorting-room/sorting_room.go
+++ go/sorting-room/sorting_room.go
@@ -37,9 +37,8 @@ func ExtractFancyNumber(fnb FancyNumberBox) int {
 	return result
 }
 
-// DescribeFancyNumberBox should return a string describing the FancyNumberBox.
 func DescribeFancyNumberBox(fnb FancyNumberBox) string {
-	panic("Please implement DescribeFancyNumberBox")
+	return fmt.Sprintf("This is a fancy box containing the number %.1f", float64(ExtractFancyNumber(fnb)))
 }
 
 // DescribeAnything should return a string describing whatever it contains.
