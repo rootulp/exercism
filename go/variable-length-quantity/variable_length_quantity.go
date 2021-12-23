1d7f3ed5ca4bb42f835e52415159cab8bbd32ed5
Rootul Patel <rootulp@gmail.com>
2021-12-23 12:51:23 -0500

    Implement encode
    Took inspiration from
    https://exercism.org/tracks/go/exercises/variable-length-quantity/solutions/paulghaddad

--- go/variable-length-quantity/variable_length_quantity.go
+++ go/variable-length-quantity/variable_length_quantity.go
@@ -1,7 +1,31 @@
 package variablelengthquantity
 
-func EncodeVarint(input []uint32) []byte {
-	panic("Please implement the EncodeVarint function")
+func EncodeVarint(input []uint32) (encoded []byte) {
+	var remainder uint32
+
+	for _, val := range input {
+		var base128Val []byte
+
+		if val == 0 {
+			encoded = append(encoded, byte(0))
+		}
+
+		// Convert val to base128 representation
+		for val > 0 {
+			val, remainder = val/128, val%128
+			// Prepend remainder to base128Val
+			base128Val = append([]byte{byte(remainder)}, base128Val...)
+		}
+
+		for i, val := range base128Val {
+			// Add a leading bit if this is not the last byte
+			if i < len(base128Val)-1 {
+				val = 0b10000000 | val
+			}
+			encoded = append(encoded, val)
+		}
+	}
+	return encoded
 }
 
 func DecodeVarint(input []byte) ([]uint32, error) {
