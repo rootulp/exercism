fa194d5511d0cdc0c579bb36d7e456a354d43acd
Rootul Patel <rootulp@gmail.com>
2021-12-24 16:54:04 -0500

    Solve secret handshake

--- go/secret-handshake/secret_handshake.go
+++ go/secret-handshake/secret_handshake.go
@@ -11,8 +11,11 @@ func Handshake(code uint) (handshake []string) {
 	binary := strconv.FormatInt(int64(code), 2)
 	fmt.Printf("code %v in binary %v\n", code, binary)
 	for i, v := range reverse(binary) {
-		if v == '1' {
+		if v == '1' && i < len(operations) {
 			handshake = append(handshake, operations[i])
+		}
+		if v == '1' && i == len(operations) {
+			handshake = reverseSlice(handshake)
 		}
 	}
 	return handshake
@@ -21,6 +24,13 @@ func Handshake(code uint) (handshake []string) {
 func reverse(input string) (result string) {
 	for _, v := range input {
 		result = string(v) + result
+	}
+	return result
+}
+
+func reverseSlice(input []string) (result []string) {
+	for _, v := range input {
+		result = append([]string{v}, result...)
 	}
 	return result
 }
