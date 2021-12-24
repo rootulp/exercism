fec94f5e25f43a33bc72a80ecb9a77dd53d7d96b
Rootul Patel <rootulp@gmail.com>
2021-12-24 16:01:18 -0500

    Implement types

--- go/perfect-numbers/perfect_numbers.go
+++ go/perfect-numbers/perfect_numbers.go
@@ -1,6 +1,14 @@
 package perfect
 
-// Define the Classification type here.
+import "errors"
+
+type Classification string
+
+const ClassificationAbundant Classification = "ClassificationAbundant"
+const ClassificationDeficient Classification = "ClassificationDeficient"
+const ClassificationPerfect Classification = "ClassificationPerfect"
+
+var ErrOnlyPositive = errors.New("error only positive numbers can be classified")
 
 func Classify(n int64) (Classification, error) {
 	panic("Please implement the Classify function")
