f9aa08101f432a4aa39b665c1ae0188dfb464c09
Rootul Patel <rootulp@gmail.com>
2021-12-24 18:40:29 -0500

    Solve binary search

--- go/binary-search/binary_search.go
+++ go/binary-search/binary_search.go
@@ -1,5 +1,39 @@
 package binarysearch
 
-func SearchInts(list []int, key int) int {
-	panic("Please implement the SearchInts function")
+import "fmt"
+
+// SearchInts performs a binary search for key in list and returns the index if
+// key is found. Returns `-1` if key is not in list.
+func SearchInts(list []int, key int) (index int) {
+	return binarySearch(list, key, 0, len(list))
+}
+
+func binarySearch(list []int, key int, start int, end int) (index int) {
+	if len(list) == 0 {
+		return -1
+	}
+	if end < start {
+		return -1
+	}
+	if start >= len(list) {
+		return -1
+	}
+	if start == end {
+		if key == list[start] {
+			return start
+		} else {
+			return -1
+		}
+	}
+	middle := ((end - start) / 2) + start
+	fmt.Printf("binarySearch(%v, %v, %v, %v)\n", list, key, start, end)
+	if key == list[middle] {
+		return middle
+	} else if key > list[middle] {
+		return binarySearch(list, key, middle+1, end)
+	} else if key < list[middle] {
+		return binarySearch(list, key, start, middle)
+	}
+	fmt.Printf("unreachable\n")
+	return -1
 }
