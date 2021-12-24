2f0dfb9e0dbf1b2d755ec3f5949263b9c644fee5
Rootul Patel <rootulp@gmail.com>
2021-12-24 16:55:33 -0500

    Remove debug log

--- go/secret-handshake/secret_handshake.go
+++ go/secret-handshake/secret_handshake.go
@@ -1,7 +1,6 @@
 package secret
 
 import (
-	"fmt"
 	"strconv"
 )
 
@@ -9,7 +8,6 @@ var operations = []string{"wink", "double blink", "close your eyes", "jump"}
 
 func Handshake(code uint) (handshake []string) {
 	binary := strconv.FormatInt(int64(code), 2)
-	fmt.Printf("code %v in binary %v\n", code, binary)
 	for i, v := range reverse(binary) {
 		if v == '1' && i < len(operations) {
 			handshake = append(handshake, operations[i])
