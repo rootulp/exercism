6edad6de0b524bef6a8810c7e943b5b7d4580f77
Rootul Patel <rootulp@gmail.com>
2021-12-24 16:48:40 -0500

    Pass first few cases

--- go/secret-handshake/secret_handshake.go
+++ go/secret-handshake/secret_handshake.go
@@ -1,5 +1,26 @@
 package secret
 
-func Handshake(code uint) []string {
-	panic("Please implement the Handshake function")
+import (
+	"fmt"
+	"strconv"
+)
+
+var operations = []string{"wink", "double blink", "close your eyes", "jump"}
+
+func Handshake(code uint) (handshake []string) {
+	binary := strconv.FormatInt(int64(code), 2)
+	fmt.Printf("code %v in binary %v\n", code, binary)
+	for i, v := range reverse(binary) {
+		if v == '1' {
+			handshake = append(handshake, operations[i])
+		}
+	}
+	return handshake
+}
+
+func reverse(input string) (result string) {
+	for _, v := range input {
+		result = string(v) + result
+	}
+	return result
 }
