a0e8edc8337f2b822cf29bf8bafb1cd3d0c9e97e
Rootul Patel <rootulp@gmail.com>
2021-12-24 16:06:02 -0500

    Solve perfect numbers

--- go/perfect-numbers/perfect_numbers.go
+++ go/perfect-numbers/perfect_numbers.go
@@ -11,5 +11,34 @@ const ClassificationPerfect Classification = "ClassificationPerfect"
 var ErrOnlyPositive = errors.New("error only positive numbers can be classified")
 
 func Classify(n int64) (Classification, error) {
-	panic("Please implement the Classify function")
+	if n <= 0 {
+		return "", ErrOnlyPositive
+	}
+	sum := aliquotSum(n)
+
+	if sum < n {
+		return ClassificationDeficient, nil
+	} else if sum == n {
+		return ClassificationPerfect, nil
+	} else if sum > n {
+		return ClassificationAbundant, nil
+	}
+	return "", errors.New("unreachable")
+}
+
+func aliquotSum(n int64) (sum int64) {
+	factors := getFactors(n)
+	for _, factor := range factors {
+		sum += factor
+	}
+	return sum
+}
+
+func getFactors(n int64) (factors []int64) {
+	for candidate := int64(1); candidate < n; candidate++ {
+		if n%candidate == 0 {
+			factors = append(factors, candidate)
+		}
+	}
+	return factors
 }
