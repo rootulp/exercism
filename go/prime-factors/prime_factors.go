026fdb204d680e6eab1a8ba0f9e8e5da5582b675
Rootul Patel <rootulp@gmail.com>
2021-12-24 19:31:26 -0500

    Solve prime factors

--- go/prime-factors/prime_factors.go
+++ go/prime-factors/prime_factors.go
@@ -1,5 +1,18 @@
 package prime
 
-func Factors(n int64) []int64 {
-	panic("Please implement the Factors function")
+func Factors(n int64) (factors []int64) {
+	if n < 2 {
+		return []int64{}
+	}
+	current := n
+	divisor := int64(2)
+	for current != 1 {
+		if current%divisor == 0 {
+			factors = append(factors, divisor)
+			current = current / divisor
+		} else {
+			divisor += 1
+		}
+	}
+	return factors
 }
