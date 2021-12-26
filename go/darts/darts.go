274631f102a9cde8ab2f7366d6b302c2854252b8
Rootul Patel <rootulp@gmail.com>
2021-12-25 21:37:13 -0500

    Solve darts

--- go/darts/darts.go
+++ go/darts/darts.go
@@ -1,5 +1,30 @@
 package darts
 
+import "math"
+
 func Score(x, y float64) int {
-	panic("Please implement the Score function")
+	if isInnerCircle(x, y) {
+		return 10
+	} else if isMiddleCircle(x, y) {
+		return 5
+	} else if isOuterCircle(x, y) {
+		return 1
+	}
+	return 0
+}
+
+func isInnerCircle(x float64, y float64) bool {
+	return distanceToCenter(x, y) <= 1
+}
+
+func isMiddleCircle(x float64, y float64) bool {
+	return distanceToCenter(x, y) <= 5
+}
+
+func isOuterCircle(x float64, y float64) bool {
+	return distanceToCenter(x, y) <= 10
+}
+
+func distanceToCenter(x float64, y float64) float64 {
+	return math.Sqrt(math.Pow(x, 2) + math.Pow(y, 2))
 }
