d0f68779ce34471916821deca30db5e0f6822ebc
Rootul Patel <rootulp@gmail.com>
2021-12-25 21:30:00 -0500

    Solve collatz

--- go/collatz-conjecture/collatz_conjecture.go
+++ go/collatz-conjecture/collatz_conjecture.go
@@ -1,5 +1,22 @@
 package collatzconjecture
 
+import "errors"
+
 func CollatzConjecture(n int) (int, error) {
-	panic("Please implement the CollatzConjecture function")
+	if n < 1 {
+		return 0, errors.New("n must be positive")
+	}
+	return collatz(n, 0), nil
+}
+
+func collatz(n int, steps int) int {
+	if n == 1 {
+		return steps
+	}
+	steps += 1
+	if n%2 == 0 {
+		return collatz(n/2, steps)
+	} else {
+		return collatz(3*n+1, steps)
+	}
 }
