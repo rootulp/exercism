426278beb6900d94f159c3905c34262c39398364
Rootul Patel <rootulp@gmail.com>
2021-12-26 11:31:04 -0500

    Implement fold left and right

---
+++ go/list-ops/list_ops.go
@@ -0,0 +1,42 @@
+package listops
+
+// IntList is an abstraction of a list of integers which we can define methods on
+type IntList []int
+
+func (s IntList) Foldl(fn func(int, int) int, initial int) int {
+	for _, v := range s {
+		initial = fn(initial, v)
+	}
+	return initial
+}
+
+func (s IntList) Foldr(fn func(int, int) int, initial int) int {
+	for i := len(s) - 1; i >= 0; i-- {
+		initial = fn(s[i], initial)
+	}
+	return initial
+}
+
+func (s IntList) Filter(fn func(int) bool) IntList {
+	panic("Please implement the Filter function")
+}
+
+func (s IntList) Length() int {
+	panic("Please implement the Length function")
+}
+
+func (s IntList) Map(fn func(int) int) IntList {
+	panic("Please implement the Map function")
+}
+
+func (s IntList) Reverse() IntList {
+	panic("Please implement the Reverse function")
+}
+
+func (s IntList) Append(lst IntList) IntList {
+	panic("Please implement the Append function")
+}
+
+func (s IntList) Concat(lists []IntList) IntList {
+	panic("Please implement the Concat function")
+}
