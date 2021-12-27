af849315c113ae006914b2ec0d23f1aa3bb33568
Rootul Patel <rootulp@gmail.com>
2021-12-26 21:49:28 -0500

    Implement All

--- go/series/series.go
+++ go/series/series.go
@@ -1,7 +1,10 @@
 package series
 
-func All(n int, s string) []string {
-	panic("Please implement the All function")
+func All(n int, s string) (result []string) {
+	for i := 0; i+n <= len(s); i += 1 {
+		result = append(result, s[i:i+n])
+	}
+	return result
 }
 
 func UnsafeFirst(n int, s string) string {
