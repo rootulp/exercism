7b5ff23832eb336bacc1c0783927e7ce241f66c3
Rootul Patel <rootulp@gmail.com>
2022-01-03 21:48:14 -0500

    WIP: pop does not work

--- go/simple-linked-list/simple_linked_list.go
+++ go/simple-linked-list/simple_linked_list.go
@@ -1,27 +1,96 @@
 package linkedlist
 
-// Define the List and Element types here.
+import (
+	"errors"
+	"fmt"
+)
 
-func New([]int) *List {
-	panic("Please implement the New function")
+// Define the List and Element types here.
+type List struct {
+	head *Element
+	size int
+}
+
+type Element struct {
+	data int
+	next *Element
+}
+
+func (l *List) String() string {
+	return fmt.Sprintf("(size: %d, head: %v)", l.size, l.head)
+}
+
+func (e *Element) String() string {
+	return fmt.Sprintf("(data: %d, next: %v)", e.data, e.next)
+}
+
+func New(array []int) (result *List) {
+	result = &List{}
+	if array == nil {
+		return result
+	}
+	for _, v := range array {
+		result.Push(v)
+	}
+	return result
 }
 
 func (l *List) Size() int {
-	panic("Please implement the Size function")
+	return l.size
 }
 
-func (l *List) Push(element int) {
-	panic("Please implement the Push function")
+func (l *List) Push(data int) {
+	element := &Element{
+		data: data,
+	}
+	if l.head == nil {
+		l.head = element
+	} else {
+		current := l.head
+		for current.next != nil {
+			current = current.next
+		}
+		current.next = element
+	}
+	l.size += 1
 }
 
 func (l *List) Pop() (int, error) {
-	panic("Please implement the Pop function")
+	if l.head == nil {
+		return 0, errors.New("attempted to pop from empty list")
+	}
+	current := l.head
+	for current.next != nil && current.next.next != nil {
+		current = current.next
+	}
+	fmt.Printf("l %v and current %v\n", l, current)
+	result := current.next.data
+	current.next = nil
+	l.size -= 1
+	return result, nil
 }
 
-func (l *List) Array() []int {
-	panic("Please implement the Array function")
+func (l *List) Array() (result []int) {
+	if l.head == nil {
+		return []int{}
+	}
+	current := l.head
+	for current.next != nil {
+		result = append(result, current.data)
+		current = current.next
+	}
+	result = append(result, current.data)
+	return result
 }
 
 func (l *List) Reverse() *List {
-	panic("Please implement the Reverse function")
+	slice := l.Array()
+	return New(reverse(slice))
+}
+
+func reverse(slice []int) (reversed []int) {
+	for _, v := range slice {
+		reversed = append([]int{v}, reversed...)
+	}
+	return reversed
 }
