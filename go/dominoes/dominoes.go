29edc9b355e7f144966919c32c0ee4b36470b802
Rootul Patel <rootulp@gmail.com>
2022-01-05 17:37:44 -0500

    Pass empty input

--- go/dominoes/dominoes.go
+++ go/dominoes/dominoes.go
@@ -1,7 +1,10 @@
 package dominoes
 
-// Define the Domino type here.
+type Domino [2]int
 
 func MakeChain(input []Domino) ([]Domino, bool) {
-	panic("Please implement the MakeChain function")
+	if len(input) == 0 {
+		return input, true
+	}
+	return nil, false
 }
