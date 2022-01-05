b68a296c588771f9ff032d23f708a147673aa55b
Rootul Patel <rootulp@gmail.com>
2022-01-04 19:51:48 -0500

    Solve custom set

--- go/custom-set/custom_set.go
+++ go/custom-set/custom_set.go
@@ -1,5 +1,11 @@
 package stringset
 
+import (
+	"fmt"
+	"reflect"
+	"strings"
+)
+
 // Implement Set as a collection of unique string values.
 //
 // For Set.String, use '{' and '}', output elements as double-quoted strings
@@ -8,51 +14,112 @@ package stringset
 // Format the empty set as {}.
 
 // Define the Set type here.
-
-func New() Set {
-	panic("Please implement the New function")
+type Set struct {
+	set map[string]bool
 }
 
-func NewFromSlice(l []string) Set {
-	panic("Please implement the NewFromSlice function")
+func New() Set {
+	return Set{map[string]bool{}}
+}
+
+func NewFromSlice(slice []string) (result Set) {
+	result = Set{map[string]bool{}}
+	for _, element := range slice {
+		result.Add(element)
+	}
+	return result
 }
 
 func (s Set) String() string {
-	panic("Please implement the String function")
+	if s.set == nil {
+		return "{}"
+	}
+	return fmt.Sprintf("{%s}", strings.Join(s.formattedKeys(), ", "))
 }
 
 func (s Set) IsEmpty() bool {
-	panic("Please implement the IsEmpty function")
+	return len(s.set) == 0
 }
 
 func (s Set) Has(elem string) bool {
-	panic("Please implement the Has function")
+	_, ok := s.set[elem]
+	return ok
 }
 
 func (s Set) Add(elem string) {
-	panic("Please implement the Add function")
+	s.set[elem] = true
 }
 
 func Subset(s1, s2 Set) bool {
-	panic("Please implement the Subset function")
+	for key, val := range s1.set {
+		if val && !s2.Has(key) {
+			return false
+		}
+	}
+	return true
 }
 
 func Disjoint(s1, s2 Set) bool {
-	panic("Please implement the Disjoint function")
+	for key, val := range s1.set {
+		if val && s2.Has(key) {
+			return false
+		}
+	}
+	for key, val := range s2.set {
+		if val && s1.Has(key) {
+			return false
+		}
+	}
+	return true
 }
 
 func Equal(s1, s2 Set) bool {
-	panic("Please implement the Equal function")
+	return reflect.DeepEqual(s1.set, s2.set)
 }
 
-func Intersection(s1, s2 Set) Set {
-	panic("Please implement the Intersection function")
+func Intersection(s1, s2 Set) (result Set) {
+	result = New()
+	for key, val := range s1.set {
+		if val && s2.Has(key) {
+			result.Add(key)
+		}
+	}
+	return result
 }
 
-func Difference(s1, s2 Set) Set {
-	panic("Please implement the Difference function")
+func Difference(s1, s2 Set) (result Set) {
+	result = New()
+	for key, val := range s1.set {
+		if val && !s2.Has(key) {
+			result.Add(key)
+		}
+	}
+	return result
 }
 
-func Union(s1, s2 Set) Set {
-	panic("Please implement the Union function")
+func Union(s1, s2 Set) (result Set) {
+	result = New()
+	for key := range s1.set {
+		result.Add(key)
+	}
+	for key := range s2.set {
+		result.Add(key)
+	}
+	return result
+}
+
+func (s Set) formattedKeys() (formatted []string) {
+	formatted = []string{}
+	for _, key := range s.keys() {
+		quotted := fmt.Sprintf("\"%s\"", key)
+		formatted = append(formatted, quotted)
+	}
+	return formatted
+}
+
+func (s Set) keys() (keys []string) {
+	for key := range s.set {
+		keys = append(keys, key)
+	}
+	return keys
 }
