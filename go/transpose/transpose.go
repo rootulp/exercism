e7a6fcaeeee88d065f45d7ac8789c23a85ae5e21
Rootul Patel <rootulp@gmail.com>
2022-01-06 22:07:44 -0500

    Pass first few tests

--- go/transpose/transpose.go
+++ go/transpose/transpose.go
@@ -1,5 +1,28 @@
 package transpose
 
-func Transpose(input []string) []string {
-	panic("Please implement the Transpose function")
+import (
+	"fmt"
+	"strings"
+)
+
+func Transpose(input []string) (result []string) {
+	if len(input) == 0 {
+		return []string{}
+	}
+	transposed := make([][]string, len(input[0]))
+	for _, v := range input {
+		for col, c := range v {
+			if transposed[col] == nil {
+				transposed[col] = []string{}
+			}
+			fmt.Printf("transposed[%v]=append(%v, %v)\n", col, transposed[col], c)
+			transposed[col] = append(transposed[col], string(c))
+		}
+	}
+	fmt.Printf("transposed: %v\n", transposed)
+	for _, row := range transposed {
+		result = append(result, strings.Join(row, ""))
+	}
+	fmt.Printf("Transpose(%v)=%v\n", input, result)
+	return result
 }
