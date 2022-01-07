2aab5ddc49402590fd30e9f101d5c41f877e276d
Rootul Patel <rootulp@gmail.com>
2022-01-06 22:37:45 -0500

    Solve transpose by padding input

--- go/transpose/transpose.go
+++ go/transpose/transpose.go
@@ -9,8 +9,9 @@ func Transpose(input []string) (result []string) {
 	if len(input) == 0 {
 		return []string{}
 	}
-	transposed := make([][]string, len(input[0]))
-	for _, v := range input {
+	transposed := make([][]string, maxLineLength(input))
+	paddedInput := padInput(input)
+	for _, v := range paddedInput {
 		for col, c := range v {
 			if transposed[col] == nil {
 				transposed[col] = []string{}
@@ -25,4 +26,31 @@ func Transpose(input []string) (result []string) {
 	}
 	fmt.Printf("Transpose(%v)=%v\n", input, result)
 	return result
+}
+
+func padInput(input []string) (padded []string) {
+	for i, line := range input {
+		lineLength := maxLineLength(input[i:])
+		paddedLine := pad(line, lineLength)
+		padded = append(padded, paddedLine)
+	}
+	fmt.Printf("pad(%v)=%v", input, padded)
+	return padded
+}
+
+// pad right pads line with whitespace to a length of lineLength
+func pad(line string, lineLength int) (padded string) {
+	if len(line) >= lineLength {
+		return line
+	}
+	return line + strings.Repeat(" ", lineLength-len(line))
+}
+
+func maxLineLength(input []string) (max int) {
+	for _, line := range input {
+		if len(line) > max {
+			max = len(line)
+		}
+	}
+	return max
 }
