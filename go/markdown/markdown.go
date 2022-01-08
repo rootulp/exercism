5bc0119d40c29b3ef41aba1b30b029b8389c700f
Rootul Patel <rootulp@gmail.com>
2022-01-07 20:32:19 -0500

    Throw out old impl

--- go/markdown/markdown.go
+++ go/markdown/markdown.go
@@ -1,62 +1,18 @@
 package markdown
 
 import (
-	"fmt"
 	"strings"
 )
 
 // Render translates markdown to HTML
 func Render(markdown string) (html string) {
-	header := 0
-	temp := handleItalic(handleBold(markdown))
-	pos := 0
-	list := 0
-	html = ""
-	for {
-		char := temp[pos]
-		if char == '#' {
-			for char == '#' {
-				header++
-				pos++
-				char = temp[pos]
-			}
-			html += fmt.Sprintf("<h%d>", header)
-			pos++
-			continue
-		}
-		if char == '*' {
-			if list == 0 {
-				html += "<ul>"
-			}
-			html += "<li>"
-			list++
-			pos += 2
-			continue
-		}
-		if char == '\n' {
-			if list > 0 {
-				html += "</li>"
-			}
-			if header > 0 {
-				html += fmt.Sprintf("</h%d>", header)
-				header = 0
-			}
-			pos++
-			continue
-		}
-		html += string(char)
-		pos++
-		if pos >= len(temp) {
-			break
-		}
-	}
-	if header > 0 {
-		return html + fmt.Sprintf("</h%d>", header)
-	}
-	if list > 0 {
-		return html + "</li></ul>"
-	}
-	return "<p>" + html + "</p>"
+	markdown = handleBold(markdown)
+	markdown = handleItalic(markdown)
+	return wrapParagraph(markdown)
+}
+
+func wrapParagraph(s string) string {
+	return "<p>" + s + "</p>"
 }
 
 func handleBold(markdown string) string {
