93149d79a982217c90c404ab54f77f20bd67c493
Rootul Patel <rootulp@gmail.com>
2022-01-07 20:48:05 -0500

    Handle headers

--- go/markdown/markdown.go
+++ go/markdown/markdown.go
@@ -1,6 +1,7 @@
 package markdown
 
 import (
+	"fmt"
 	"strings"
 )
 
@@ -8,6 +9,9 @@ import (
 func Render(markdown string) (html string) {
 	markdown = handleBold(markdown)
 	markdown = handleItalic(markdown)
+	if isHeader(markdown) {
+		return handleHeader(markdown)
+	}
 	return wrapParagraph(markdown)
 }
 
@@ -21,8 +25,38 @@ func handleBold(markdown string) string {
 	return markdown
 }
 
-func handleItalic(markdown string) (result string) {
+func handleItalic(markdown string) string {
 	markdown = strings.Replace(markdown, "_", "<em>", 1)
 	markdown = strings.Replace(markdown, "_", "</em>", 1)
 	return markdown
+}
+
+func isHeader(line string) bool {
+	return strings.HasPrefix(line, "#")
+}
+
+func handleHeader(line string) string {
+	if strings.HasPrefix(line, "######") {
+		return handleHeaderWithLevel(line, 6)
+	}
+	if strings.HasPrefix(line, "#####") {
+		return handleHeaderWithLevel(line, 5)
+	}
+	if strings.HasPrefix(line, "####") {
+		return handleHeaderWithLevel(line, 4)
+	}
+	if strings.HasPrefix(line, "###") {
+		return handleHeaderWithLevel(line, 3)
+	}
+	if strings.HasPrefix(line, "##") {
+		return handleHeaderWithLevel(line, 2)
+	}
+	if strings.HasPrefix(line, "#") {
+		return handleHeaderWithLevel(line, 1)
+	}
+	panic("handleHeader called with a line that doesn't contain a header")
+}
+
+func handleHeaderWithLevel(markdown string, level int) string {
+	return fmt.Sprintf("<h%d>%s</h%d>", level, markdown[level+1:], level)
 }
