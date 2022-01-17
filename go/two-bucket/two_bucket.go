7815a2ec33849149ee9e5325e3524707e5744d56
Rootul Patel <rootulp@gmail.com>
2022-01-17 16:56:24 -0500

    WIP: observing nil pointer dereference

--- go/two-bucket/two_bucket.go
+++ go/two-bucket/two_bucket.go
@@ -2,70 +2,76 @@ package twobucket
 
 import "fmt"
 
-func Solve(sizeBucketOne, sizeBucketTwo, goalAmount int, startBucket string) (string, int, int, error) {
-	tb := TwoBucket{
-		sizeBucketOne: sizeBucketOne,
-		sizeBucketTwo: sizeBucketTwo,
-		goalAmount:    goalAmount,
-		startBucket:   startBucket,
-		moves:         0,
-		bucketOne:     0,
-		bucketTwo:     0,
-	}
-	return tb.Solve()
+type Bucket struct {
+	name     string
+	capacity int
+	level    int
 }
 
-type TwoBucket struct {
-	sizeBucketOne int
-	sizeBucketTwo int
-	goalAmount    int
-	startBucket   string
-
-	// state
-	moves     int
-	bucketOne int
-	bucketTwo int
+func NewBucket(name string, capacity int, level int) *Bucket {
+	return &Bucket{name, capacity, level}
 }
-
-func (tb TwoBucket) Solve() (goalBucket string, moves int, otherBucket int, e error) {
-	if tb.bucketOne == tb.goalAmount {
-		return "one", tb.moves, tb.bucketTwo, nil
-	} else if tb.bucketTwo == tb.goalAmount {
-		return "two", tb.moves, tb.bucketOne, nil
-	}
-
-	if tb.moves == 0 {
-		tb.fillBucket(tb.startBucket)
-		return tb.Solve()
-	}
-
-	tb1 := tb.Clone()
-
+func (b *Bucket) Clone(level int) *Bucket {
+	return NewBucket(b.name, b.capacity, level)
 }
-
-func (tb TwoBucket) fillBucket(bucketToFill string) {
-	if bucketToFill == "one" {
-		tb.bucketOne = tb.sizeBucketOne
-		tb.moves += 1
-	} else if bucketToFill == "two" {
-		tb.bucketTwo = tb.sizeBucketTwo
-		tb.moves += 1
+func (b *Bucket) Fill() *Bucket {
+	return b.Clone(b.capacity)
+}
+func (b *Bucket) Empty() *Bucket {
+	return b.Clone(0)
+}
+func (b *Bucket) Transfer(destination *Bucket) (src *Bucket, dst *Bucket) {
+	if b.level+destination.level > destination.capacity {
+		return src.Clone(destination.capacity - destination.level), destination.Clone(destination.capacity)
 	} else {
-		panic(fmt.Sprintf("invalid bucketToFill %s", bucketToFill))
+		return src.Clone(0), destination.Clone(destination.level + src.level)
 	}
 }
 
-func (tb TwoBucket) Clone() (cloned TwoBucket) {
-	cloned = TwoBucket{
-		sizeBucketOne: tb.sizeBucketOne,
-		sizeBucketTwo: tb.sizeBucketTwo,
-		goalAmount:    tb.goalAmount,
-		startBucket:   tb.startBucket,
+type state struct {
+	a     *Bucket
+	b     *Bucket
+	moves int
+}
 
-		// state
-		moves:     tb.moves,
-		bucketOne: tb.bucketOne,
-		bucketTwo: tb.bucketTwo,
+func Solve(bucketOneCapacity, bucketTwoCapacity, goalAmount int, startBucket string) (goalBucket string, moves int, otherBucketLevel int, e error) {
+
+	one := NewBucket("one", bucketOneCapacity, 0)
+	two := NewBucket("two", bucketTwoCapacity, 0)
+	moves = 0
+	queue := []state{}
+
+	switch startBucket {
+	case "one":
+		one.Fill()
+		moves += 1
+		queue = append(queue, state{one, two, moves})
+	case "two":
+		two.Fill()
+		moves += 1
+		queue = append(queue, state{one, two, moves})
+	default:
+		return "", -1, -1, fmt.Errorf("start bucket invalid %s", startBucket)
 	}
-	return cloned
+
+	for len(queue) > 0 {
+		item, queue := queue[0], queue[1:]
+		if item.a.level == goalAmount {
+			return "one", item.moves, item.b.level, nil
+		} else if item.b.level == goalAmount {
+			return "two", item.moves, item.a.level, nil
+		}
+
+		// Execute possible moves
+		queue = append(queue, state{item.a.Fill(), item.b, moves + 1})
+		queue = append(queue, state{item.a, item.b.Fill(), moves + 1})
+		queue = append(queue, state{item.a.Empty(), item.b, moves + 1})
+		queue = append(queue, state{item.a, item.b.Empty(), moves + 1})
+		a, b := item.a.Transfer(item.b)
+		queue = append(queue, state{a, b, moves + 1})
+		a, b = item.b.Transfer(item.a)
+		queue = append(queue, state{a, b, moves + 1})
+	}
+
+	return "", -1, -1, fmt.Errorf("could not find a solution")
 }
