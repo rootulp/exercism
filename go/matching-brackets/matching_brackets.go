ee7f224606596850f14ebdbca66222fb446114f4
Rootul Patel <rootulp@gmail.com>
2022-01-28 21:22:23 -0500

    Solve matching brackets

--- go/matching-brackets/matching_brackets.go
+++ go/matching-brackets/matching_brackets.go
@@ -1,5 +1,62 @@
 package brackets
 
+var openBracketToCloseBracket = map[rune]rune{
+	'[': ']',
+	'{': '}',
+	'(': ')',
+}
+
 func Bracket(input string) bool {
-	panic("Please implement the Bracket function")
+	stack := Stack{}
+	for _, char := range input {
+		if isOpenBracket(char) {
+			stack.Push(char)
+		} else if isCloseBracket(char) {
+			if stack.Len() > 0 && isMatching(stack.Peek(), char) {
+				stack.Pop()
+			} else {
+				return false
+			}
+		} else {
+			continue
+		}
+	}
+	return stack.Len() == 0
+}
+
+func isOpenBracket(r rune) bool {
+	_, ok := openBracketToCloseBracket[r]
+	return ok
+}
+
+func isCloseBracket(r rune) bool {
+	for _, v := range openBracketToCloseBracket {
+		if r == v {
+			return true
+		}
+	}
+	return false
+}
+
+func isMatching(open rune, close rune) bool {
+	return openBracketToCloseBracket[open] == close
+}
+
+type Stack struct {
+	slice []rune
+}
+
+func (s *Stack) Len() int {
+	return len(s.slice)
+}
+func (s *Stack) Push(r rune) {
+	s.slice = append(s.slice, r)
+}
+func (s *Stack) Peek() rune {
+	return s.slice[len(s.slice)-1]
+}
+func (s *Stack) Pop() rune {
+	result := s.Peek()
+	s.slice = s.slice[:len(s.slice)-1]
+	return result
 }
