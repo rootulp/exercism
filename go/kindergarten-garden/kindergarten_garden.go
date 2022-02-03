b4f66b453736821bcfe6d8f312913ccd738be836
Rootul Patel <rootulp@gmail.com>
2022-02-02 23:45:44 -0500

    Pass first 5 gardens

--- go/kindergarten-garden/kindergarten_garden.go
+++ go/kindergarten-garden/kindergarten_garden.go
@@ -1,6 +1,22 @@
 package kindergarten
 
+import (
+	"fmt"
+	"strings"
+)
+
 // Define the Garden type here.
+type Garden struct {
+	diagram  []string
+	children []string
+}
+
+var symbolToPlant = map[string]string{
+	"R": "radishes",
+	"C": "clover",
+	"G": "grass",
+	"V": "violets",
+}
 
 // The diagram argument starts each row with a '\n'.  This allows Go's
 // raw string literals to present diagrams in source code nicely as two
@@ -11,9 +27,32 @@ package kindergarten
 //     VVCCGG`
 
 func NewGarden(diagram string, children []string) (*Garden, error) {
-	panic("Please implement the NewGarden function")
+	trimmed := strings.Trim(diagram, "\n")
+	rows := strings.Split(trimmed, "\n")
+	return &Garden{diagram: rows, children: children}, nil
 }
 
-func (g *Garden) Plants(child string) ([]string, bool) {
-	panic("Please implement the Plants function")
+func (g *Garden) Plants(child string) (plants []string, ok bool) {
+	fmt.Printf("diagrams %v\n", g.diagram)
+	index := indexOf(g.children, child)
+	column := index * 2
+	cups := []string{
+		string(g.diagram[0][column]),
+		string(g.diagram[0][column+1]),
+		string(g.diagram[1][column]),
+		string(g.diagram[1][column+1]),
+	}
+	for _, cup := range cups {
+		plants = append(plants, symbolToPlant[cup])
+	}
+	return plants, true
+}
+
+func indexOf(slice []string, element string) int {
+	for i, v := range slice {
+		if v == element {
+			return i
+		}
+	}
+	return -1
 }
