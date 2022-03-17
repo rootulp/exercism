af4d700d2e9956dccff979da48e26c4fc733d476
Rootul Patel <rootulp@gmail.com>
2022-03-16 23:30:14 -0400

    Solve all but one test case

---
+++ typescript/dnd-character/dnd-character.ts
@@ -0,0 +1,35 @@
+export class DnDCharacter {
+  public static generateAbilityScore(): number {
+    return DnDCharacter.randomInt(3, 18)
+  }
+
+  public static getModifierFor(abilityValue: number): number {
+    return Math.floor((abilityValue - 10) / 2)
+  }
+
+  public strength: number;
+  public dexterity: number;
+  public constitution: number;
+  public intelligence: number;
+  public wisdom: number;
+  public charisma: number;
+  public hitpoints: number;
+
+  public constructor() {
+    this.strength = DnDCharacter.generateAbilityScore()
+    this.dexterity = DnDCharacter.generateAbilityScore()
+    this.constitution = DnDCharacter.generateAbilityScore()
+    this.intelligence = DnDCharacter.generateAbilityScore()
+    this.wisdom = DnDCharacter.generateAbilityScore()
+    this.charisma = DnDCharacter.generateAbilityScore()
+    this.hitpoints = 10 + DnDCharacter.getModifierFor(this.charisma)
+  }
+
+
+  /**
+   * randomNumber returns a random number between min and max (inclusive)
+   */
+  private static randomInt(min: number, max: number) {
+    return Math.floor((Math.random()) * (max - min + 1)) + min;
+  }
+}
