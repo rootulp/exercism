09606e745e869f60dda9ec236e306dcbfc1bbc18
Rootul Patel <rootulp@gmail.com>
2022-03-16 23:08:22 -0400

    Pass first test

--- typescript/resistor-color-trio/resistor-color-trio.ts
+++ typescript/resistor-color-trio/resistor-color-trio.ts
@@ -1,3 +1,30 @@
-export function decodedResistorValue() {
-  throw new Error('Remove this statement and implement this function')
+
+const bandToNumber: {[key: string]: number} = {
+  'black': 0,
+  'brown': 1,
+  'red': 2,
+  'orange': 3,
+  'yellow': 4,
+  'green': 5,
+  'blue': 6,
+  'violet': 7,
+  'grey': 8,
+  'white': 9,
+}
+
+export function decodedResistorValue(bands: string[]): string {
+  const number = decodeResistorDuo(bands.slice(0, 2))
+  console.log(number)
+  const result = number * 10 ** decode(bands[2])
+  return `${result} ohms`
+}
+
+function decodeResistorDuo(bands: string[]): number {
+  const band1 = decode(bands[0])
+  const band2 = decode(bands[1])
+  return parseInt(`${band1}${band2}`)
+}
+
+function decode(band: string): number {
+  return bandToNumber[band]
 }
