9155e37df6ba824271337389213f545ff127338f
Rootul Patel <rootulp@gmail.com>
2022-03-20 13:09:06 -0400

    Polish solution

--- typescript/binary-search/binary-search.ts
+++ typescript/binary-search/binary-search.ts
@@ -1,31 +1,22 @@
 /**
  * Find performs a binary search for `item` in `array` and returns its index if
- * found. Array must be provided in sorted order. `find` throws an Error if
- * value if nost in the array.
+ * found. `find` throws an Error if value if item is not in the array. `array`
+ * must be provided in sorted order.
  */
 export function find(array: number[], item: number): number {
-  if (array.length === 0) {
-    throw new Error('Value not in array')
-  }
-
   return binarySearch(array, item, 0, array.length - 1)
 }
 
 function binarySearch(array: number[], item: number, left: number, right: number): number {
   if(right < left) {
-    console.log(`right: ${right}, left: ${left}`)
     throw new Error('Value not in array')
   }
-  if (right == left && array[right] !== item) {
+  if (right === left && array[right] !== item) {
     throw new Error('Value not in array')
-  }
-  if (right == left && array[right] === item) {
-    return right
   }
 
   const middle = Math.floor((right - left) / 2) + left
   const middleVal = array[middle]
-  console.log(`right: ${right}, left: ${left}, middle: ${middle}, middleVal ${middleVal}`)
   if (item === middleVal) {
     return middle
   }
