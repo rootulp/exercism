01160aa456e93938ec5e9e6a39d2faf066fa1f43
Rootul Patel <rootulp@gmail.com>
2022-03-24 17:01:07 -0400

    Solve rail fence cipher

--- go/rail-fence-cipher/rail_fence_cipher.go
+++ go/rail-fence-cipher/rail_fence_cipher.go
@@ -1,11 +1,22 @@
 package railfence
 
-import "fmt"
+import (
+	"fmt"
+	"unicode/utf8"
+)
 
 const Down = "down"
 const Up = "up"
 
 func Encode(message string, numRails int) (encoded string) {
+	rails := encodeRails(message, numRails)
+	for _, rail := range rails {
+		encoded += rail
+	}
+	return encoded
+}
+
+func encodeRails(message string, numRails int) []string {
 	rails := initRails(numRails)
 	railIndex := 0
 	direction := Down
@@ -15,15 +26,47 @@ func Encode(message string, numRails int) (encoded string) {
 		direction, railIndex = advance(direction, railIndex, numRails)
 	}
 
-	fmt.Printf("rails: %v\n", rails)
-	for _, rail := range rails {
-		encoded += rail
-	}
-	return encoded
+	fmt.Printf("encodeRails: %v\n", rails)
+	return rails
 }
 
-func Decode(message string, rails int) (decoded string) {
-	return ""
+func Decode(message string, numRails int) (decoded string) {
+	rails := splitMessageIntoRails(message, numRails)
+	railIndex := 0
+	direction := Down
+
+	for rails[railIndex] != "" {
+		r, trimmed := trimFirstRune(rails[railIndex])
+		decoded += string(r)
+		rails[railIndex] = trimmed
+		direction, railIndex = advance(direction, railIndex, numRails)
+	}
+
+	return decoded
+}
+
+func trimFirstRune(s string) (rune, string) {
+	r, i := utf8.DecodeRuneInString(s)
+	return r, s[i:]
+}
+
+func splitMessageIntoRails(message string, numRails int) []string {
+	copy := message
+	rails := initRails(numRails)
+
+	for i := range rails {
+		length := lengthOfRail(message, numRails, i)
+		rails[i] = copy[:length]
+		copy = copy[length:]
+		// fmt.Printf("index %v, rail %v, length %v, copy %v\n", i, rails[i], length, copy)
+	}
+
+	return rails
+}
+
+func lengthOfRail(message string, numRails int, index int) int {
+	rails := encodeRails(message, numRails)
+	return len(rails[index])
 }
 
 func initRails(numRails int) (rails []string) {
