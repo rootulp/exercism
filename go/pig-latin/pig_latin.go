9d3dfdc60e4dbff7ab63d60a306a3e382983f15f
Rootul Patel <rootulp@gmail.com>
2022-04-01 21:04:37 -0400

    Handle all consonant sounds

--- go/pig-latin/pig_latin.go
+++ go/pig-latin/pig_latin.go
@@ -5,43 +5,49 @@ import (
 	"strings"
 )
 
-var consonants = []string{"p", "k"}
+var vowels = []string{"a", "e", "i", "o", "u"}
+var consonantSounds = []string{"squ", "thr", "th", "sch", "qu", "ch", "rh", "p", "k", "x", "q", "y", "m"}
 
 func Sentence(sentence string) string {
 	if startsWithVowel(sentence) ||
 		strings.HasPrefix(sentence, "xr") ||
 		strings.HasPrefix(sentence, "yt") {
-		return sentence + "ay"
+		return handleVowel(sentence)
 	}
-	if startsWithConsonant(sentence) {
-		return handleConsonant(sentence)
+	if startsWithConsonantSound(sentence) {
+		return handleConsonantSound(sentence)
 	}
 
 	return sentence
 }
 
 func startsWithVowel(sentence string) bool {
-	return strings.HasPrefix(sentence, "a") ||
-		strings.HasPrefix(sentence, "e") ||
-		strings.HasPrefix(sentence, "i") ||
-		strings.HasPrefix(sentence, "o") ||
-		strings.HasPrefix(sentence, "u")
-}
-
-func startsWithConsonant(sentence string) bool {
-	for _, consonant := range consonants {
-		if strings.HasPrefix(sentence, consonant) {
+	for _, vowel := range vowels {
+		if strings.HasPrefix(sentence, vowel) {
 			return true
 		}
 	}
 	return false
 }
 
-func handleConsonant(sentence string) string {
-	for _, consonant := range consonants {
-		if strings.HasPrefix(sentence, consonant) {
-			return strings.TrimPrefix(sentence, consonant) + consonant + "ay"
+func startsWithConsonantSound(sentence string) bool {
+	for _, cs := range consonantSounds {
+		if strings.HasPrefix(sentence, cs) {
+			return true
 		}
 	}
-	panic(fmt.Sprintf("could not find consonant prefix for sentence %v", sentence))
+	return false
+}
+
+func handleConsonantSound(sentence string) string {
+	for _, cs := range consonantSounds {
+		if strings.HasPrefix(sentence, cs) {
+			return strings.TrimPrefix(sentence, cs) + cs + "ay"
+		}
+	}
+	panic(fmt.Sprintf("could not find consonant sound prefix for sentence %v", sentence))
+}
+
+func handleVowel(sentence string) string {
+	return sentence + "ay"
 }
