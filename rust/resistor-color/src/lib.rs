9ca64c77ae606917915362f7507bc1f1b1d08812
Rootul Patel <rootulp@gmail.com>
2022-05-10 12:11:21 -0400

    Pass value_to_color_string

--- rust/resistor-color/src/lib.rs
+++ rust/resistor-color/src/lib.rs
@@ -1,7 +1,9 @@
+use std::default;
+
 use enum_iterator::IntoEnumIterator;
 use int_enum::IntEnum;
 
-#[repr(u8)]
+#[repr(usize)]
 #[derive(Clone, Copy, Debug, IntoEnumIterator, IntEnum, PartialEq)]
 pub enum ResistorColor {
     Black = 0,
@@ -17,16 +19,34 @@ pub enum ResistorColor {
 }
 
 pub fn color_to_value(_color: ResistorColor) -> usize {
-    return _color.int_value().into()
+    return _color.int_value()
 }
 
 pub fn value_to_color_string(value: usize) -> String {
-    unimplemented!(
-        "convert the value {} into a string representation of color",
-        value
-    )
+    for color in ResistorColor::into_enum_iter() {
+        if value == color.int_value() {
+            return label(color)
+        }
+    }
+
+    return "value out of range".to_string()
 }
 
 pub fn colors() -> Vec<ResistorColor> {
     unimplemented!("return a list of all the colors ordered by resistance")
+}
+
+fn label(color: ResistorColor) -> String {
+    match color {
+        ResistorColor::Black => "Black".to_string(),
+        ResistorColor::Brown => "Brown".to_string(),
+        ResistorColor::Red => "Red".to_string(),
+        ResistorColor::Orange => "Orange".to_string(),
+        ResistorColor::Yellow => "Yellow".to_string(),
+        ResistorColor::Green => "Green".to_string(),
+        ResistorColor::Blue => "Blue".to_string(),
+        ResistorColor::Violet => "Violet".to_string(),
+        ResistorColor::Grey => "Grey".to_string(),
+        ResistorColor::White => "White".to_string(),
+    }
 }
--- rust/resistor-color/tests/resistor-color.rs
+++ rust/resistor-color/tests/resistor-color.rs
@@ -16,25 +16,21 @@ fn test_white() {
 }
 
 #[test]
-#[ignore]
 fn test_2() {
     assert_eq!(value_to_color_string(2), String::from("Red"));
 }
 
 #[test]
-#[ignore]
 fn test_6() {
     assert_eq!(value_to_color_string(6), String::from("Blue"));
 }
 
 #[test]
-#[ignore]
 fn test_8() {
     assert_eq!(value_to_color_string(8), String::from("Grey"));
 }
 
 #[test]
-#[ignore]
 fn test_11_out_of_range() {
     assert_eq!(
         value_to_color_string(11),
