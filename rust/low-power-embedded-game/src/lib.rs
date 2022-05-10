5209caf5983fe55f8202ecb337ee7a23c2d88b2f
Rootul Patel <rootulp@gmail.com>
2022-05-10 14:31:30 -0400

    Implement iterator

--- rust/low-power-embedded-game/src/lib.rs
+++ rust/low-power-embedded-game/src/lib.rs
@@ -1,16 +1,12 @@
-// This stub file contains items that aren't used yet; feel free to remove this module attribute
-// to enable stricter warnings.
-#![allow(unused)]
-
 pub fn divmod(dividend: i16, divisor: i16) -> (i16, i16) {
     return (dividend / divisor, dividend % divisor);
 }
 
 pub fn evens<T>(iter: impl Iterator<Item = T>) -> impl Iterator<Item = T> {
-    unimplemented!("implement `fn evens`");
-    // TODO: remove this; it's only necessary to allow this function to compile
-    // before the student has done any work.
-    std::iter::empty()
+    return iter
+        .enumerate()
+        .filter(|(index, _)| index % 2 == 0)
+        .map(|(_, val)| val);
 }
 
 pub struct Position(pub i16, pub i16);
--- rust/low-power-embedded-game/tests/low-power-embedded-game.rs
+++ rust/low-power-embedded-game/tests/low-power-embedded-game.rs
@@ -11,25 +11,21 @@ mod divmod {
     }
 
     #[test]
-    #[ignore]
     fn powerup() {
         assert_eq!(divmod(100, 3), (33, 1));
     }
 
     #[test]
-    #[ignore]
     fn less() {
         assert_eq!(divmod(3, 10), (0, 3));
     }
 
     #[test]
-    #[ignore]
     fn eq() {
         assert_eq!(divmod(3, 3), (1, 0));
     }
 
     #[test]
-    #[ignore]
     fn multiple() {
         assert_eq!(divmod(9, 3), (3, 0));
     }
@@ -39,28 +35,24 @@ mod evens {
     use low_power_embedded_game::evens;
 
     #[test]
-    #[ignore]
     fn simple_i32() {
         let out: Vec<i32> = evens(0..).take(5).collect();
         assert_eq!(out, &[0, 2, 4, 6, 8]);
     }
 
     #[test]
-    #[ignore]
     fn reverse_i32() {
         let out: Vec<i32> = evens((0..=10).rev()).collect();
         assert_eq!(out, &[10, 8, 6, 4, 2, 0]);
     }
 
     #[test]
-    #[ignore]
     fn offset_i32() {
         let out: Vec<i32> = evens(1..).take(5).collect();
         assert_eq!(out, &[1, 3, 5, 7, 9]);
     }
 
     #[test]
-    #[ignore]
     fn strs() {
         let input = "You really must never be above joking.".split_whitespace();
         let expected: Vec<_> = "You must be joking.".split_whitespace().collect();
