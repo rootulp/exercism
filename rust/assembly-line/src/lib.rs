d628ef72e2126b905181762a78239bb9ec8243dd
Rootul Patel <rootulp@gmail.com>
2022-05-10 10:18:20 -0400

    Pass all tests

--- rust/assembly-line/src/lib.rs
+++ rust/assembly-line/src/lib.rs
@@ -23,5 +23,5 @@ pub fn success_rate(speed: u8) -> f64 {
 }
 
 pub fn working_items_per_minute(speed: u8) -> u32 {
-    unimplemented!("calculate the amount of working items at speed: {}", speed)
+    return production_rate_per_hour(speed) as u32 / 60;
 }
