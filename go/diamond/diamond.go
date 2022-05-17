0e83675cc3bd413cd9e16bd9e1863207cdbfd77b
Rootul Patel <rootulp@gmail.com>
2022-05-17 11:58:20 -0400

    Handle error

--- go/diamond/diamond.go
+++ go/diamond/diamond.go
@@ -1,5 +1,10 @@
 package diamond
 
+import "fmt"
+
 func Gen(char byte) (string, error) {
-	panic("Please implement the Gen function")
+	if char < 'A' || char > 'Z' {
+		return "", fmt.Errorf("char %v is not a valid capital letter", char)
+	}
+	return "A", nil
 }
