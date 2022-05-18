9196ea9f67806c9b2e3452266c937693975bb16b
Rootul Patel <rootulp@gmail.com>
2022-05-18 13:28:28 -0400

    Solve pascals triangle

--- go/pascals-triangle/pascals_triangle.go
+++ go/pascals-triangle/pascals_triangle.go
@@ -1,5 +1,21 @@
 package pascal
 
 func Triangle(n int) [][]int {
-	panic("Please implement the Triangle function")
+	if n == 1 {
+		return [][]int{{1}}
+	}
+
+	previousRows := Triangle(n - 1)
+	lastRow := previousRows[len(previousRows)-1]
+
+	currentRow := []int{}
+	for i := 0; i < n; i++ {
+		if i == 0 || i == n-1 {
+			currentRow = append(currentRow, 1)
+		} else {
+			sum := lastRow[i-1] + lastRow[i]
+			currentRow = append(currentRow, sum)
+		}
+	}
+	return append(previousRows, currentRow)
 }
