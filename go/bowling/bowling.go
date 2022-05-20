927b2209014b9e24491fed01f346fc5b9ce09a24
Rootul Patel <rootulp@gmail.com>
2022-05-20 13:09:43 -0400

    Pass a few tests

--- go/bowling/bowling.go
+++ go/bowling/bowling.go
@@ -1,15 +1,92 @@
 package bowling
 
-// Define the Game type here.
+import (
+	"fmt"
+	"log"
+)
+
+type Game struct {
+	frames []*Frame
+	rolls  []int
+}
 
 func NewGame() *Game {
-	panic("Please implement the NewGame function")
+	return &Game{}
 }
 
 func (g *Game) Roll(pins int) error {
-	panic("Please implement the Roll function")
+	g.rolls = append(g.rolls, pins)
+	return nil
 }
 
-func (g *Game) Score() (int, error) {
-	panic("Please implement the Score function")
+func (g *Game) parseFrames() {
+	g.frames = []*Frame{}
+	for _, roll := range g.rolls {
+		if len(g.frames) == 0 {
+			isComplete := roll == 10
+			g.frames = append(g.frames, &Frame{roll, 0, isComplete})
+		} else if !g.lastFrame().isComplete {
+			g.lastFrame().rollTwo = roll
+			g.lastFrame().isComplete = true
+		} else {
+			isComplete := roll == 10
+			g.frames = append(g.frames, &Frame{roll, 0, isComplete})
+		}
+	}
+}
+
+func (g *Game) Score() (score int, err error) {
+	g.parseFrames()
+	fmt.Printf("rolls: %v\nframes: %v\n", g.rolls, g.frames)
+	for i, frame := range g.frames {
+		score += frame.score(g.frames[i+1:])
+	}
+	return score, nil
+}
+
+func (g *Game) lastFrame() *Frame {
+	return g.frames[len(g.frames)-1]
+}
+
+type Frame struct {
+	rollOne    int
+	rollTwo    int
+	isComplete bool
+}
+
+func (f *Frame) String() string {
+	return fmt.Sprintf("[%d, %d]", f.rollOne, f.rollTwo)
+}
+
+func (f *Frame) score(nextFrames []*Frame) int {
+	if f.isOpenFrame() {
+		return f.rollOne + f.rollTwo
+	}
+	nextFrame := nextFrames[0]
+	nextRoll := nextFrame.rollOne
+	nextNextRoll := nextFrame.rollTwo
+	if f.isSpare() {
+		return 10 + nextRoll
+	}
+	if f.isStrike() {
+		return 10 + nextRoll + nextNextRoll
+	}
+	log.Fatalf("frame %v is not an open frame, spare, or strike", f)
+	return 0
+}
+
+func (f *Frame) isStrike() bool {
+	return isStrike(f.rollOne)
+}
+
+func (f *Frame) isSpare() bool {
+	return !f.isStrike() && f.rollOne+f.rollTwo == 10
+}
+
+func (f *Frame) isOpenFrame() bool {
+	return !f.isStrike() && !f.isSpare()
+}
+
+func isStrike(roll int) bool {
+	return roll == 10
 }
