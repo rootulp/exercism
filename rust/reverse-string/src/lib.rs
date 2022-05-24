fa7cab7c8021378a9a3fff3008bc03b7ee68f121
Rootul Patel <rootulp@gmail.com>
2022-05-23 23:40:28 -0400

    Pass reverse test graphemes

--- rust/reverse-string/src/lib.rs
+++ rust/reverse-string/src/lib.rs
@@ -1,4 +1,7 @@
+extern crate unicode_segmentation;
+use unicode_segmentation::UnicodeSegmentation;
+
 pub fn reverse(input: &str) -> String {
-    let reversed: String = input.chars().rev().collect();
+    let reversed: String = input.graphemes(true).rev().collect();
     reversed
 }
