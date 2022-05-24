f13f77f9737de9105adbb2f20dbd2e629285c07a
Rootul Patel <rootulp@gmail.com>
2022-05-24 12:25:35 -0400

    Prefer checked add

--- rust/gigasecond/src/lib.rs
+++ rust/gigasecond/src/lib.rs
@@ -5,5 +5,5 @@ const GIGASECOND: Duration = Duration::new(1_000_000_000, 0);
 
 // Returns a DateTime one billion seconds after start.
 pub fn after(start: DateTime) -> DateTime {
-    return start + GIGASECOND;
+    return start.checked_add(GIGASECOND).expect("overflow occured");
 }
