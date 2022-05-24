05fff7e9883c58188b9f113b20a5afc2e475ceb7
Rootul Patel <rootulp@gmail.com>
2022-05-24 14:54:43 -0400

    Remove hours from internal state

--- rust/clock/src/lib.rs
+++ rust/clock/src/lib.rs
@@ -2,35 +2,47 @@ use std::fmt;
 
 #[derive(PartialEq)]
 pub struct Clock {
-    hours: i32,
     minutes: i32,
 }
 
 impl Clock {
     pub fn new(hours: i32, minutes: i32) -> Self {
-        return Clock { hours, minutes };
+        return Clock {
+            minutes: (hours * 60) + minutes,
+        };
     }
 
     pub fn add_minutes(&self, minutes: i32) -> Self {
-        return Clock::new(self.hours, self.minutes + minutes);
+        return Clock::new(0, self.minutes + minutes);
     }
 }
 
 impl fmt::Display for Clock {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let minutes = self.minutes % 60;
-        let additional_hours = self.minutes / 60;
-        let mut hours = self.hours + additional_hours;
-        while hours < 0 {
-            hours += 24
+        let mut minutes = self.minutes;
+        let mut hours = 0;
+
+        while minutes < 0 {
+            hours -= 1;
+            minutes += 60
         }
-        let hours = hours % 24;
+        while minutes >= 60 {
+            hours += 1;
+            minutes -= 60
+        }
+        while hours < 0 {
+            hours += 24;
+        }
+        while hours >= 24 {
+            hours -= 24;
+        }
+
         write!(f, "{:02}:{:02}", hours, minutes)
     }
 }
 
 impl fmt::Debug for Clock {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{:02}:{:02}", self.hours, self.minutes)
+        write!(f, "{}", self.minutes)
     }
 }
