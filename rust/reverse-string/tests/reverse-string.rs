10c922b9e4d1e2b5761222b729a60836c9ca1c34
Rootul Patel <rootulp@gmail.com>
2022-05-23 23:34:46 -0400

    Pass all standard tests

--- rust/reverse-string/tests/reverse-string.rs
+++ rust/reverse-string/tests/reverse-string.rs
@@ -19,42 +19,36 @@ fn test_an_empty_string() {
 }
 
 #[test]
-#[ignore]
 /// a word
 fn test_a_word() {
     process_reverse_case("robot", "tobor");
 }
 
 #[test]
-#[ignore]
 /// a capitalized word
 fn test_a_capitalized_word() {
     process_reverse_case("Ramen", "nemaR");
 }
 
 #[test]
-#[ignore]
 /// a sentence with punctuation
 fn test_a_sentence_with_punctuation() {
     process_reverse_case("I'm hungry!", "!yrgnuh m'I");
 }
 
 #[test]
-#[ignore]
 /// a palindrome
 fn test_a_palindrome() {
     process_reverse_case("racecar", "racecar");
 }
 
 #[test]
-#[ignore]
 /// an even-sized word
 fn test_an_even_sized_word() {
     process_reverse_case("drawer", "reward");
 }
 
 #[test]
-#[ignore]
 /// wide characters
 fn test_wide_characters() {
     process_reverse_case("子猫", "猫子");
