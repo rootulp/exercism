2e81caa2b21d07af06b86c42bdf37f121f3d6ce5
Rootul Patel <rootulp@gmail.com>
2022-05-24 14:23:00 -0400

    Left pad with two zeros

--- rust/clock/src/lib.rs
+++ rust/clock/src/lib.rs
@@ -18,12 +18,12 @@ impl Clock {
 
 impl fmt::Display for Clock {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "({}:{})", self.hours, self.minutes)
+        write!(f, "{:02}:{:02}", self.hours, self.minutes)
     }
 }
 
 impl fmt::Debug for Clock {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "({}:{})", self.hours, self.minutes)
+        write!(f, "{:02}:{:02}", self.hours, self.minutes)
     }
 }
