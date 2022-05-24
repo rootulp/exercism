f683d673692b0ff7775303e9b725c2ac3f11f1f4
Rootul Patel <rootulp@gmail.com>
2022-05-24 12:23:07 -0400

    Make gigasecond a constant
    Duration multiplication is not a constant trait so had to use Duration::new

--- rust/gigasecond/src/lib.rs
+++ rust/gigasecond/src/lib.rs
@@ -1,8 +1,9 @@
 use time::Duration;
 use time::PrimitiveDateTime as DateTime;
 
+const GIGASECOND: Duration = Duration::new(1_000_000_000, 0);
+
 // Returns a DateTime one billion seconds after start.
 pub fn after(start: DateTime) -> DateTime {
-    let gigasecond: Duration = Duration::SECOND * 1_000_000_000;
-    return start + gigasecond;
+    return start + GIGASECOND;
 }
