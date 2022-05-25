d774b202578b3aa94a5001287711a5636baf0c23
Rootul Patel <rootulp@gmail.com>
2022-05-25 11:49:04 -0400

    Pass first test

--- rust/anagram/src/lib.rs
+++ rust/anagram/src/lib.rs
@@ -1,9 +1,27 @@
 use std::collections::HashSet;
 
-pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&str]) -> HashSet<&'a str> {
-    unimplemented!(
-        "For the '{}' word find anagrams among the following words: {:?}",
-        word,
-        possible_anagrams
-    );
+pub fn anagrams_for<'a>(word: &str, possible_anagrams: &'a [&str]) -> HashSet<&'a str> {
+    let mut anagrams: HashSet<&'a str> = HashSet::new();
+
+    for possible in possible_anagrams {
+        if is_anagram(word, possible) {
+            anagrams.insert(possible);
+        }
+    }
+    anagrams
+}
+
+fn is_anagram(word: &str, possible: &str) -> bool {
+    if word == possible {
+        return false;
+    }
+
+    return sorted_lowercase_chars(word) == sorted_lowercase_chars(possible);
+}
+
+fn sorted_lowercase_chars(word: &str) -> Vec<char> {
+    let mut word_chars = word.to_lowercase().chars().collect::<Vec<char>>();
+    word_chars.sort_unstable();
+
+    return word_chars;
 }
