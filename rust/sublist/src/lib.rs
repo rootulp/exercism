12d5244bbe14afae2b4110fe4bfa0f37c0dcda00
Rootul Patel <rootulp@gmail.com>
2022-05-25 20:55:46 -0400

    Solve sublist

--- rust/sublist/src/lib.rs
+++ rust/sublist/src/lib.rs
@@ -6,9 +6,27 @@ pub enum Comparison {
     Unequal,
 }
 
-pub fn sublist<T: PartialEq>(_first_list: &[T], _second_list: &[T]) -> Comparison {
-    if _first_list == _second_list {
+pub fn sublist<T: PartialEq>(a: &[T], b: &[T]) -> Comparison {
+    if a == b {
         return Comparison::Equal;
     }
+    if is_sublist(a, b) {
+        return Comparison::Sublist;
+    }
+    if is_sublist(b, a) {
+        return Comparison::Superlist;
+    }
     return Comparison::Unequal;
+}
+
+fn is_sublist<T: PartialEq>(a: &[T], b: &[T]) -> bool {
+    if a.len() == 0 {
+        return true;
+    }
+    for list in b.windows(a.len()) {
+        if a == list {
+            return true;
+        }
+    }
+    return false;
 }
--- rust/sublist/tests/sublist.rs
+++ rust/sublist/tests/sublist.rs
@@ -8,25 +8,21 @@ fn empty_equals_empty() {
 }
 
 #[test]
-#[ignore]
 fn test_empty_is_a_sublist_of_anything() {
     assert_eq!(Comparison::Sublist, sublist(&[], &['a', 's', 'd', 'f']));
 }
 
 #[test]
-#[ignore]
 fn test_anything_is_a_superlist_of_empty() {
     assert_eq!(Comparison::Superlist, sublist(&['a', 's', 'd', 'f'], &[]));
 }
 
 #[test]
-#[ignore]
 fn test_1_is_not_2() {
     assert_eq!(Comparison::Unequal, sublist(&[1], &[2]));
 }
 
 #[test]
-#[ignore]
 fn test_compare_larger_equal_lists() {
     use std::iter::repeat;
 
@@ -36,31 +32,26 @@ fn test_compare_larger_equal_lists() {
 }
 
 #[test]
-#[ignore]
 fn test_sublist_at_start() {
     assert_eq!(Comparison::Sublist, sublist(&[1, 2, 3], &[1, 2, 3, 4, 5]));
 }
 
 #[test]
-#[ignore]
 fn sublist_in_middle() {
     assert_eq!(Comparison::Sublist, sublist(&[4, 3, 2], &[5, 4, 3, 2, 1]));
 }
 
 #[test]
-#[ignore]
 fn sublist_at_end() {
     assert_eq!(Comparison::Sublist, sublist(&[3, 4, 5], &[1, 2, 3, 4, 5]));
 }
 
 #[test]
-#[ignore]
 fn partially_matching_sublist_at_start() {
     assert_eq!(Comparison::Sublist, sublist(&[1, 1, 2], &[1, 1, 1, 2]));
 }
 
 #[test]
-#[ignore]
 fn sublist_early_in_huge_list() {
     let huge: Vec<u32> = (1..1_000_000).collect();
 
@@ -68,7 +59,6 @@ fn sublist_early_in_huge_list() {
 }
 
 #[test]
-#[ignore]
 fn huge_sublist_not_in_huge_list() {
     let v1: Vec<u64> = (10..1_000_001).collect();
     let v2: Vec<u64> = (1..1_000_000).collect();
@@ -77,31 +67,26 @@ fn huge_sublist_not_in_huge_list() {
 }
 
 #[test]
-#[ignore]
 fn superlist_at_start() {
     assert_eq!(Comparison::Superlist, sublist(&[1, 2, 3, 4, 5], &[1, 2, 3]));
 }
 
 #[test]
-#[ignore]
 fn superlist_in_middle() {
     assert_eq!(Comparison::Superlist, sublist(&[5, 4, 3, 2, 1], &[4, 3, 2]));
 }
 
 #[test]
-#[ignore]
 fn superlist_at_end() {
     assert_eq!(Comparison::Superlist, sublist(&[1, 2, 3, 4, 5], &[3, 4, 5]));
 }
 
 #[test]
-#[ignore]
 fn second_list_missing_element_from_first_list() {
     assert_eq!(Comparison::Unequal, sublist(&[1, 2, 3], &[1, 3]));
 }
 
 #[test]
-#[ignore]
 fn superlist_early_in_huge_list() {
     let huge: Vec<u32> = (1..1_000_000).collect();
 
@@ -109,7 +94,6 @@ fn superlist_early_in_huge_list() {
 }
 
 #[test]
-#[ignore]
 fn recurring_values_sublist() {
     assert_eq!(
         Comparison::Sublist,
@@ -118,7 +102,6 @@ fn recurring_values_sublist() {
 }
 
 #[test]
-#[ignore]
 fn recurring_values_unequal() {
     assert_eq!(
         Comparison::Unequal,
