770b228d078c22b55d78f15678320a6ab5047476
Rootul Patel <rootulp@gmail.com>
2022-05-26 12:56:50 -0400

    Solve space age

--- rust/space-age/src/lib.rs
+++ rust/space-age/src/lib.rs
@@ -33,15 +33,45 @@ impl Planet for Mercury {
         return Earth::years_during(duration) / MERCURY_ORBITAL_PERIOD;
     }
 }
-impl Planet for Venus {}
+impl Planet for Venus {
+    fn years_during(duration: &Duration) -> f64 {
+        const VENUS_ORBITAL_PERIOD: f64 = 0.61519726;
+        return Earth::years_during(duration) / VENUS_ORBITAL_PERIOD;
+    }
+}
 impl Planet for Earth {
     fn years_during(duration: &Duration) -> f64 {
         const SECONDS_IN_EARTH_YEAR: f64 = 31557600.0;
         return duration.seconds as f64 / SECONDS_IN_EARTH_YEAR;
     }
 }
-impl Planet for Mars {}
-impl Planet for Jupiter {}
-impl Planet for Saturn {}
-impl Planet for Uranus {}
-impl Planet for Neptune {}
+impl Planet for Mars {
+    fn years_during(duration: &Duration) -> f64 {
+        const MARS_ORBITAL_PERIOD: f64 = 1.8808158;
+        return Earth::years_during(duration) / MARS_ORBITAL_PERIOD;
+    }
+}
+impl Planet for Jupiter {
+    fn years_during(duration: &Duration) -> f64 {
+        const JUPITER_ORBITAL_PERIOD: f64 = 11.862615;
+        return Earth::years_during(duration) / JUPITER_ORBITAL_PERIOD;
+    }
+}
+impl Planet for Saturn {
+    fn years_during(duration: &Duration) -> f64 {
+        const SATURN_ORBITAL_PERIOD: f64 = 29.447498;
+        return Earth::years_during(duration) / SATURN_ORBITAL_PERIOD;
+    }
+}
+impl Planet for Uranus {
+    fn years_during(duration: &Duration) -> f64 {
+        const URANUS_ORBITAL_PERIOD: f64 = 84.016846;
+        return Earth::years_during(duration) / URANUS_ORBITAL_PERIOD;
+    }
+}
+impl Planet for Neptune {
+    fn years_during(duration: &Duration) -> f64 {
+        const NEPTUNE_ORBITAL_PERIOD: f64 = 164.79132;
+        return Earth::years_during(duration) / NEPTUNE_ORBITAL_PERIOD;
+    }
+}
--- rust/space-age/tests/space-age.rs
+++ rust/space-age/tests/space-age.rs
@@ -24,42 +24,36 @@ fn mercury_age() {
 }
 
 #[test]
-#[ignore]
 fn venus_age() {
     let duration = Duration::from(189_839_836);
     assert_in_delta(9.78, Venus::years_during(&duration));
 }
 
 #[test]
-#[ignore]
 fn mars_age() {
     let duration = Duration::from(2_129_871_239);
     assert_in_delta(35.88, Mars::years_during(&duration));
 }
 
 #[test]
-#[ignore]
 fn jupiter_age() {
     let duration = Duration::from(901_876_382);
     assert_in_delta(2.41, Jupiter::years_during(&duration));
 }
 
 #[test]
-#[ignore]
 fn saturn_age() {
     let duration = Duration::from(2_000_000_000);
     assert_in_delta(2.15, Saturn::years_during(&duration));
 }
 
 #[test]
-#[ignore]
 fn uranus_age() {
     let duration = Duration::from(1_210_123_456);
     assert_in_delta(0.46, Uranus::years_during(&duration));
 }
 
 #[test]
-#[ignore]
 fn neptune_age() {
     let duration = Duration::from(1_821_023_456);
     assert_in_delta(0.35, Neptune::years_during(&duration));
