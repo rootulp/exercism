9e3fb281d493e004212b67b5f70f00cbcfd39544
Rootul Patel <rootulp@gmail.com>
2022-05-25 20:42:46 -0400

    Start sublist

---
+++ rust/sublist/src/lib.rs
@@ -0,0 +1,14 @@
+#[derive(Debug, PartialEq)]
+pub enum Comparison {
+    Equal,
+    Sublist,
+    Superlist,
+    Unequal,
+}
+
+pub fn sublist<T: PartialEq>(_first_list: &[T], _second_list: &[T]) -> Comparison {
+    if _first_list == _second_list {
+        return Comparison::Equal;
+    }
+    return Comparison::Unequal;
+}
