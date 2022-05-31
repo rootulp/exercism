f05d02850314b13e068a9066075eccede686fdc8
Rootul Patel <rootulp@gmail.com>
2022-05-31 12:22:12 -0400

    Pass part 1

--- go/robot-simulator/robot_simulator.go
+++ go/robot-simulator/robot_simulator.go
@@ -1,29 +1,84 @@
 package robot
 
+import (
+	"fmt"
+	"log"
+)
+
 // See defs.go for other definitions
 
 // Step 1
 // Define Dir type here.
+
 // Define N, E, S, W here.
+const N Dir = 0
+const E Dir = 1
+const S Dir = 2
+const W Dir = 3
 
 func Right() {
-	panic("Please implement the Right function")
+	switch Step1Robot.Dir {
+	case N:
+		Step1Robot.Dir = E
+	case E:
+		Step1Robot.Dir = S
+	case S:
+		Step1Robot.Dir = W
+	case W:
+		Step1Robot.Dir = N
+	default:
+		log.Fatalf("unrecognized direction %d", Step1Robot.Dir)
+	}
 }
 
 func Left() {
-	panic("Please implement the Left function")
+	switch Step1Robot.Dir {
+	case N:
+		Step1Robot.Dir = W
+	case E:
+		Step1Robot.Dir = N
+	case S:
+		Step1Robot.Dir = E
+	case W:
+		Step1Robot.Dir = S
+	default:
+		log.Fatalf("unrecognized direction %d", Step1Robot.Dir)
+	}
 }
 
 func Advance() {
-	panic("Please implement the Advance function")
+	switch Step1Robot.Dir {
+	case N:
+		Step1Robot.Y++
+	case E:
+		Step1Robot.X++
+	case S:
+		Step1Robot.Y--
+	case W:
+		Step1Robot.X--
+	default:
+		log.Fatalf("unrecognized direction %d", Step1Robot.Dir)
+	}
 }
 
 func (d Dir) String() string {
-	panic("Please implement the String function")
+	switch d {
+	case N:
+		return "N"
+	case E:
+		return "E"
+	case S:
+		return "S"
+	case W:
+		return "W"
+	default:
+		return fmt.Sprintf("unrecognized direction %d", d)
+	}
 }
 
 // Step 2
 // Define Action type here.
+type Action struct{}
 
 func StartRobot(command chan Command, action chan Action) {
 	panic("Please implement the StartRobot function")
@@ -35,6 +90,7 @@ func Room(extent Rect, robot Step2Robot, action chan Action, report chan Step2Ro
 
 // Step 3
 // Define Action3 type here.
+type Action3 struct{}
 
 func StartRobot3(name, script string, action chan Action3, log chan string) {
 	panic("Please implement the StartRobot3 function")
