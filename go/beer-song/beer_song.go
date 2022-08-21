1eac60b3af582e04f8b4cdba67a29872408c98a0
Rootul Patel <rootulp@gmail.com>
2022-08-21 14:37:05 -0400

    Implement Verse

--- go/beer-song/beer_song.go
+++ go/beer-song/beer_song.go
@@ -1,5 +1,7 @@
 package beer
 
+import "fmt"
+
 func Song() string {
 	panic("Please implement the Song function")
 }
@@ -9,5 +11,14 @@ func Verses(start, stop int) (string, error) {
 }
 
 func Verse(n int) (string, error) {
-	panic("Please implement the Verse function")
+	switch n {
+	case 0:
+		return "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n", nil
+	case 1:
+		return "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n", nil
+	case 2:
+		return "2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n", nil
+	default:
+		return fmt.Sprintf("%d bottles of beer on the wall, %d bottles of beer.\nTake one down and pass it around, %d bottles of beer on the wall.\n", n, n, n-1), nil
+	}
 }
