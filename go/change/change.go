c791e187281e80f3d28c4f148d4f3ede39755e08
Rootul Patel <rootulp@gmail.com>
2023-02-10 14:02:46 -0500

    chore: improve change

--- go/change/change.go
+++ go/change/change.go
@@ -1,33 +1,35 @@
 package change
 
 import (
-	"errors"
+	"fmt"
 	"math"
 )
 
-// Change returns the minimum coins from available that add up to the target. It
-// returns an error if target is negative or if not enough coins are available
-// to make the target change.
-func Change(coins []int, target int) ([]int, error) {
-	changeToCoins := generateChangeToCoins(coins, target)
-	result, ok := changeToCoins[target]
+// Change returns the minimum coins from availableCoins that add up to the
+// target. It returns an error if target is negative or if not enough coins are
+// available to make the target change.
+func Change(availableCoins []int, target int) ([]int, error) {
+	sumToCoins := generateSumToCoins(availableCoins, target)
+	result, ok := sumToCoins[target]
 	if !ok {
-		return nil, errors.New("no change available")
+		return nil, fmt.Errorf("no change available for target %d", target)
 	}
 	return result, nil
 }
 
-func generateChangeToCoins(coins []int, target int) map[int][]int {
-	changeToCoins := map[int][]int{0: {}}
+// generateSumToCoins returns a map from sum to the fewest coins that add up
+// to that sum. The map spans the range from 0 to target inclusive.
+func generateSumToCoins(availableCoins []int, target int) map[int][]int {
+	sumToCoins := map[int][]int{0: {}}
 	for i := 1; i <= target; i++ {
-		var min = math.MaxUint8
-		for _, coin := range coins {
-			remainingChange, ok := changeToCoins[i-coin]
-			if ok && min > len(remainingChange) {
-				min = len(remainingChange)
-				changeToCoins[i] = append([]int{coin}, remainingChange...)
+		var minNumCoins = math.MaxUint8
+		for _, coin := range availableCoins {
+			remainingChange, ok := sumToCoins[i-coin]
+			if ok && minNumCoins > len(remainingChange) {
+				minNumCoins = len(remainingChange)
+				sumToCoins[i] = append([]int{coin}, remainingChange...)
 			}
 		}
 	}
-	return changeToCoins
+	return sumToCoins
 }
