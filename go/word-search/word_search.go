5997683b185a7a97866fcee0d202b77d4c26e7e2
Rootul Patel <rootulp@gmail.com>
2023-02-20 15:14:05 -0500

    pass error test cases

--- go/word-search/word_search.go
+++ go/word-search/word_search.go
@@ -1,5 +1,16 @@
 package wordsearch
 
-func Solve(words []string, puzzle []string) (map[string][2][2]int, error) {
-	panic("Please implement the Solve function")
+import "fmt"
+
+func Solve(words []string, puzzle []string) (result map[string][2][2]int, err error) {
+	result = make(map[string][2][2]int)
+	for _, word := range words {
+		result[word] = [2][2]int{{-1, -1}, {-1, -1}}
+	}
+	for k, v := range result {
+		if v == [2][2]int{{-1, -1}, {-1, -1}} {
+			err = fmt.Errorf("word %s not found", k)
+		}
+	}
+	return result, err
 }
