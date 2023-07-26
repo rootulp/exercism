51a9037d46245e42aae546cb49594fdb3712f592
Rootul Patel <rootulp@gmail.com>
2023-07-26 13:34:43 -0400

    solve rna transcription

--- rust/rna-transcription/src/lib.rs
+++ rust/rna-transcription/src/lib.rs
@@ -1,48 +1,53 @@
 use std::collections::HashMap;
 
 #[derive(Debug, PartialEq, Eq)]
-pub struct Dna<'a> {
-    contents: &'a str,
+pub struct Dna {
+    contents: String,
 }
 
 #[derive(Debug, PartialEq, Eq)]
-pub struct Rna<'a> {
-    contents: &'a str,
+pub struct Rna {
+    contents: String,
 }
 
-impl<'a> Dna<'a> {
+impl Dna {
     const VALID_NUCLEOTIDES: &str = "ACGT";
 
-    pub fn new(dna: &str) -> Result<Dna, usize> {
-        for (i, c) in dna.chars().enumerate() {
+    pub fn new(input: &str) -> Result<Dna, usize> {
+        for (i, c) in input.chars().enumerate() {
             if !Self::VALID_NUCLEOTIDES.contains(c) {
                 return Err(i);
             }
         }
-        Ok(Dna { contents: dna })
+        Ok(Dna {
+            contents: input.to_string(),
+        })
     }
 
-    pub fn into_rna(self) -> Rna<'a> {
+    pub fn into_rna(self) -> Rna {
         let complements = HashMap::from([('G', 'C'), ('C', 'G'), ('T', 'A'), ('A', 'U')]);
 
-        let mut rna = self.contents.chars().collect::<Vec<_>>();
-        for (i, c) in self.contents.chars().enumerate() {
-            rna[i] = *complements.get(&c).unwrap();
+        let mut converted = String::new();
+
+        for (_, c) in self.contents.chars().enumerate() {
+            converted.push(*complements.get(&c).unwrap())
         }
 
-        let result: Rna<'_> = Rna::new(self.contents).expect("dna could not be converted to rna");
+        let result = Rna::new(converted.as_str()).expect("dna could not be converted to rna");
         result
     }
 }
 
-impl<'a> Rna<'a> {
+impl Rna {
     const VALID_NUCLEOTIDES: &str = "ACGU";
-    pub fn new(rna: &str) -> Result<Rna, usize> {
-        for (i, c) in rna.chars().enumerate() {
+    pub fn new(input: &str) -> Result<Rna, usize> {
+        for (i, c) in input.chars().enumerate() {
             if !Self::VALID_NUCLEOTIDES.contains(c) {
                 return Err(i);
             }
         }
-        Ok(Rna { contents: rna })
+        Ok(Rna {
+            contents: input.to_string(),
+        })
     }
 }
--- rust/rna-transcription/tests/rna-transcription.rs
+++ rust/rna-transcription/tests/rna-transcription.rs
@@ -39,7 +39,6 @@ fn test_acid_equals_acid() {
 }
 
 #[test]
-#[ignore]
 fn test_transcribes_cytosine_guanine() {
     assert_eq!(
         dna::Rna::new("G").unwrap(),
@@ -48,7 +47,6 @@ fn test_transcribes_cytosine_guanine() {
 }
 
 #[test]
-#[ignore]
 fn test_transcribes_guanine_cytosine() {
     assert_eq!(
         dna::Rna::new("C").unwrap(),
@@ -57,7 +55,6 @@ fn test_transcribes_guanine_cytosine() {
 }
 
 #[test]
-#[ignore]
 fn test_transcribes_adenine_uracil() {
     assert_eq!(
         dna::Rna::new("U").unwrap(),
@@ -66,7 +63,6 @@ fn test_transcribes_adenine_uracil() {
 }
 
 #[test]
-#[ignore]
 fn test_transcribes_thymine_to_adenine() {
     assert_eq!(
         dna::Rna::new("A").unwrap(),
@@ -75,7 +71,6 @@ fn test_transcribes_thymine_to_adenine() {
 }
 
 #[test]
-#[ignore]
 fn test_transcribes_all_dna_to_rna() {
     assert_eq!(
         dna::Rna::new("UGCACCAGAAUU").unwrap(),
