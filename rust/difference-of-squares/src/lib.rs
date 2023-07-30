8f35c4e95bdda25350427609683c31de7fdbb45b
Rootul Patel <rootulp@gmail.com>
2023-07-30 12:24:12 -0400

    solve difference of squares

--- rust/difference-of-squares/src/lib.rs
+++ rust/difference-of-squares/src/lib.rs
@@ -1,11 +1,19 @@
 pub fn square_of_sum(n: u32) -> u32 {
-    unimplemented!("square of sum of 1...{n}")
+    let mut sum = 0;
+    for i in 1..=n {
+        sum += i;
+    }
+    sum * sum
 }
 
 pub fn sum_of_squares(n: u32) -> u32 {
-    unimplemented!("sum of squares of 1...{n}")
+    let mut sum = 0;
+    for i in 1..=n {
+        sum += i * i;
+    }
+    sum
 }
 
 pub fn difference(n: u32) -> u32 {
-    unimplemented!("difference between square of sum of 1...{n} and sum of squares of 1...{n}")
+    square_of_sum(n) - sum_of_squares(n)
 }
