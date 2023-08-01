9195904853eae9ca0fc2c2f8911ebc6cc2f97d28
Rootul Patel <rootulp@gmail.com>
2023-08-01 10:54:53 -0400

    implement sing

--- rust/beer-song/src/lib.rs
+++ rust/beer-song/src/lib.rs
@@ -3,11 +3,14 @@ pub fn verse(n: u32) -> String {
         0 => "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n".to_string(),
         1 => "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n".to_string(),
         2 => "2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n".to_string(),
-        8 => "8 bottles of beer on the wall, 8 bottles of beer.\nTake one down and pass it around, 7 bottles of beer on the wall.\n".to_string(),
-        _ => unimplemented!("sing verse {n}")
+        n => format!("{n} bottles of beer on the wall, {n} bottles of beer.\nTake one down and pass it around, {} bottles of beer on the wall.\n", n-1),
     }
 }
 
 pub fn sing(start: u32, end: u32) -> String {
-    unimplemented!("sing verses {start} to {end}, inclusive")
+    let mut song: Vec<String> = Vec::new();
+    for i in (end..=start).rev() {
+        song.push(verse(i));
+    }
+    song.join("\n")
 }
