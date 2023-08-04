c03acdf1017d4fc23e5e87d67131cad9da6f6a90
Rootul Patel <rootulp@gmail.com>
2023-08-04 15:14:14 -0400

    solve most test cases

---
+++ rust/sum-of-multiples/src/lib.rs
@@ -0,0 +1,17 @@
+use std::collections::HashSet;
+
+pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
+    let mut points = HashSet::new();
+
+    for factor in factors {
+        let multiples = multiples(limit, *factor);
+        for multiple in multiples {
+            points.insert(multiple);
+        }
+    }
+    points.into_iter().sum()
+}
+
+fn multiples(limit: u32, factor: u32) -> Vec<u32> {
+    (1..limit).filter(|x| x % factor == 0).collect()
+}
