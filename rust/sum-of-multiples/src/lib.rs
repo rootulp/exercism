cc8c672316dba5c8b5673c1870e9ab0ad97e027a
Rootul Patel <rootulp@gmail.com>
2023-08-04 15:27:30 -0400

    rename

--- rust/sum-of-multiples/src/lib.rs
+++ rust/sum-of-multiples/src/lib.rs
@@ -5,8 +5,8 @@ pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
 }
 
 fn unique_multiples(limit: u32, factors: &[u32]) -> Vec<u32> {
-    let multiples: Vec<_> = factors.iter().flat_map(|x| multiples(limit, *x)).collect();
-    let unique_multiples: HashSet<u32> = multiples.into_iter().collect();
+    let all_multiples: Vec<_> = factors.iter().flat_map(|x| multiples(limit, *x)).collect();
+    let unique_multiples: HashSet<u32> = all_multiples.into_iter().collect();
     unique_multiples.into_iter().collect()
 }
 
