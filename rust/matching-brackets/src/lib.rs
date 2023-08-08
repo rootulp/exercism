3d94eb92b1bcb2561b544d9265ba61317ed83c12
Rootul Patel <rootulp@gmail.com>
2023-08-08 14:00:24 -0400

    solve matching brackets

--- rust/matching-brackets/src/lib.rs
+++ rust/matching-brackets/src/lib.rs
@@ -1,3 +1,40 @@
-pub fn brackets_are_balanced(string: &str) -> bool {
-    string == "[]"
+pub fn brackets_are_balanced(input: &str) -> bool {
+    if input.is_empty() {
+        return true;
+    }
+    let mut stack = Vec::new();
+    for c in input.chars() {
+        if is_opening_bracket(c) {
+            stack.push(c);
+        }
+        if is_closing_bracket(c) {
+            let opening_bracket = get_opening_bracket(c);
+            match stack.pop() {
+                Some(opening) => {
+                    if opening != opening_bracket {
+                        return false;
+                    }
+                }
+                None => return false,
+            }
+        }
+    }
+    stack.is_empty()
+}
+
+fn is_opening_bracket(c: char) -> bool {
+    c == '[' || c == '{' || c == '('
+}
+
+fn is_closing_bracket(c: char) -> bool {
+    c == ']' || c == '}' || c == ')'
+}
+
+fn get_opening_bracket(c: char) -> char {
+    match c {
+        ']' => '[',
+        '}' => '{',
+        ')' => '(',
+        _ => panic!("Invalid closing bracket"),
+    }
 }
