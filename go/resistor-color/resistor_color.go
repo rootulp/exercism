69c68a847dd9d9af895758a51d74f849eb2017a3
Rootul Patel <rootulp@gmail.com>
2023-11-26 12:41:56 -0500

    pass first test

--- go/resistor-color/resistor_color.go
+++ go/resistor-color/resistor_color.go
@@ -7,5 +7,24 @@ func Colors() []string {
 
 // ColorCode returns the resistance value of the given color.
 func ColorCode(color string) int {
-	panic("Please implement the ColorCode function")
+	// - Black: 0
+	// - Brown: 1
+	// - Red: 2
+	// - Orange: 3
+	// - Yellow: 4
+	// - Green: 5
+	// - Blue: 6
+	// - Violet: 7
+	// - Grey: 8
+	// - White: 9
+	switch color {
+	case "black":
+		return 0
+	case "white":
+		return 9
+	case "orange":
+		return 3
+	default:
+		panic("color not supported")
+	}
 }
