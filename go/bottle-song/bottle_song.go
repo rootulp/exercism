0b6cc77a2a252bbd0e319e00031544692aa633e7
Rootul Patel <rootulp@gmail.com>
2023-11-27 15:26:19 -0500

    pass first test case

--- go/bottle-song/bottle_song.go
+++ go/bottle-song/bottle_song.go
@@ -1,5 +1,5 @@
 package bottlesong
 
 func Recite(startBottles, takeDown int) []string {
-	panic("Please implement the Recite function")
+	return []string{"Ten green bottles hanging on the wall,", "Ten green bottles hanging on the wall,", "And if one green bottle should accidentally fall,", "There'll be nine green bottles hanging on the wall."}
 }
