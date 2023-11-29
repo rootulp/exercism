a6ee80b4f2a63b64a8403960124810d8b93cfa0b
Rootul Patel <rootulp@gmail.com>
2023-11-29 14:09:51 -0500

    refactor: extract firstLine() and lastLine()

--- go/food-chain/food_chain.go
+++ go/food-chain/food_chain.go
@@ -1,10 +1,24 @@
 package foodchain
 
+import (
+	"fmt"
+	"strings"
+)
+
 func Verse(v int) string {
 	if v == 1 {
-		return "I know an old lady who swallowed a fly.\nI don't know why she swallowed the fly. Perhaps she'll die."
+		lines := []string{firstLine("fly"), lastLine()}
+		return strings.Join(lines, "\n")
 	}
 	return "I know an old lady who swallowed a spider.\nIt wriggled and jiggled and tickled inside her.\nShe swallowed the spider to catch the fly.\nI don't know why she swallowed the fly. Perhaps she'll die."
+}
+
+func firstLine(animal string) string {
+	return fmt.Sprintf("I know an old lady who swallowed a %v.", animal)
+}
+
+func lastLine() string {
+	return "I don't know why she swallowed the fly. Perhaps she'll die."
 }
 
 func Verses(start, end int) string {
