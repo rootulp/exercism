eb388e7a7b85a6713aa1902a885682f5d21328e6
Rootul Patel <rootulp@gmail.com>
2023-11-29 14:40:40 -0500

    refactor: extract map for middle lines

--- go/food-chain/food_chain.go
+++ go/food-chain/food_chain.go
@@ -28,50 +28,59 @@ func middleLines(verseNumber int) []string {
 		return []string{}
 	}
 	if verseNumber == 2 {
-		return []string{
-			"It wriggled and jiggled and tickled inside her.",
-			"She swallowed the spider to catch the fly.",
-		}
+		return verseNumberToMiddleLines[verseNumber]
 	}
 	if verseNumber == 3 {
-		lines := []string{
-			"How absurd to swallow a bird!",
-			"She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.",
-		}
+		lines := verseNumberToMiddleLines[verseNumber]
 		return append(lines, middleLines(verseNumber - 1)[1:]...)
 	}
 	if verseNumber == 4 {
-		lines := []string{
-			"Imagine that, to swallow a cat!",
-			"She swallowed the cat to catch the bird.",
-		}
+		lines := verseNumberToMiddleLines[verseNumber]
 		return append(lines, middleLines(verseNumber - 1)[1:]...)
 	}
 	if verseNumber == 5 {
-		lines := []string{
-			"What a hog, to swallow a dog!",
-			"She swallowed the dog to catch the cat.",
-		}
+		lines := verseNumberToMiddleLines[verseNumber]
 		return append(lines, middleLines(verseNumber - 1)[1:]...)
 	}
 	if verseNumber == 6 {
-		lines := []string{
-			"Just opened her throat and swallowed a goat!",
-			"She swallowed the goat to catch the dog.",
-		}
+		lines := verseNumberToMiddleLines[verseNumber]
 		return append(lines, middleLines(verseNumber - 1)[1:]...)
 	}
 	if verseNumber == 7 {
-		lines := []string{
-			"I don't know how she swallowed a cow!",
-			"She swallowed the cow to catch the goat.",
-		}
+		lines := verseNumberToMiddleLines[verseNumber]
 		return append(lines, middleLines(verseNumber - 1)[1:]...)
 	}
 	if verseNumber == 8 {
 		return []string{}
 	}
 	panic(fmt.Sprintf("unsupported verseNumber %v", verseNumber))
+}
+
+var verseNumberToMiddleLines = map[int][]string{
+	2: {
+		"It wriggled and jiggled and tickled inside her.",
+		"She swallowed the spider to catch the fly.",
+	},
+	3: {
+		"How absurd to swallow a bird!",
+		"She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.",
+	},
+	4: []string{
+		"Imagine that, to swallow a cat!",
+		"She swallowed the cat to catch the bird.",
+	},
+	5: []string{
+		"What a hog, to swallow a dog!",
+		"She swallowed the dog to catch the cat.",
+	},
+	6: []string{
+		"Just opened her throat and swallowed a goat!",
+		"She swallowed the goat to catch the dog.",
+	},
+	7: []string{
+		"I don't know how she swallowed a cow!",
+		"She swallowed the cow to catch the goat.",
+	},
 }
 
 func startingAnimal(verseNumber int) string {
