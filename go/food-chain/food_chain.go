295021524546c4d497490d75368ba54a20d2a87c
Rootul Patel <rootulp@gmail.com>
2023-11-29 14:17:17 -0500

    refactor: startingAnimal()

--- go/food-chain/food_chain.go
+++ go/food-chain/food_chain.go
@@ -5,12 +5,32 @@ import (
 	"strings"
 )
 
-func Verse(v int) string {
-	if v == 1 {
-		lines := []string{firstLine("fly"), lastLine()}
-		return strings.Join(lines, "\n")
+func startingAnimal(verseNumber int) string {
+	switch verseNumber {
+	case 1:
+		return "fly"
+	case 2:
+		return "spider"
+	default:
+		panic(fmt.Sprintf("unsupported verseNumber %v", verseNumber))
 	}
-	return "I know an old lady who swallowed a spider.\nIt wriggled and jiggled and tickled inside her.\nShe swallowed the spider to catch the fly.\nI don't know why she swallowed the fly. Perhaps she'll die."
+}
+
+func Verse(verseNumber int) string {
+	lines := []string{firstLine(startingAnimal(verseNumber))}
+	lines = append(lines, middleLines(verseNumber)...)
+	lines = append(lines, lastLine())
+	return strings.Join(lines, "\n")
+}
+
+func middleLines(v int) []string {
+	if v == 0 || v == 1 {
+		return []string{}
+	}
+	if v == 2 {
+		return []string{"It wriggled and jiggled and tickled inside her.", "She swallowed the spider to catch the fly."}
+	}
+	return []string{}
 }
 
 func firstLine(animal string) string {
