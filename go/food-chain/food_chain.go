ad0559c90f9f320284be22c6fc462aaaef605057
Rootul Patel <rootulp@gmail.com>
2023-11-29 14:05:03 -0500

    refactor to use " instead of `

--- go/food-chain/food_chain.go
+++ go/food-chain/food_chain.go
@@ -2,13 +2,9 @@ package foodchain
 
 func Verse(v int) string {
 	if v == 1 {
-		return `I know an old lady who swallowed a fly.
-I don't know why she swallowed the fly. Perhaps she'll die.`
+		return "I know an old lady who swallowed a fly.\nI don't know why she swallowed the fly. Perhaps she'll die."
 	}
-	return `I know an old lady who swallowed a spider.
-It wriggled and jiggled and tickled inside her.
-She swallowed the spider to catch the fly.
-I don't know why she swallowed the fly. Perhaps she'll die.`
+	return "I know an old lady who swallowed a spider.\nIt wriggled and jiggled and tickled inside her.\nShe swallowed the spider to catch the fly.\nI don't know why she swallowed the fly. Perhaps she'll die."
 }
 
 func Verses(start, end int) string {
