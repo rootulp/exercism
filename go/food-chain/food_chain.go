bd0a003a286b0a9ef0270ae2dead0a910137fe3e
Rootul Patel <rootulp@gmail.com>
2023-11-29 14:03:03 -0500

    pass Verse(2)

--- go/food-chain/food_chain.go
+++ go/food-chain/food_chain.go
@@ -1,7 +1,13 @@
 package foodchain
 
 func Verse(v int) string {
-	return `I know an old lady who swallowed a fly.
+	if v == 1 {
+		return `I know an old lady who swallowed a fly.
+I don't know why she swallowed the fly. Perhaps she'll die.`
+	}
+	return `I know an old lady who swallowed a spider.
+It wriggled and jiggled and tickled inside her.
+She swallowed the spider to catch the fly.
 I don't know why she swallowed the fly. Perhaps she'll die.`
 }
 
