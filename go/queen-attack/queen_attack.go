328ca6e2ac6ca0852cfb313c1036e755f8112ac8
Rootul Patel <rootulp@gmail.com>
2023-12-01 14:15:47 -0500

    solve queen attack

--- go/queen-attack/queen_attack.go
+++ go/queen-attack/queen_attack.go
@@ -5,9 +5,15 @@ import (
 	"strconv"
 )
 
-type location struct {
-	row    int
-	column string
+var fileToColumn = map[string]int{
+	"a": 1,
+	"b": 2,
+	"c": 3,
+	"d": 4,
+	"e": 5,
+	"f": 6,
+	"g": 7,
+	"h": 8,
 }
 
 func CanQueenAttack(whitePosition, blackPosition string) (bool, error) {
@@ -25,8 +31,36 @@ func CanQueenAttack(whitePosition, blackPosition string) (bool, error) {
 	return white.canAttack(black), nil
 }
 
+func parse(position string) (location, error) {
+	if len(position) != 2 {
+		return location{}, errors.New("invalid position")
+	}
+
+	file := string(position[0])
+	row, err := strconv.Atoi(position[1:])
+	if err != nil || !isValidRow(row) || !isValidFile(file) {
+		return location{}, errors.New("invalid position")
+	}
+	column := fileToColumn[file]
+
+	return location{row: row, column: column}, nil
+}
+
+func isValidRow(row int) bool {
+	return row >= 1 && row <= 8
+}
+
+func isValidFile(column string) bool {
+	return column >= "a" && column <= "h"
+}
+
+type location struct {
+	row    int
+	column int
+}
+
 func (l location) canAttack(other location) bool {
-	return l.isSameRow(other) || l.isSameColumn(other)
+	return l.isSameRow(other) || l.isSameColumn(other) || l.isDiagonal(other)
 }
 
 func (l location) isSameRow(other location) bool {
@@ -37,29 +71,13 @@ func (l location) isSameColumn(other location) bool {
 	return l.column == other.column
 }
 
-// func (l location) isDiagonal(other location) bool {
-// 	return abs(l.row-other.row) == abs(int(l.column[0])-int(other.column[0]))
-// }
-
-func parse(position string) (location, error) {
-	if len(position) != 2 {
-		return location{}, errors.New("invalid position")
-	}
-
-	column := string(position[0])
-	row, err := strconv.Atoi(position[1:])
-
-	if err != nil || !isValidRow(row) || !isValidColumn(column) {
-		return location{}, errors.New("invalid position")
-	}
-
-	return location{row: row, column: column}, nil
+func (l location) isDiagonal(other location) bool {
+	return abs(l.row-other.row) == abs(l.column-other.column)
 }
 
-func isValidRow(row int) bool {
-	return row >= 1 && row <= 8
-}
-
-func isValidColumn(column string) bool {
-	return column >= "a" && column <= "h"
+func abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
 }
