e0d83ccc5bde184104ad2ae7d2029511cf8df3ef
Rootul Patel <rootulp@gmail.com>
2023-12-04 14:28:25 -0500

    pass first invalid test

--- go/state-of-tic-tac-toe/state_of_tic_tac_toe.go
+++ go/state-of-tic-tac-toe/state_of_tic_tac_toe.go
@@ -1,5 +1,11 @@
 package stateoftictactoe
 
+import (
+	"errors"
+	"fmt"
+	"strings"
+)
+
 type State string
 
 const (
@@ -9,5 +15,42 @@ const (
 )
 
 func StateOfTicTacToe(board []string) (State, error) {
-	panic("Please implement the StateOfTicTacToe function")
+	game, err := parse(board)
+	if err != nil {
+		return "", err
+	}
+	return game.getState()
+}
+
+type game struct {
+	grid [][]string
+}
+
+func parse(board []string) (*game, error) {
+	grid := [][]string{}
+	for _, row := range board {
+		grid = append(grid, strings.Split(row, ""))
+	}
+	fmt.Printf("%#v\n", grid)
+	return &game{grid: grid}, nil
+}
+
+func (g game) getState() (State, error) {
+	movesX := g.getCount("X")
+	movesO := g.getCount("O")
+	if movesX > movesO+1 {
+		return "", errors.New("invalid board")
+	}
+	return Ongoing, nil
+}
+
+func (g game) getCount(player string) (count int) {
+	for _, row := range g.grid {
+		for _, cell := range row {
+			if cell == player {
+				count++
+			}
+		}
+	}
+	return count
 }
