63f17adec20cbfe6b417be04167db3f627cd313e
Rootul Patel <rootulp@gmail.com>
2023-12-04 14:36:30 -0500

    uncomment passing tests

--- go/state-of-tic-tac-toe/state_of_tic_tac_toe.go
+++ go/state-of-tic-tac-toe/state_of_tic_tac_toe.go
@@ -39,7 +39,49 @@ func (g game) getState() (State, error) {
 	if err := g.isInvalidBoard(); err != nil {
 		return "", err
 	}
+	if isWin := g.isWin(); isWin {
+		return Win, nil
+	}
+	if isDraw := g.isDraw(); isDraw {
+		return Draw, nil
+	}
 	return Ongoing, nil
+}
+
+func (g game) isWin() bool {
+	return g.isWinByRow() || g.isWinByColumn() || g.isWinByDiagonal()
+}
+
+func (g game) isWinByRow() bool {
+	for _, row := range g.grid {
+		if row[0] == row[1] && row[1] == row[2] && row[0] != " " {
+			return true
+		}
+	}
+	return false
+}
+
+func (g game) isWinByColumn() bool {
+	for i := 0; i < 3; i++ {
+		if g.grid[0][i] == g.grid[1][i] && g.grid[1][i] == g.grid[2][i] && g.grid[0][i] != " " {
+			return true
+		}
+	}
+	return false
+}
+
+func (g game) isWinByDiagonal() bool {
+	if g.grid[0][0] == g.grid[1][1] && g.grid[1][1] == g.grid[2][2] && g.grid[0][0] != " " {
+		return true
+	}
+	if g.grid[0][2] == g.grid[1][1] && g.grid[1][1] == g.grid[2][0] && g.grid[0][2] != " " {
+		return true
+	}
+	return false
+}
+
+func (g game) isDraw() bool {
+	return g.getCount("X")+g.getCount("O") == 9 && !g.isWin()
 }
 
 func (g game) isInvalidBoard() error {
