b5a2015070707bdf67b9568978ba9907117e914e
Rootul Patel <rootulp@gmail.com>
2023-12-05 16:51:07 -0500

    pass first test case

--- go/alphametics/alphametics.go
+++ go/alphametics/alphametics.go
@@ -3,6 +3,8 @@ package alphametics
 import (
 	"errors"
 	"fmt"
+	"math/rand"
+	"strconv"
 	"strings"
 )
 
@@ -21,10 +23,69 @@ func Solve(input string) (map[string]int, error) {
 	fmt.Printf("addends: %s\n", addends)
 	fmt.Printf("sum: %s\n", sum)
 	words := append([]string{sum}, addends...)
+
 	letters := getUniqueLetters(words)
 	fmt.Printf("letters %v\n", letters)
 
-	return map[string]int{}, nil
+	for {
+		letterToNumber := assignRandomValues(letters)
+		fmt.Printf("letterToNumber %v\n", letterToNumber)
+
+		evaluated, err := evaluate(sum, addends, letterToNumber)
+		if err != nil {
+			return nil, err
+		}
+		if evaluated {
+			return letterToNumber, nil
+		}
+	}
+}
+
+func evaluate(sum string, addends []string, letterToNumber map[string]int) (bool, error) {
+	result := 0
+	for _, addend := range addends {
+		translated, err := translate(letterToNumber, addend)
+		if err != nil {
+			return false, err
+		}
+		result += translated
+	}
+	realSum, err := translate(letterToNumber, sum)
+	if err != nil {
+		return false, err
+	}
+	return result == realSum, nil
+}
+
+func translate(letterToNumber map[string]int, word string) (int, error) {
+	translated := ""
+	for _, letter := range word {
+		value := letterToNumber[string(letter)]
+		translated += strconv.Itoa(value)
+	}
+	result, err := strconv.Atoi(translated)
+	if err != nil {
+		return 0, err
+	}
+	return result, nil
+}
+
+func assignRandomValues(letters map[string]bool) map[string]int {
+	values := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
+	result := map[string]int{}
+	for letter := range letters {
+		value, remainingElements := popRandomElement(values)
+		values = remainingElements
+		result[letter] = value
+	}
+	return result
+}
+
+func popRandomElement(x []int) (int, []int) {
+	index := rand.Intn(len(x))
+	result := x[index]
+	x = append(x[:index], x[index+1:]...)
+	return result, x
 }
 
 func getUniqueLetters(words []string) map[string]bool {
