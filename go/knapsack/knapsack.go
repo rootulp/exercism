cb0b19866acf8065ac160e7617cc6f42224a47f5
Rootul Patel <rootulp@gmail.com>
2024-08-12 21:52:59 -0400

    refactor: remove unnecessary code

--- go/knapsack/knapsack.go
+++ go/knapsack/knapsack.go
@@ -8,9 +8,6 @@ type Item struct {
 // and returns the maximum value that can be carried by the knapsack
 // given that the knapsack can only carry a maximum weight given by maximumWeight
 func Knapsack(maximumWeight int, items []Item) int {
-	if len(items) == 0 {
-		return 0
-	}
 	currentWeight := maximumWeight
 	total := 0
 	for _, item := range items {
