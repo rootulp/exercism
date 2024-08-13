5eba7625dbc30f316f0c146c129e4b432d3219fd
Rootul Patel <rootulp@gmail.com>
2024-08-12 21:50:50 -0400

    solve first two test cases

--- go/knapsack/knapsack.go
+++ go/knapsack/knapsack.go
@@ -8,5 +8,16 @@ type Item struct {
 // and returns the maximum value that can be carried by the knapsack
 // given that the knapsack can only carry a maximum weight given by maximumWeight
 func Knapsack(maximumWeight int, items []Item) int {
-	panic("Please implement the Knapsack() function")
+	if len(items) == 0 {
+		return 0
+	}
+	currentWeight := maximumWeight
+	total := 0
+	for _, item := range items {
+		if item.Weight <= currentWeight {
+			currentWeight -= item.Weight
+			total += item.Value
+		}
+	}
+	return total
 }
