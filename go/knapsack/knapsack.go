1dac75d917a0b05bd85693b9a0a58eba59762d06
Rootul Patel <rootulp@gmail.com>
2024-08-12 22:15:58 -0400

    cleanup

--- go/knapsack/knapsack.go
+++ go/knapsack/knapsack.go
@@ -7,10 +7,10 @@ type Item struct {
 // Knapsack takes in a maximum carrying capacity and a collection of items
 // and returns the maximum value that can be carried by the knapsack
 // given that the knapsack can only carry a maximum weight given by maximumWeight
-func Knapsack(maximumWeight int, items []Item) (total int) {
-	currentValue := 0
-	currentWeight := 0
-	return knapsack(items, currentValue, currentWeight, maximumWeight)
+func Knapsack(maxWeight int, items []Item) (total int) {
+	value := 0
+	weight := 0
+	return knapsack(items, value, weight, maxWeight)
 }
 
 func knapsack(items []Item, value int, weight int, maxWeight int) (total int) {
@@ -21,8 +21,8 @@ func knapsack(items []Item, value int, weight int, maxWeight int) (total int) {
 	totalWithItem := knapsack(items[1:], value+item.Value, weight+item.Weight, maxWeight)
 	totalWithoutItem := knapsack(items[1:], value, weight, maxWeight)
 
-	// If the current item is too heavy, return the total without the current item
 	if weight+item.Weight > maxWeight {
+		// If the current item is too heavy, return the total without the current item
 		return totalWithoutItem
 	}
 	return max(totalWithItem, totalWithoutItem)
