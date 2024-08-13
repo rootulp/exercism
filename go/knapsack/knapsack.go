2d6c968e034293dab32ada7c8abef4a5e4de66c5
Rootul Patel <rootulp@gmail.com>
2024-08-12 22:04:09 -0400

    wip

--- go/knapsack/knapsack.go
+++ go/knapsack/knapsack.go
@@ -1,5 +1,10 @@
 package knapsack
 
+import (
+	"fmt"
+	"sort"
+)
+
 type Item struct {
 	Weight, Value int
 }
@@ -7,14 +12,24 @@ type Item struct {
 // Knapsack takes in a maximum carrying capacity and a collection of items
 // and returns the maximum value that can be carried by the knapsack
 // given that the knapsack can only carry a maximum weight given by maximumWeight
-func Knapsack(maximumWeight int, items []Item) int {
+func Knapsack(maximumWeight int, items []Item) (maxValue int) {
+	sorted := sortItemsByValuePerWeight(items)
+	fmt.Printf("sorted: %v\n", sorted)
 	currentWeight := maximumWeight
-	total := 0
-	for _, item := range items {
+	for _, item := range sorted {
 		if item.Weight <= currentWeight {
 			currentWeight -= item.Weight
-			total += item.Value
+			maxValue += item.Value
 		}
 	}
-	return total
+	return maxValue
+}
+
+func sortItemsByValuePerWeight(items []Item) []Item {
+	sort.Slice(items, func(i, j int) bool {
+		iValuePerWeight := float64(items[i].Value) / float64(items[i].Weight)
+		jValuePerWeight := float64(items[j].Value) / float64(items[j].Weight)
+		return iValuePerWeight > jValuePerWeight
+	})
+	return items
 }
