98cedda3039da58d2e307b82a74b3b7b79615539
Rootul Patel <rootulp@gmail.com>
2025-01-03 20:13:06 -0500

    feat: pass first test

--- rust/collatz-conjecture/src/lib.rs
+++ rust/collatz-conjecture/src/lib.rs
@@ -1,3 +1,6 @@
 pub fn collatz(n: u64) -> Option<u64> {
+    if n == 1 {
+        return Some(0);
+    }
     todo!("return Some(x) where x is the number of steps required to reach 1 starting with {n}")
 }
