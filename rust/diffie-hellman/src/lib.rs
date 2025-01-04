f3fd1e91fcc6fb9cd05ad7a4d84bd94cde2e38ff
Rootul Patel <rootulp@gmail.com>
2025-01-03 20:52:40 -0500

    refactor

--- rust/diffie-hellman/src/lib.rs
+++ rust/diffie-hellman/src/lib.rs
@@ -3,8 +3,7 @@ use rand::Rng;
 
 pub fn private_key(p: u64) -> u64 {
     let mut rng = rand::thread_rng();
-    let result = rng.gen_range(2..p);
-    return result;
+    rng.gen_range(2..p)
 }
 
 pub fn public_key(p: u64, g: u64, a: u64) -> u64 {
