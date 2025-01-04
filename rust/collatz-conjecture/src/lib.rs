9fa5fcdf9ee9c48389e63f439f72c7fa6da8fed9
Rootul Patel <rootulp@gmail.com>
2025-01-03 20:57:49 -0500

    cargo fmt

--- rust/collatz-conjecture/src/lib.rs
+++ rust/collatz-conjecture/src/lib.rs
@@ -7,14 +7,14 @@ pub fn collatz(n: u64) -> Option<u64> {
 
 fn num_steps(n: u64, steps: u64) -> u64 {
     if n == 1 {
-        return steps
+        return steps;
     }
     if is_even(n) {
-        return num_steps(n / 2, steps + 1)
+        return num_steps(n / 2, steps + 1);
     }
-    return num_steps((3 * n) + 1, steps + 1)
+    return num_steps((3 * n) + 1, steps + 1);
 }
 
 fn is_even(n: u64) -> bool {
-    return n % 2 == 0
+    return n % 2 == 0;
 }
--- rust/diffie-hellman/src/lib.rs
+++ rust/diffie-hellman/src/lib.rs
@@ -7,9 +7,18 @@ pub fn private_key(p: u64) -> u64 {
 }
 
 pub fn public_key(p: u64, g: u64, a: u64) -> u64 {
-    g.to_biguint().unwrap().modpow(&a.to_biguint().unwrap(), &p.to_biguint().unwrap()).try_into().unwrap()
+    g.to_biguint()
+        .unwrap()
+        .modpow(&a.to_biguint().unwrap(), &p.to_biguint().unwrap())
+        .try_into()
+        .unwrap()
 }
 
 pub fn secret(p: u64, b_pub: u64, a: u64) -> u64 {
-    b_pub.to_biguint().unwrap().modpow(&a.to_biguint().unwrap(), &p.to_biguint().unwrap()).try_into().unwrap()
+    b_pub
+        .to_biguint()
+        .unwrap()
+        .modpow(&a.to_biguint().unwrap(), &p.to_biguint().unwrap())
+        .try_into()
+        .unwrap()
 }
