6af3b17181a1aa748e92587ad736f0f9190a7522
Rootul Patel <rootulp@gmail.com>
2025-01-05 18:01:24 -0500

    refactor

--- rust/series/src/lib.rs
+++ rust/series/src/lib.rs
@@ -3,8 +3,10 @@ pub fn series(digits: &str, len: usize) -> Vec<String> {
         return vec![];
     }
 
-    let windows: Vec<&str> = (0..=digits.len() - len)
-    .map(|i| &digits[i..i + len])
-    .collect();
-    windows.iter().map(|&s| s.to_string()).collect()
+    (0..=digits.len() - len)
+        .map(|i| &digits[i..i + len])
+        .collect::<Vec<&str>>()
+        .iter()
+        .map(|&s| s.to_string())
+        .collect()
 }
