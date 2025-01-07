8d38402df0b29602e8a9888351a0bc5d921f91a4
Rootul Patel <rootulp@gmail.com>
2025-01-07 10:36:54 -0500

    refactor

--- rust/eliuds-eggs/src/lib.rs
+++ rust/eliuds-eggs/src/lib.rs
@@ -1,20 +1,16 @@
 pub fn egg_count(decimal: u32) -> usize {
     let binary = decimal_to_binary(decimal);
-    let eggs = count('1', binary);
-    eggs
+    count('1', binary)
 }
 
 fn decimal_to_binary(decimal: u32) -> String {
     let mut result = "".to_string();
-
     let mut n = decimal;
     while n != 0 {
         let remainder = n % 2;
         n = n / 2;
-        let c = char::from_digit(remainder, 10).unwrap();
-        result.insert(0, c);
+        result.insert(0, char::from_digit(remainder, 10).unwrap());
     }
-
     result
 }
 
