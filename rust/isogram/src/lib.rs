6b26124ea1bae4d1606266145aad01b716878ce0
Rootul Patel <rootulp@gmail.com>
2025-01-09 13:40:01 -0500

    refactor

--- rust/isogram/src/lib.rs
+++ rust/isogram/src/lib.rs
@@ -2,10 +2,8 @@ use std::collections::HashSet;
 
 pub fn check(candidate: &str) -> bool {
     let mut seen: HashSet<char> = HashSet ::new();
-    for char in candidate.to_lowercase().chars() {
-        if !char.is_alphabetic() {
-            continue
-        }
+
+    for char in candidate.to_lowercase().chars().filter(|c| c.is_alphabetic()) {
         if seen.contains(&char) {
             return false
         }
