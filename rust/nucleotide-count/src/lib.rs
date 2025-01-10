02f388617fada8da559739ec2a8958655dfb9f1a
Rootul Patel <rootulp@gmail.com>
2025-01-09 22:18:03 -0500

    refactor

--- rust/nucleotide-count/src/lib.rs
+++ rust/nucleotide-count/src/lib.rs
@@ -5,21 +5,22 @@ pub fn count(nucleotide: char, dna: &str) -> Result<usize, char> {
         return Err(nucleotide)
     }
     let counts = nucleotide_counts(dna)?;
-    let result = counts.get(&nucleotide).unwrap_or(&0);
-    return Ok(*result)
+    Ok(*counts.get(&nucleotide).unwrap())
 }
 
 pub fn nucleotide_counts(dna: &str) -> Result<HashMap<char, usize>, char> {
-    let mut counts: HashMap<char, usize> = default_map();
     for c in dna.chars() {
         if !valid_nucleotide(c) {
             return Err(c)
         }
+    }
+    let mut counts: HashMap<char, usize> = default_map();
+    for c in dna.chars() {
         let default: usize = 0;
         let v = counts.get(&c).unwrap_or(&default);
         counts.insert(c, v + 1);
     }
-    return Ok(counts)
+    Ok(counts)
 }
 
 fn default_map() -> HashMap<char, usize> {
